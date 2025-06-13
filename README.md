beac0caf161a396504a90bfd4915881ff50e90d6
henrikabel25 <henrikabel@foxmail.com>
2025-06-13 15:20:56 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Getting Started
+
+**Not recommended for use during development. The first official release is expected on December 31, 2025.**
---
+++ spring-ddd-launcher/src/main/java/com/springddd/SpringDDDLauncherApplication.java
@@ -0,0 +1,12 @@
+package com.springddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDDDLauncherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDDDLauncherApplication.class, args);
+    }
+}
