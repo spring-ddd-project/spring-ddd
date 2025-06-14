66aa818d8fc9dc21108e4ea9fa236d4c2c2875d7
henrikabel25 <henrikabel@foxmail.com>
2025-06-14 12:10:17 +0800

    feat: add the create factory method for system user

---
+++ spring-ddd-domain/src/main/java/com/springddd/domain/user/SysUserDomainFactory.java
@@ -0,0 +1,7 @@
+package com.springddd.domain.user;
+
+public interface SysUserDomainFactory {
+
+    SysUserDomain newInstance(Account account, ExtendInfo extendInfo,
+                              Long deptId, String createBy);
+}
