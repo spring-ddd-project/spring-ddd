7d30af6ca41071b71986d0c08587396c8dccf438
henrikabel25 <henrikabel@foxmail.com>
2025-06-14 11:12:13 +0800

    fix: update example code

--- spring-ddd-infrastructure-persistence/src/main/java/com/springddd/infrastructure/persistence/mapper/TestUserMapper.java
+++ spring-ddd-infrastructure-persistence/src/main/java/com/springddd/infrastructure/persistence/mapper/TestUserMapper.java
@@ -1,6 +1,6 @@
-package com.springddd.mapper;
+package com.springddd.infrastructure.persistence.mapper;
 
-import com.springddd.entity.TestUserEntity;
+import com.springddd.domain.TestUserEntity;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 public interface TestUserMapper extends ReactiveCrudRepository<TestUserEntity, Integer> {
