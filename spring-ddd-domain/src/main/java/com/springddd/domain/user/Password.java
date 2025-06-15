c85771ec97ae2ecde5550084011c14ee14db414a
henrikabel25 <henrikabel@foxmail.com>
2025-06-15 21:33:55 +0800

    feat: Add system role domain models and exception information, and update the syntax of the system user value object.

--- spring-ddd-domain/src/main/java/com/springddd/domain/user/Password.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/user/Password.java
@@ -1,6 +1,6 @@
 package com.springddd.domain.user;
 
-import com.springddd.domain.exception.PasswordNullException;
+import com.springddd.domain.user.exception.PasswordNullException;
 import org.springframework.util.ObjectUtils;
 
 public record Password(String value) {
