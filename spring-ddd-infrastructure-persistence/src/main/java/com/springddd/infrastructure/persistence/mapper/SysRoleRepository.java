1b6bbbaa82f3f864fe98ab24431b7a1020b8db88
henrikabel25 <henrikabel@foxmail.com>
2025-06-16 16:41:50 +0800

    feat: Implement the infrastructure layer for system roles

---
+++ spring-ddd-infrastructure-persistence/src/main/java/com/springddd/infrastructure/persistence/mapper/SysRoleRepository.java
@@ -0,0 +1,7 @@
+package com.springddd.infrastructure.persistence.mapper;
+
+import com.springddd.infrastructure.persistence.entity.SysRoleEntity;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+public interface SysRoleRepository extends ReactiveCrudRepository<SysRoleEntity, Long> {
+}
