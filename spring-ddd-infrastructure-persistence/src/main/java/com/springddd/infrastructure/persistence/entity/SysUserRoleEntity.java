c55da96f830f43757f5b3f4f7473fa33a1f0e738
henrikabel25 <henrikabel@foxmail.com>
2025-06-17 15:46:26 +0800

    feat: Implement the persistence layer for associating system users with system roles.

---
+++ spring-ddd-infrastructure-persistence/src/main/java/com/springddd/infrastructure/persistence/entity/SysUserRoleEntity.java
@@ -0,0 +1,40 @@
+package com.springddd.infrastructure.persistence.entity;
+
+import com.springddd.domain.util.IdGenerate;
+import lombok.Data;
+import org.springframework.data.annotation.*;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDateTime;
+
+@Data
+@Table("sys_user_role")
+public class SysUserRoleEntity {
+
+    @Id
+    @IdGenerate
+    private Long id;
+
+    private Long userId;
+
+    private Long roleId;
+
+    private Long deptId;
+
+    @CreatedBy
+    private String createBy;
+
+    @CreatedDate
+    private LocalDateTime createTime;
+
+    @LastModifiedBy
+    private String updateBy;
+
+    @LastModifiedDate
+    private LocalDateTime updateTime;
+
+    private String deleteStatus;
+
+    @Version
+    private String version;
+}
