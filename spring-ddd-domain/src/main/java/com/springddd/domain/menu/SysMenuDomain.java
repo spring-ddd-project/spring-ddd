9cfe16c589bc702ab8366ffae0bd26f859cc81fb
henrikabel25 <henrikabel@foxmail.com>
2025-06-18 17:25:24 +0800

    feat: Complete CRUD for system menu module

--- spring-ddd-domain/src/main/java/com/springddd/domain/menu/SysMenuDomain.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/SysMenuDomain.java
@@ -4,6 +4,8 @@ import com.springddd.domain.AbstractDomainMask;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.time.LocalDateTime;
+
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class SysMenuDomain extends AbstractDomainMask {
@@ -15,4 +17,23 @@ public class SysMenuDomain extends AbstractDomainMask {
     private MenuBasicInfo menuBasicInfo;
 
     private MenuExtendInfo menuExtendInfo;
+
+    public void create() {}
+
+    public void update(MenuId parentId,
+                       MenuBasicInfo menuBasicInfo,
+                       MenuExtendInfo menuExtendInfo, Long deptId, String updateBy) {
+        this.parentId = parentId;
+        this.menuBasicInfo = menuBasicInfo;
+        this.menuExtendInfo = menuExtendInfo;
+        super.setDeptId(deptId);
+        super.setUpdateBy(updateBy);
+        super.setUpdateTime(LocalDateTime.now());
+    }
+
+    public void delete(String updateBy) {
+        super.setDeleteStatus("1");
+        super.setUpdateBy(updateBy);
+        super.setUpdateTime(LocalDateTime.now());
+    }
 }
