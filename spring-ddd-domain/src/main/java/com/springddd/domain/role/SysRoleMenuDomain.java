280d74c4dc9a175f38e9a3f6c3b3396212a68074
henrikabel25 <henrikabel@foxmail.com>
2025-06-18 12:10:40 +0800

    feat: implement CRUD for the system role-menu binding domain model

--- spring-ddd-domain/src/main/java/com/springddd/domain/role/SysRoleMenuDomain.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/role/SysRoleMenuDomain.java
@@ -5,6 +5,8 @@ import com.springddd.domain.menu.MenuId;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.time.LocalDateTime;
+
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class SysRoleMenuDomain extends AbstractDomainMask {
@@ -14,4 +16,18 @@ public class SysRoleMenuDomain extends AbstractDomainMask {
     private RoleId roleId;
 
     private MenuId menuId;
+
+    public void create() {}
+
+    public void update(RoleId roleId, MenuId menuId, Long deptId, String updateBy) {
+        this.roleId = roleId;
+        this.menuId = menuId;
+        super.setDeptId(deptId);
+        super.setUpdateBy(updateBy);
+        super.setUpdateTime(LocalDateTime.now());
+    }
+
+    public void delete(String updateBy) {
+        super.setDeleteStatus("1");
+    }
 }
