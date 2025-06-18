5da387af04add88d979a4facaec29fa13dfa8f69
henrikabel25 <henrikabel@foxmail.com>
2025-06-18 12:38:05 +0800

    feat: Add a system menu data model layer to support structured menu management.

---
+++ spring-ddd-infrastructure-persistence/src/main/java/com/springddd/infrastructure/persistence/entity/SysMenuEntity.java
@@ -0,0 +1,53 @@
+package com.springddd.infrastructure.persistence.entity;
+
+import lombok.Data;
+import org.springframework.data.annotation.*;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDateTime;
+
+@Data
+@Table("sys_menu")
+public class SysMenuEntity {
+
+    private Long id;
+
+    private Long parentId;
+
+    private String name;
+
+    private String permission;
+
+    private String path;
+
+    private String icon;
+
+    private String visible;
+
+    private Integer sortOrder;
+
+    private String embedded;
+
+    private String menuType;
+
+    private String menuStatus;
+
+    private Long deptId;
+
+    @CreatedBy
+    private String createBy;
+
+    @CreatedDate
+    private LocalDateTime createTime;
+
+    @LastModifiedBy
+    private String updateBy;
+
+    @LastModifiedDate
+    private LocalDateTime updateTime;
+
+    private String deleteStatus;
+
+    @Version
+    private String version;
+}
