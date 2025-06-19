158f6d09b9d8f52e97bf14d6441e890bee1d5b6d
henrikabel25 <henrikabel@foxmail.com>
2025-06-19 18:10:29 +0800

    feat: Introduce a centralized Redis utility class for unified operations.

---
+++ spring-ddd-domain/src/main/java/com/springddd/domain/auth/SecurityUtils.java
@@ -0,0 +1,51 @@
+package com.springddd.domain.auth;
+
+import com.springddd.domain.menu.MenuPermission;
+import com.springddd.domain.role.RoleCode;
+
+import java.util.List;
+
+public class SecurityUtils {
+
+    private static Long userId;
+
+    private static String username;
+
+    private static List<RoleCode> roles;
+
+    private static List<MenuPermission> permissions;
+
+    public static void setUserId(Long userId) {
+        SecurityUtils.userId = userId;
+    }
+
+    public static void setUsername(String username) {
+        SecurityUtils.username = username;
+    }
+
+    public static void setRoles(List<RoleCode> roles) {
+        SecurityUtils.roles = roles;
+    }
+
+    public static void setPermissions(List<MenuPermission> permissions) {
+        SecurityUtils.permissions = permissions;
+    }
+
+    public static Long getUserId() {
+        return SecurityUtils.userId;
+    }
+
+    public static String getUsername() {
+        return SecurityUtils.username;
+    }
+
+    public static List<RoleCode> getRoles() {
+        return SecurityUtils.roles;
+    }
+
+    public static List<MenuPermission> getPermissions() {
+        return SecurityUtils.permissions;
+    }
+
+}
+
