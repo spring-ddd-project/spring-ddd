0836eee19edef8ff68aa590a5dffb7fada3ddcbd
henrikabel25 <henrikabel@foxmail.com>
2025-06-21 18:20:12 +0800

    feat: Intercept unauthorized access attempts

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/jwt/JwtAuthenticationConverter.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/jwt/JwtAuthenticationConverter.java
@@ -1,5 +1,6 @@
 package com.springddd.application.service.auth.jwt;
 
+import com.springddd.application.service.auth.SecurityProperties;
 import com.springddd.domain.auth.AuthUser;
 import com.springddd.domain.menu.MenuPermission;
 import io.jsonwebtoken.Claims;
@@ -13,6 +14,7 @@ import org.springframework.security.web.server.authentication.ServerAuthenticati
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.util.pattern.PathPatternParser;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
@@ -23,11 +25,25 @@ public class JwtAuthenticationConverter implements ServerAuthenticationConverter
 
     private final JwtTemplate jwtTemplate;
 
+    private final SecurityProperties securityProperties;
+
+    private final PathPatternParser pathPatternParser = new PathPatternParser();
+
+
     @Override
     public Mono<Authentication> convert(ServerWebExchange exchange) {
+        boolean isIgnored = securityProperties.getIgnorePaths().stream()
+                .map(pathPatternParser::parse)
+                .anyMatch(pattern -> pattern.matches(exchange.getRequest().getPath()));
+
+        if (isIgnored) {
+            return Mono.empty();
+        }
+
         String token = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
         if (ObjectUtils.isEmpty(token)) {
-            return Mono.error(new AccessDeniedException("No authorization header"));
+            return Mono.error(new AccessDeniedException("Missing Authorization header"));
         }
 
         try {
@@ -37,7 +53,11 @@ public class JwtAuthenticationConverter implements ServerAuthenticationConverter
 
             AuthUser user = new AuthUser();
             user.setUsername(username);
-            user.setPermissions(permissions.stream().map(MenuPermission::new).toList());
+            user.setPermissions(
+                    permissions.stream()
+                            .map(MenuPermission::new)
+                            .toList()
+            );
 
             return Mono.just(
                     new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities())
@@ -46,5 +66,6 @@ public class JwtAuthenticationConverter implements ServerAuthenticationConverter
             return Mono.error(new AccessDeniedException(e.getMessage()));
         }
     }
+
 }
 
