3fca5daea55860c3cf45517245553d494afd55ca
henrikabel25 <henrikabel@foxmail.com>
2025-06-23 13:34:35 +0800

    pref: Enhancements to the Command option in the system menu

--- spring-ddd-domain/src/main/java/com/springddd/domain/menu/MenuExtendInfo.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/MenuExtendInfo.java
@@ -1,8 +1,10 @@
 package com.springddd.domain.menu;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class MenuExtendInfo {
 
     private Integer order;
@@ -22,4 +24,16 @@ public class MenuExtendInfo {
     private Boolean embedded;
 
     private Boolean menuStatus;
+
+    public MenuExtendInfo(Integer order, String title, Boolean affixTab, Boolean noBasicLayout, String icon, Integer menuType, Boolean visible, Boolean embedded, Boolean menuStatus) {
+        this.order = order;
+        this.title = title;
+        this.affixTab = affixTab;
+        this.noBasicLayout = noBasicLayout;
+        this.icon = icon;
+        this.menuType = menuType;
+        this.visible = visible;
+        this.embedded = embedded;
+        this.menuStatus = menuStatus;
+    }
 }
