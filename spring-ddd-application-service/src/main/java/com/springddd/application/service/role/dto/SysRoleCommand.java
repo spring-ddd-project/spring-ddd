4290f4e61edf5e1ab464d04a01189fa50b15cc5c
henrikabel25 <henrikabel@foxmail.com>
2025-06-23 19:53:10 +0800

    feat： Added owner role with full permissions

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/role/dto/SysRoleCommand.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/role/dto/SysRoleCommand.java
@@ -19,6 +19,8 @@ public class SysRoleCommand implements Serializable {
 
     private Boolean roleStatus;
 
+    private Boolean owner;
+
     private Long deptId;
 
     private Boolean deleteStatus;
