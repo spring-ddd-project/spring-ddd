308c8bdf476d63ceac25a5d6044d40c93e4e50bd
henrikabel25 <henrikabel@foxmail.com>
2025-06-24 21:51:01 +0800

    fix: Remove the requirement for permission and component to be mandatory fields.

--- spring-ddd-domain/src/main/java/com/springddd/domain/menu/MenuComponent.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/MenuComponent.java
@@ -1,13 +1,4 @@
 package com.springddd.domain.menu;
 
-import com.springddd.domain.menu.exception.MenuComponentNullException;
-import org.springframework.util.ObjectUtils;
-
 public record MenuComponent(String value) {
-
-    public MenuComponent {
-        if (ObjectUtils.isEmpty(value)) {
-            throw new MenuComponentNullException();
-        }
-    }
 }
