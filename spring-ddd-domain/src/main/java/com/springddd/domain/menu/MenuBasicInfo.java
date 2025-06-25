0d0da8ae538033e040beba49eda9139c9d938a46
henrikabel25 <henrikabel@foxmail.com>
2025-06-25 17:51:43 +0800

    fix: Update menu fields to be compatible with the frontend

--- spring-ddd-domain/src/main/java/com/springddd/domain/menu/MenuBasicInfo.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/MenuBasicInfo.java
@@ -7,14 +7,12 @@ public class MenuBasicInfo {
 
     private MenuName menuName;
 
-    private MenuPermission menuPermission;
-
-    private MenuType menuType;
-
-    private MenuIcon menuIcon;
-
     private MenuPath menuPath;
 
     private MenuComponent menuComponent;
+
+    private MenuRedirect menuRedirect;
+
+    private MenuPermission menuPermission;
 
 }
