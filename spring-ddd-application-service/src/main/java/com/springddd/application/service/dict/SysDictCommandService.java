2c61bb159b877cfdf2f769926b3ed26b8fa717cd
henrikabel25 <henrikabel@foxmail.com>
2025-06-25 16:31:35 +0800

    feat: Add a domain service for the data dictionary

---
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/SysDictCommandService.java
@@ -0,0 +1,41 @@
+package com.springddd.application.service.dict;
+
+import com.springddd.application.service.dict.dto.SysDictCommand;
+import com.springddd.domain.dict.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class SysDictCommandService {
+
+    private final SysDictDomainRepository sysDictDomainRepository;
+
+    private final SysDictDomainFactory sysDictDomainFactory;
+
+    public Mono<Long> create(SysDictCommand command) {
+        DictBasicInfo basicInfo = new DictBasicInfo(new DictName(command.getDictName()), new DictCode(command.getDictCode()));
+        DictExtendInfo extendInfo = new DictExtendInfo(command.getSortOrder(), command.getDictStatus());
+        SysDictDomain sysDictDomain = sysDictDomainFactory.newInstance(basicInfo, extendInfo);
+        sysDictDomain.create();
+        return sysDictDomainRepository.save(sysDictDomain);
+    }
+
+    public Mono<Void> update(SysDictCommand command) {
+        return sysDictDomainRepository.load(new DictId(command.getId())).map(domain -> {
+            DictBasicInfo basicInfo = new DictBasicInfo(new DictName(command.getDictName()), new DictCode(command.getDictCode()));
+            DictExtendInfo extendInfo = new DictExtendInfo(command.getSortOrder(), command.getDictStatus());
+
+            domain.update(basicInfo, extendInfo);
+            return sysDictDomainRepository.save(domain);
+        }).then();
+    }
+
+    public Mono<Void> delete(SysDictCommand command) {
+        return sysDictDomainRepository.load(new DictId(command.getId())).map(domain -> {
+            domain.delete();
+            return sysDictDomainRepository.save(domain);
+        }).then();
+    }
+}
