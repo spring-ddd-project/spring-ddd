94a0976224daf76ba09872ca9b7c8bed3d9f69cc
henrikabel25 <henrikabel@foxmail.com>
2025-06-28 19:10:16 +0800

    feat: while deleting roles, also delete the associated user-role and role-menu mappings.

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/role/WipeSysRoleByIdsDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/role/WipeSysRoleByIdsDomainServiceImpl.java
@@ -1,10 +1,17 @@
 package com.springddd.application.service.role;
 
+import com.springddd.application.service.role.dto.SysRoleMenuView;
+import com.springddd.application.service.user.SysUserRoleCommandService;
+import com.springddd.application.service.user.SysUserRoleQueryService;
+import com.springddd.application.service.user.dto.SysUserRoleView;
+import com.springddd.domain.auth.SecurityUtils;
 import com.springddd.domain.role.WipeSysRoleByIdsDomainService;
 import com.springddd.infrastructure.persistence.r2dbc.SysRoleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
@@ -15,9 +22,34 @@ public class WipeSysRoleByIdsDomainServiceImpl implements WipeSysRoleByIdsDomain
 
     private final SysRoleRepository sysRoleRepository;
 
+    private final SysUserRoleQueryService sysUserRoleQueryService;
+
+    private final SysUserRoleCommandService sysUserRoleCommandService;
+
+    private final SysRoleMenuQueryService sysRoleMenuQueryService;
+
+    private final SysRoleMenuCommandService sysRoleMenuCommandService;
+
     @Override
     @Transactional(rollbackFor = Exception.class)
     public Mono<Void> deleteByIds(List<Long> ids) {
-        return sysRoleRepository.deleteAllById(ids);
+        return Flux.fromIterable(ids)
+                .flatMap(roleId -> sysUserRoleQueryService.queryLinkUserAndRoleByRoleId(roleId)
+                        .flatMap(ur -> {
+                            List<Long> urIds = ur.stream().map(SysUserRoleView::getId).toList();
+                            if (CollectionUtils.isEmpty(urIds)) {
+                                return Mono.empty();
+                            }
+                            return sysUserRoleCommandService.wipe(urIds);
+                        })
+                        .then(sysRoleMenuQueryService.queryLinkRoleAndMenus(roleId))
+                        .flatMap(rm -> {
+                            List<Long> rmIds = rm.stream().map(SysRoleMenuView::getId).toList();
+                            if (CollectionUtils.isEmpty(rmIds)) {
+                                return Mono.empty();
+                            }
+                            return sysRoleMenuCommandService.wipe(rmIds);
+                        }), SecurityUtils.concurrency())
+                .thenMany(sysRoleRepository.deleteAllById(ids)).then();
     }
 }
