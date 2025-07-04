18b7719e38d36e33d161c1509390266b0fbf0c30
henrikabel25 <henrikabel@foxmail.com>
2025-07-04 16:15:50 +0800

    feat: add column properties for code generation in the Command service

--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenColumnsDomain.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenColumnsDomain.java
@@ -15,4 +15,15 @@ public class GenColumnsDomain extends AbstractDomainMask {
     private GenColumnsBasicInfo basicInfo;
 
     private GenColumnsExtendInfo extendInfo;
+
+    public void create() {}
+
+    public void update(GenColumnsBasicInfo basicInfo, GenColumnsExtendInfo extendInfo) {
+        this.basicInfo = basicInfo;
+        this.extendInfo = extendInfo;
+    }
+
+    public void delete() {
+        super.setDeleteStatus(true);
+    }
 }
