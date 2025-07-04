2fe49c85a778b909c5fb385a6413fd1a63beaa34
henrikabel25 <henrikabel@foxmail.com>
2025-07-04 14:44:31 +0800

    fix: remove unnecessary attributes in code generation for table information

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenInfoQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenInfoQuery.java
@@ -19,37 +19,5 @@ public class GenInfoQuery implements Serializable {
 
     private String requestName;
 
-    private Boolean propValueObject;
-
-    private String propColumnKey;
-
-    private String propColumnName;
-
-    private String propColumnType;
-
-    private String propColumnComment;
-
-    private String propJavaEntity;
-
-    private String propJavaType;
-
-    private Long propDictId;
-
-    private Boolean tableVisible;
-
-    private Boolean tableOrder;
-
-    private Boolean tableFilter;
-
-    private Integer tableFilterComponent;
-
-    private Integer tableFilterType;
-
-    private Integer formComponent;
-
-    private Boolean formVisible;
-
-    private Boolean formRequired;
-
     private Boolean deleteStatus;
 }
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenInfoView.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenInfoView.java
@@ -17,46 +17,5 @@ public class GenInfoView implements Serializable {
 
     private String requestName;
 
-    private Boolean propValueObject;
-
-    private String propColumnKey;
-
-    private String propColumnName;
-
-    private String propColumnType;
-
-    private String propColumnComment;
-
-    private String propJavaEntity;
-
-    private String propJavaType;
-
-    private Long propDictId;
-
-    private Boolean tableVisible;
-
-    private Boolean tableOrder;
-
-    private Boolean tableFilter;
-
-    private Integer tableFilterComponent;
-
-    private Integer tableFilterType;
-
-    private Integer formComponent;
-
-    private Boolean formVisible;
-
-    private Boolean formRequired;
-
     private Boolean deleteStatus;
-
-    public GenInfoView() {}
-
-    public GenInfoView(String propColumnKey, String propColumnName, String propColumnType, String propColumnComment) {
-        this.propColumnKey = propColumnKey;
-        this.propColumnName = propColumnName;
-        this.propColumnType = propColumnType;
-        this.propColumnComment = propColumnComment;
-    }
 }
