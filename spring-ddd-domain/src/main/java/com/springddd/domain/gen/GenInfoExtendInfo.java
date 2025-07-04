c23f672b1cca8fa68a783afb370322e7e32f0d7a
henrikabel25 <henrikabel@foxmail.com>
2025-07-04 14:12:17 +0800

    fix: refactor code generation for table information service

--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenInfoExtendInfo.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenInfoExtendInfo.java
@@ -1,19 +1,4 @@
 package com.springddd.domain.gen;
 
-public record GenInfoExtendInfo(Boolean propValueObject,
-                                String propColumnKey,
-                                String propColumnName,
-                                String propColumnType,
-                                String propColumnComment,
-                                String propJavaEntity,
-                                String propJavaType,
-                                Long propDictId,
-                                Boolean tableVisible,
-                                Boolean tableOrder,
-                                Boolean tableFilter,
-                                Integer tableFilterComponent,
-                                Integer tableFilterType,
-                                Integer formComponent,
-                                Boolean formVisible,
-                                Boolean formRequired) {
+public record GenInfoExtendInfo(String requestName) {
 }
