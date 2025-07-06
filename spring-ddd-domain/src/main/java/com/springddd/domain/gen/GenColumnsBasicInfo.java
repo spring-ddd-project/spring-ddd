a4252ea2a35ed34611d8b0be84cb2744408ad88e
henrikabel25 <henrikabel@foxmail.com>
2025-07-06 17:14:12 +0800

    feat: add field to store aggregate root, value object, and entity in JSON format

--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenColumnsBasicInfo.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenColumnsBasicInfo.java
@@ -1,4 +1,4 @@
 package com.springddd.domain.gen;
 
-public record GenColumnsBasicInfo(PropValueObject valueObject, PropColumnKey key, PropColumnName name, PropColumnType type, PropColumnComment comment, PropJavaEntity entity) {
+public record GenColumnsBasicInfo(PropAggregate aggregate, PropColumnKey key, PropColumnName name, PropColumnType type, PropColumnComment comment, PropJavaEntity entity) {
 }
