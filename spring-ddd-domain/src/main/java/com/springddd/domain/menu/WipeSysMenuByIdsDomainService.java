1e1ca249fb7661cf9ac2895b0ffd0340efec4583
henrikabel25 <henrikabel@foxmail.com>
2025-07-14 16:50:07 +0800

    refactor: fix conflict between logical and physical deletion semantics

--- spring-ddd-domain/src/main/java/com/springddd/domain/menu/WipeSysMenuByIdsDomainService.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/WipeSysMenuByIdsDomainService.java
@@ -4,7 +4,7 @@ import reactor.core.publisher.Mono;
 
 import java.util.List;
 
-public interface DeleteSysMenuByIdsDomainService {
+public interface WipeSysMenuByIdsDomainService {
 
     Mono<Void> deleteByIds(List<Long> ids);
 }
