9ba7a90588e37863821343363838b748a16dd4ac
henrikabel25 <henrikabel@foxmail.com>
2025-07-17 18:05:44 +0800

    feat: added support for cascading physical deletion in system menus.

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/WipeSysMenuByIdsDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/WipeSysMenuByIdsDomainServiceImpl.java
@@ -1,10 +1,13 @@
 package com.springddd.application.service.menu;
 
+import com.springddd.application.service.menu.dto.SysMenuView;
 import com.springddd.domain.menu.WipeSysMenuByIdsDomainService;
+import com.springddd.domain.util.ReactiveTreeUtils;
 import com.springddd.infrastructure.persistence.r2dbc.SysMenuRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
@@ -15,9 +18,20 @@ public class WipeSysMenuByIdsDomainServiceImpl implements WipeSysMenuByIdsDomain
 
     private final SysMenuRepository sysMenuRepository;
 
+    private final SysMenuQueryService sysMenuQueryService;
+
     @Override
     @Transactional(rollbackFor = Exception.class)
     public Mono<Void> deleteByIds(List<Long> ids) {
-        return sysMenuRepository.deleteAllById(ids);
+        return sysMenuQueryService.queryAllMenu()
+                .flatMapMany(menus ->
+                        Flux.fromIterable(ids)
+                                .flatMap(id -> {
+                                    List<SysMenuView> allChildren = ReactiveTreeUtils.findAllChildrenFrom(
+                                            id, menus, SysMenuView::getId, SysMenuView::getParentId);
+                                    return Flux.fromIterable(allChildren)
+                                            .map(SysMenuView::getId);
+                                }).distinct())
+                .flatMap(sysMenuRepository::deleteById).then();
     }
 }
