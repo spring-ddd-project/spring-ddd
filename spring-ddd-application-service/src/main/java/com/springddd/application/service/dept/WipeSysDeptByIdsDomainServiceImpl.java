3a6b982b45f85af637797988d42218a2fd7de328
henrikabel25 <henrikabel@foxmail.com>
2025-07-17 20:46:05 +0800

    feat: implemented cascading logical and physical deletion for system departments.

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dept/WipeSysDeptByIdsDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dept/WipeSysDeptByIdsDomainServiceImpl.java
@@ -1,10 +1,13 @@
 package com.springddd.application.service.dept;
 
+import com.springddd.application.service.dept.dto.SysDeptView;
 import com.springddd.domain.dept.WipeSysDeptByIdsDomainService;
+import com.springddd.domain.util.ReactiveTreeUtils;
 import com.springddd.infrastructure.persistence.r2dbc.SysDeptRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
@@ -15,9 +18,18 @@ public class WipeSysDeptByIdsDomainServiceImpl implements WipeSysDeptByIdsDomain
 
     private final SysDeptRepository sysDeptRepository;
 
+    private final SysDeptQueryService sysDeptQueryService;
+
     @Override
     @Transactional(rollbackFor = Exception.class)
     public Mono<Void> deleteByIds(List<Long> ids) {
-        return sysDeptRepository.deleteAllById(ids);
+        return sysDeptQueryService.queryAllDept()
+                .flatMapMany(ds -> Flux.fromIterable(ids)
+                        .flatMap(id -> {
+                            List<SysDeptView> allChildren = ReactiveTreeUtils.findAllChildrenFrom(id, ds, SysDeptView::getId, SysDeptView::getParentId);
+                            return Flux.fromIterable(allChildren)
+                                    .map(SysDeptView::getId);
+                        }).distinct())
+                .flatMap(sysDeptRepository::deleteById).then();
     }
 }
