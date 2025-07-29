e62714a37494ee236e53d65de951c684424b849d
henrikabel25 <henrikabel@foxmail.com>
2025-07-29 11:48:11 +0800

    fix: refactor code generation for table information service

--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenInfoDomain.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenInfoDomain.java
@@ -1,10 +1,8 @@
 package com.springddd.domain.gen;
 
 import com.springddd.domain.AbstractDomainMask;
-import com.springddd.domain.gen.exception.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import org.springframework.util.ObjectUtils;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
@@ -17,65 +15,14 @@ public class GenInfoDomain extends AbstractDomainMask {
     private GenInfoExtendInfo extendInfo;
 
     public void create(GenInfoExtendInfo extendInfo) {
-        check(extendInfo);
     }
 
     public void update(GenInfoBasicInfo basicInfo, GenInfoExtendInfo extendInfo) {
         this.basicInfo = basicInfo;
-        check(extendInfo);
         this.extendInfo = extendInfo;
     }
 
     public void delete() {
         super.setDeleteStatus(true);
-    }
-
-    private void check(GenInfoExtendInfo info) {
-        if (ObjectUtils.isEmpty(info.propValueObject())) {
-            throw new ValueObjectNullException();
-        }
-        if (ObjectUtils.isEmpty(info.propColumnKey())) {
-            throw new ColumnKeyNullException();
-        }
-        if (ObjectUtils.isEmpty(info.propColumnName())) {
-            throw new ColumnNameNullException();
-        }
-        if (ObjectUtils.isEmpty(info.propColumnType())) {
-            throw new ColumnTypeNullException();
-        }
-        if (ObjectUtils.isEmpty(info.propColumnComment())) {
-            throw new ColumnCommentNullException();
-        }
-        if (ObjectUtils.isEmpty(info.propJavaEntity())) {
-            throw new JavaEntityNullException();
-        }
-        if (ObjectUtils.isEmpty(info.propJavaType())) {
-            throw new JavaTypeNullException();
-        }
-
-        if (ObjectUtils.isEmpty(info.tableVisible())) {
-            throw new VisibleNullException();
-        }
-        if (ObjectUtils.isEmpty(info.tableOrder())) {
-            throw new OrderNullException();
-        }
-        if (ObjectUtils.isEmpty(info.tableFilter())) {
-            throw new FilterNullException();
-        }
-        if (ObjectUtils.isEmpty(info.tableFilterComponent())) {
-            throw new FilterComponentNullException();
-        }
-        if (ObjectUtils.isEmpty(info.tableFilterType())) {
-            throw new FilterTypeNullException();
-        }
-        if (ObjectUtils.isEmpty(info.formComponent())) {
-            throw new FormComponentNullException();
-        }
-        if (ObjectUtils.isEmpty(info.formVisible())) {
-            throw new VisibleNullException();
-        }
-        if (ObjectUtils.isEmpty(info.formRequired())) {
-            throw new FormRequiredNullException();
-        }
     }
 }
