bc248041941e3b84b14cb620a3ce9f66bb7d9745
henrikabel25 <henrikabel@foxmail.com>
2025-07-30 21:25:20 +0800

    fix: Fix issue with storing table information in code generation.

--- spring-ddd-infrastructure-persistence/src/main/java/com/springddd/infrastructure/persistence/entity/GenInfoEntity.java
+++ spring-ddd-infrastructure-persistence/src/main/java/com/springddd/infrastructure/persistence/entity/GenInfoEntity.java
@@ -23,38 +23,6 @@ public class GenInfoEntity {
 
     private String requestName;
 
-    private Boolean propValueObject;
-
-    private String propColumnKey;
-
-    private String propColumnName;
-
-    private String propColumnType;
-
-    private String propColumnComment;
-
-    private String propJavaEntity;
-
-    private String propJavaType;
-
-    private Long propDictId;
-
-    private Boolean tableVisible;
-
-    private Boolean tableOrder;
-
-    private Boolean tableFilter;
-
-    private Integer tableFilterComponent;
-
-    private Integer tableFilterType;
-
-    private Integer formComponent;
-
-    private Boolean formVisible;
-
-    private Boolean formRequired;
-
     private Boolean deleteStatus;
 
     @CreatedBy
