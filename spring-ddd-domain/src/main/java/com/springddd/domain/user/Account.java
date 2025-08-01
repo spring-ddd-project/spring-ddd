30c2095083baed7fcd329fe71c7993b2d7bfd2ef
henrikabel25 <henrikabel@foxmail.com>
2025-08-01 13:10:54 +0800

    feat: Add User domain

---
+++ spring-ddd-domain/src/main/java/com/springddd/domain/user/Account.java
@@ -0,0 +1,17 @@
+package com.springddd.domain.user;
+
+import lombok.Data;
+
+@Data
+public class Account {
+
+    private Username username;
+
+    private Password password;
+
+    private String email;
+
+    private String phone;
+
+
+}
