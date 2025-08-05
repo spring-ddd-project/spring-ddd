df1da424d091182cf1b3662ac4bb3bc60a625246
henrikabel25 <henrikabel@foxmail.com>
2025-08-05 21:02:30 +0800

    fix: display the primary key name directly in the field, instead of showing the boolean value

--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/PropColumnKey.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/PropColumnKey.java
@@ -3,7 +3,7 @@ package com.springddd.domain.gen;
 import com.springddd.domain.gen.exception.ColumnKeyNullException;
 import org.springframework.util.ObjectUtils;
 
-public record PropColumnKey(Boolean value) {
+public record PropColumnKey(String value) {
 
     public PropColumnKey {
         if (ObjectUtils.isEmpty(value)) {
