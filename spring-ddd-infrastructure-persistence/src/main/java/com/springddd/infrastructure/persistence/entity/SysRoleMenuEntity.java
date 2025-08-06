1d268f019f92ef7552a412400b18b0dcc7508b43
henrikabel25 <henrikabel@foxmail.com>
2025-08-06 10:34:37 +0800

    feat: Add a data model to associate system roles with menu items

---
+++ spring-ddd-infrastructure-persistence/src/main/java/com/springddd/infrastructure/persistence/entity/SysRoleMenuEntity.java
@@ -0,0 +1,40 @@
+package com.springddd.infrastructure.persistence.entity;
+
+import com.springddd.domain.util.IdGenerate;
+import lombok.Data;
+import org.springframework.data.annotation.*;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDateTime;
+
+@Data
+@Table("sys_role_menu")
+public class SysRoleMenuEntity {
+
+    @Id
+    @IdGenerate
+    private Long id;
+
+    private Long roleId;
+
+    private Long menuId;
+
+    private Long deptId;
+
+    private String deleteStatus;
+
+    @CreatedBy
+    private String createBy;
+
+    @CreatedDate
+    private LocalDateTime createTime;
+
+    @LastModifiedBy
+    private String updateBy;
+
+    @LastModifiedDate
+    private LocalDateTime updateTime;
+
+    @Version
+    private String version;
+}
