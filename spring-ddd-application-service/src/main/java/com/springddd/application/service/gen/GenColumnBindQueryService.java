3a29d40a71ceba4044867fcfa7ed6d5b9412847d
henrikabel25 <henrikabel@foxmail.com>
2025-08-08 19:01:45 +0800

    feat: add field binding recycle service

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenColumnBindQueryService.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenColumnBindQueryService.java
@@ -1,7 +1,6 @@
 package com.springddd.application.service.gen;
 
 import com.springddd.application.service.gen.dto.GenColumnBindPageQuery;
-import com.springddd.application.service.gen.dto.GenColumnBindQuery;
 import com.springddd.application.service.gen.dto.GenColumnBindView;
 import com.springddd.application.service.gen.dto.GenColumnBindViewMapStruct;
 import com.springddd.domain.util.PageResponse;
@@ -25,6 +24,16 @@ public class GenColumnBindQueryService {
 
     public Mono<PageResponse<GenColumnBindView>> index(GenColumnBindPageQuery query) {
         Criteria criteria = Criteria.where(GenColumnBindPageQuery.Fields.deleteStatus).is(false);
+        Query qry = Query.query(criteria)
+                .limit(query.getPageSize())
+                .offset((long) (query.getPageNum() - 1) * query.getPageSize());
+        Mono<List<GenColumnBindView>> list = r2dbcEntityTemplate.select(GenColumnBindEntity.class).matching(qry).all().collectList().map(genColumnBindViewMapStruct::toViews);
+        Mono<Long> count = r2dbcEntityTemplate.count(Query.query(criteria), GenColumnBindEntity.class);
+        return Mono.zip(list, count).map(tuple -> new PageResponse<>(tuple.getT1(), tuple.getT2(), query.getPageNum(), query.getPageSize()));
+    }
+
+    public Mono<PageResponse<GenColumnBindView>> recycle(GenColumnBindPageQuery query) {
+        Criteria criteria = Criteria.where(GenColumnBindPageQuery.Fields.deleteStatus).is(true);
         Query qry = Query.query(criteria)
                 .limit(query.getPageSize())
                 .offset((long) (query.getPageNum() - 1) * query.getPageSize());
