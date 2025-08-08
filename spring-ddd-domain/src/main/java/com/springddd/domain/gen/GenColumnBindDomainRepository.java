ab43e1cb1fc92b483cd93cd8a39c3d8f3e8edd4c
henrikabel25 <henrikabel@foxmail.com>
2025-08-08 15:42:17 +0800

    feat: improve field binding infrastructure layer services

---
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenColumnBindDomainRepository.java
@@ -0,0 +1,6 @@
+package com.springddd.domain.gen;
+
+import com.springddd.domain.DomainRepository;
+
+public interface GenColumnBindDomainRepository extends DomainRepository<ColumnBindId, GenColumnBindDomain> {
+}
