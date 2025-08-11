a8a1c68daca1623a3bbfd841ff3dcad49e200be8
henrikabel25 <henrikabel@foxmail.com>
2025-08-11 14:03:09 +0800

    feat: implement database and codegen table synchronization

--- spring-ddd-interface-web/src/main/java/com/springddd/web/GenTableController.java
+++ spring-ddd-interface-web/src/main/java/com/springddd/web/GenTableController.java
@@ -1,5 +1,6 @@
 package com.springddd.web;
 
+import com.springddd.application.service.gen.GenTableInfoCommandService;
 import com.springddd.application.service.gen.GenTableInfoQueryService;
 import com.springddd.application.service.gen.dto.GenTableInfoPageQuery;
 import com.springddd.domain.util.ApiResponse;
@@ -8,7 +9,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
-
 @RestController
 @RequestMapping("/gen/table")
 @RequiredArgsConstructor
@@ -16,8 +16,16 @@ public class GenTableController {
 
     private final GenTableInfoQueryService genTableInfoQueryService;
 
+    private final GenTableInfoCommandService genTableInfoCommandService;
+
     @PostMapping("/index")
     public Mono<ApiResponse> tableIndex(@RequestBody @Validated Mono<GenTableInfoPageQuery> query) {
         return ApiResponse.validated(query, genTableInfoQueryService::index);
     }
+
+    @DeleteMapping("/wipe")
+    public Mono<ApiResponse> wipe() {
+        return ApiResponse.ok(genTableInfoCommandService.wipe());
+    }
+
 }
