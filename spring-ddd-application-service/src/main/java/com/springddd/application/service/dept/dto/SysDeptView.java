76c83d09fbb98cc37e9f96bb200e39833dc02a38
henrikabel25 <henrikabel@foxmail.com>
2025-08-14 15:38:51 +0800

    feat: Implement a query-only service for retrieving department data

---
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dept/dto/SysDeptView.java
@@ -0,0 +1,22 @@
+package com.springddd.application.service.dept.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class SysDeptView implements Serializable {
+
+    private Long id;
+
+    private Long parentId;
+
+    private String deptName;
+
+    private Integer sortOrder;
+
+    private Boolean deptStatus;
+
+    private Boolean deleteStatus;
+
+}
