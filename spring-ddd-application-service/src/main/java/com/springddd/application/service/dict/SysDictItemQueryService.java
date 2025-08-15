c75178e9e9052432e369bdc2cee9dc351d851ecc
henrikabel25 <henrikabel@foxmail.com>
2025-08-15 16:33:57 +0800

    feat: add data dictionary value conversion service

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/SysDictItemQueryService.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/SysDictItemQueryService.java
@@ -27,7 +27,7 @@ public class SysDictItemQueryService {
         Criteria criteria = Criteria
                 .where("delete_status").is(false);
         if (!ObjectUtils.isEmpty(query) && !ObjectUtils.isEmpty(query.getDictId())) {
-            criteria.and("dict_id").is(query.getDictId());
+            criteria = criteria.and("dict_id").is(query.getDictId());
         }
         Query qry = Query.query(criteria)
                 .limit(query.getPageSize())
@@ -35,5 +35,14 @@ public class SysDictItemQueryService {
         Mono<List<SysDictItemView>> list = r2dbcEntityTemplate.select(SysDictItemEntity.class).matching(qry).all().collectList().map(sysDictItemViewMapStruct::toViews);
         Mono<Long> count = r2dbcEntityTemplate.count(Query.query(criteria), SysDictItemEntity.class);
         return Mono.zip(list, count).map(tuple -> new PageResponse<>(tuple.getT1(), tuple.getT2(), query.getPageNum(), query.getPageSize()));
+    }
+
+    public Mono<SysDictItemView> queryItemLabelByItemValueAndDictId(Long dictId, Integer itemValue) {
+        Criteria criteria = Criteria
+                .where("delete_status").is(false)
+                .and("dict_id").is(dictId)
+                .and("item_value").is(itemValue);
+        Query qry = Query.query(criteria);
+        return r2dbcEntityTemplate.select(SysDictItemEntity.class).matching(qry).one().map(sysDictItemViewMapStruct::toView);
     }
 }
