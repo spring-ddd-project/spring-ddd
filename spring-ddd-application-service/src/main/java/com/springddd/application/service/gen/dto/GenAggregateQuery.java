59110445cc4d5a20acfc5b43a019aad4bae519f5
henrikabel25 <henrikabel@foxmail.com>
2025-08-17 16:30:42 +0800

    feat: add domain infrastructure layer for aggregate roots in code generation

---
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenAggregateQuery.java
@@ -0,0 +1,19 @@
+package com.springddd.application.service.gen.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class GenAggregateQuery implements Serializable {
+
+    private Long id;
+
+    private Long infoId;
+
+    private String aggregate;
+
+    private String valueObject;
+
+    private String domainMask;
+}
