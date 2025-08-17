6e8d0aa4557983709b61ed2e5e1053d4d12784a1
henrikabel25 <henrikabel@foxmail.com>
2025-08-17 16:52:54 +0800

    feat: add Query service for aggregate root in codegen

---
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenAggregateQueryService.java
@@ -0,0 +1,41 @@
+package com.springddd.application.service.gen;
+
+
+import com.springddd.application.service.gen.dto.GenAggregatePageQuery;
+import com.springddd.application.service.gen.dto.GenAggregateQuery;
+import com.springddd.application.service.gen.dto.GenAggregateView;
+import com.springddd.application.service.gen.dto.GenAggregateViewMapStruct;
+import com.springddd.domain.util.PageResponse;
+import com.springddd.infrastructure.persistence.entity.GenAggregateEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
+import org.springframework.data.relational.core.query.Criteria;
+import org.springframework.data.relational.core.query.Query;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class GenAggregateQueryService {
+
+    private final R2dbcEntityTemplate r2dbcEntityTemplate;
+
+    private final GenAggregateViewMapStruct aggregateViewMapStruct;
+
+    public Mono<PageResponse<GenAggregateView>> index(GenAggregatePageQuery query) {
+        if (ObjectUtils.isEmpty(query.getInfoId())) {
+            return Mono.empty();
+        }
+        Criteria criteria = Criteria.where(GenAggregateQuery.Fields.infoId).is(query.getInfoId());
+        Query qry = Query.query(criteria)
+                .limit(query.getPageSize())
+                .offset((long) (query.getPageNum() - 1) * query.getPageSize());
+
+        Mono<List<GenAggregateView>> list = r2dbcEntityTemplate.select(GenAggregateEntity.class).matching(qry).all().collectList().map(aggregateViewMapStruct::toViews);
+        Mono<Long> count = r2dbcEntityTemplate.count(Query.query(criteria), GenAggregateEntity.class);
+        return Mono.zip(list, count).map(tuple -> new PageResponse<>(tuple.getT1(), tuple.getT2(), query.getPageNum(), query.getPageSize()));
+    }
+}
