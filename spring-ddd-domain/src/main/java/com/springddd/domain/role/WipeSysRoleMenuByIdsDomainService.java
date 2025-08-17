96cce4a30bbe07ba05cd1fdc92ec9a418bb4d7ab
henrikabel25 <henrikabel@foxmail.com>
2025-08-17 20:28:36 +0800

    refactor: resolve the semantic conflicts between logical deletion and physical deletion in the system roles and menus.

--- spring-ddd-domain/src/main/java/com/springddd/domain/role/WipeSysRoleMenuByIdsDomainService.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/role/WipeSysRoleMenuByIdsDomainService.java
@@ -4,7 +4,7 @@ import reactor.core.publisher.Mono;
 
 import java.util.List;
 
-public interface DeleteSysRoleMenuByIdsDomainService {
+public interface WipeSysRoleMenuByIdsDomainService {
 
     Mono<Void> deleteByIds(List<Long> ids);
 }
