15ade0f53dd90a099d28b7abf2bdc9f8239f1ee6
henrikabel25 <henrikabel@foxmail.com>
2025-08-27 17:28:59 +0800

    feat: add query service for codegen template

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenTemplateQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenTemplateQuery.java
@@ -1,10 +1,12 @@
 package com.springddd.application.service.gen.dto;
 
 import lombok.Data;
+import lombok.experimental.FieldNameConstants;
 
 import java.io.Serializable;
 
 @Data
+@FieldNameConstants
 public class GenTemplateQuery implements Serializable {
 
     private Long id;
