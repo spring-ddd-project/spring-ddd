0ce95b09c13240eafa764c0d861eb6db1ccbb74b
henrikabel25 <henrikabel@foxmail.com>
2025-09-12 18:40:49 +0800

    feat: decouple the directory from the button in the advanced config options

--- spring-ddd-domain/src/main/java/com/springddd/domain/menu/AdvancedOptions.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/AdvancedOptions.java
@@ -1,4 +1,16 @@
 package com.springddd.domain.menu;
 
 public record AdvancedOptions(Integer order, String icon, Integer menuType, Boolean visible, Boolean menuStatus) {
+
+    // Catalog
+    public AdvancedOptions(Integer order, Integer menuType, String icon, Boolean MenuStatus, Boolean visible) {
+        this(order, icon, menuType, visible, MenuStatus);
+    }
+
+    // Menu default
+
+    // Button
+    public AdvancedOptions(Integer order, Integer menuType, Boolean MenuStatus) {
+        this(order, null, menuType, null, MenuStatus);
+    }
 }
