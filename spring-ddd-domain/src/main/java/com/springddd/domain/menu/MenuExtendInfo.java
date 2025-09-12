0b6a64276c4811c2e772fb6a01584aaa7c69af22
henrikabel25 <henrikabel@foxmail.com>
2025-09-12 14:10:44 +0800

    feat: replace menu extension attributes and merge them into the Menu value object

--- spring-ddd-domain/src/main/java/com/springddd/domain/menu/MenuExtendInfo.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/MenuExtendInfo.java
@@ -1,17 +1,16 @@
 package com.springddd.domain.menu;
 
-public record MenuExtendInfo(Integer order, String title, Boolean affixTab, Boolean noBasicLayout, String icon,
-                             Integer menuType, Boolean visible, Boolean embedded, Boolean menuStatus) {
+public record MenuExtendInfo(Integer order, String title, String icon, Integer menuType, Boolean visible, Boolean menuStatus) {
 
     // Catalog
     public MenuExtendInfo(Integer order, String title, Integer menuType, String icon, Boolean MenuStatus, Boolean visible) {
-        this(order, title, null, null, icon, menuType, visible, null, MenuStatus);
+        this(order, title, icon, menuType, visible, MenuStatus);
     }
 
     // Menu default
 
     // Button
     public MenuExtendInfo(Integer order, Integer menuType, Boolean MenuStatus) {
-        this(order, null, null, null, null, menuType, null, null, MenuStatus);
+        this(order, null, null, menuType, null, MenuStatus);
     }
 }
