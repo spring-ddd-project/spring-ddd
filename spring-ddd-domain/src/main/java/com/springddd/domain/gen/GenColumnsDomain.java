6d0d27fc6fb567461b66d7d2a7c6861c9ba9a828
henrikabel25 <henrikabel@foxmail.com>
2025-09-13 16:56:09 +0800

    feat: add i18n configuration for field attributes

--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenColumnsDomain.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenColumnsDomain.java
@@ -18,14 +18,17 @@ public class GenColumnsDomain extends AbstractDomainMask {
 
     private Form form;
 
+    private I18n i18n;
+
     private GenColumnsExtendInfo extendInfo;
 
     public void create() {}
 
-    public void update(Prop prop, Table table, Form form, GenColumnsExtendInfo extendInfo) {
+    public void update(Prop prop, Table table, Form form, I18n i18n, GenColumnsExtendInfo extendInfo) {
         this.prop = prop;
         this.table = table;
         this.form = form;
+        this.i18n = i18n;
         this.extendInfo = extendInfo;
     }
 
