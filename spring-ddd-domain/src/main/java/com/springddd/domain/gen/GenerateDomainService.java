547645d64591ff0983b47a730d726533fc49107a
henrikabel25 <henrikabel@foxmail.com>
2025-09-19 16:17:06 +0800

    pref: dynamically load project and module names into template

--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenerateDomainService.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenerateDomainService.java
@@ -4,5 +4,5 @@ import reactor.core.publisher.Mono;
 
 public interface GenerateDomainService {
 
-    Mono<Void> generate(String tableName, String projectName);
+    Mono<Void> generate(String tableName);
 }
