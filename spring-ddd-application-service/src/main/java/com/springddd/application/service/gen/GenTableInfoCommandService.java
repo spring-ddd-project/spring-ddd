0b003e319514f5b5da7413732cef5d26401feb15
henrikabel25 <henrikabel@foxmail.com>
2025-09-26 20:36:39 +0800

    feat: add project structure download API

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenTableInfoCommandService.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenTableInfoCommandService.java
@@ -1,8 +1,11 @@
 package com.springddd.application.service.gen;
 
+import com.springddd.domain.gen.GenDownloadDomainService;
 import com.springddd.domain.gen.GenerateDomainService;
 import com.springddd.domain.gen.WipeGenDataDomainService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
@@ -14,11 +17,17 @@ public class GenTableInfoCommandService {
 
     private final GenerateDomainService generateDomainService;
 
+    private final GenDownloadDomainService genDownloadDomainService;
+
     public Mono<Void> wipe() {
         return wipeGenDataDomainService.wipe();
     }
 
     public Mono<Void> generate(String tableName) {
         return generateDomainService.generate(tableName);
+    }
+
+    public Mono<ResponseEntity<Resource>> download() {
+        return genDownloadDomainService.download();
     }
 }
