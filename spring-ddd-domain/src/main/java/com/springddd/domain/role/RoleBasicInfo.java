6977b58c79450711400ccff351f2de47bdb5e69e
henrikabel25 <henrikabel@foxmail.com>
2025-10-01 21:57:41 +0800

    pref: optimize system role value objects for lower memory usage

--- spring-ddd-domain/src/main/java/com/springddd/domain/role/RoleBasicInfo.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/role/RoleBasicInfo.java
@@ -1,4 +1,21 @@
 package com.springddd.domain.role;
 
-public record RoleBasicInfo(RoleName roleName, RoleCode roleCode, RoleDataScope roleDataScope, RoleOwner roleOwner) {
+import com.springddd.domain.role.exception.RoleCodeNullException;
+import com.springddd.domain.role.exception.RoleDataScopeNullException;
+import com.springddd.domain.role.exception.RoleNameNullException;
+import org.springframework.util.ObjectUtils;
+
+public record RoleBasicInfo(String roleName, String roleCode, Integer roleDataScope, Boolean roleOwner) {
+
+    public RoleBasicInfo {
+        if (ObjectUtils.isEmpty(roleName)) {
+            throw new RoleNameNullException();
+        }
+        if (ObjectUtils.isEmpty(roleCode)) {
+            throw new RoleCodeNullException();
+        }
+        if (ObjectUtils.isEmpty(roleDataScope)) {
+            throw new RoleDataScopeNullException();
+        }
+    }
 }
