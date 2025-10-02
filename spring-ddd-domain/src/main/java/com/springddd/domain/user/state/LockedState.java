a8fffe71f94977b6a12cb262e3ee81658d8c2f5f
henrikabel25 <henrikabel@foxmail.com>
2025-10-02 17:23:29 +0800

    feat: implement state pattern for user account status

---
+++ spring-ddd-domain/src/main/java/com/springddd/domain/user/state/LockedState.java
@@ -0,0 +1,16 @@
+package com.springddd.domain.user.state;
+
+import com.springddd.domain.user.SysUserDomain;
+
+public class LockedState implements UserState {
+    @Override
+    public void lock(SysUserDomain domain) {
+        // Already locked
+    }
+
+    @Override
+    public void unlock(SysUserDomain domain) {
+        domain.getAccount().setLockStatus(false);
+        domain.setState(new NormalState());
+    }
+}
