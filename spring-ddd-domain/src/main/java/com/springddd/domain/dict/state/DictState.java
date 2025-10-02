056c198ec424154d1a6a4e2eea23e1f112d7ff21
henrikabel25 <henrikabel@foxmail.com>
2025-10-02 20:39:17 +0800

    feat: implement state pattern for dictionary status management

---
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dict/state/DictState.java
@@ -0,0 +1,8 @@
+package com.springddd.domain.dict.state;
+
+import com.springddd.domain.dict.SysDictDomain;
+
+public interface DictState {
+    void delete(SysDictDomain domain);
+    void restore(SysDictDomain domain);
+}
