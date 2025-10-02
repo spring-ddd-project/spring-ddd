a2d469809f9e28953e59c1b76fb35e0cf9cdead2
henrikabel25 <henrikabel@foxmail.com>
2025-10-02 20:10:36 +0800

    feat: implement chain of responsibility for authentication providers

---
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/handler/AuthHandler.java
@@ -0,0 +1,15 @@
+package com.springddd.application.service.auth.handler;
+
+import com.springddd.application.service.auth.dto.LoginUserQuery;
+import com.springddd.domain.auth.AuthUser;
+import reactor.core.publisher.Mono;
+
+public abstract class AuthHandler {
+    protected AuthHandler next;
+
+    public void setNext(AuthHandler next) {
+        this.next = next;
+    }
+
+    public abstract Mono<AuthUser> handle(LoginUserQuery query);
+}
