fd88a19a3ffd2d7ffd9adba29d9c868760836ee1
henrikabel25 <henrikabel@foxmail.com>
2025-10-03 14:10:23 +0800

    feat: implement iterator pattern for query result pagination

--- spring-ddd-domain/src/main/java/com/springddd/domain/util/PageResponse.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/util/PageResponse.java
@@ -3,22 +3,30 @@ package com.springddd.domain.util;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Iterator;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
-public class PageResponse<T> {
+public class PageResponse<T> implements java.io.Serializable, Iterable<T> {
 
-    private List<T> items;
-    private long total;
-    private int pageNum;
-    private int pageSize;
+    private List<T> list;
 
-    public PageResponse(List<T> items, long total, int pageNum, int pageSize) {
-        this.items = items;
+    private Long total;
+
+    private Integer pageNum;
+
+    private Integer pageSize;
+
+    public PageResponse(List<T> list, Long total, Integer pageNum, Integer pageSize) {
+        this.list = list;
         this.total = total;
         this.pageNum = pageNum;
         this.pageSize = pageSize;
     }
-}
 
+    @Override
+    public Iterator<T> iterator() {
+        return list.iterator();
+    }
+}
