e0c56877945882a1eb74e6cfcda95bfad97f8468
henrikabel25 <henrikabel@foxmail.com>
2025-10-03 17:10:54 +0800

    feat: implement state pattern for menu visibility

---
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/state/VisibleMenuState.java
@@ -0,0 +1,19 @@
+package com.springddd.domain.menu.state;
+
+import com.springddd.domain.menu.SysMenuDomain;
+
+public class VisibleMenuState implements MenuState {
+    @Override
+    public void show(SysMenuDomain domain) {
+        // Already visible
+    }
+
+    @Override
+    public void hide(SysMenuDomain domain) {
+        com.springddd.domain.menu.AdvancedOptions old = domain.getAdvancedOptions();
+        if (old != null) {
+            domain.setAdvancedOptions(new com.springddd.domain.menu.AdvancedOptions(old.order(), old.icon(), old.menuType(), false, old.menuStatus()));
+        }
+        domain.setState(new HiddenMenuState());
+    }
+}
