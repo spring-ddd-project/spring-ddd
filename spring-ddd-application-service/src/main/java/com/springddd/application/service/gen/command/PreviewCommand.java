9e410ed1d08825d353cb869b61393f6b678ddcf3
henrikabel25 <henrikabel@foxmail.com>
2025-10-04 17:10:14 +0800

    feat: implement command pattern for asynchronous code generation tasks

---
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/command/PreviewCommand.java
@@ -0,0 +1,19 @@
+package com.springddd.application.service.gen.command;
+
+import com.springddd.application.service.gen.GenerateDomainServiceImpl;
+import com.springddd.application.service.gen.dto.GenProjectInfoDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class PreviewCommand implements GenerateCommand {
+
+    private final GenerateDomainServiceImpl generateDomainService;
+
+    @Override
+    public Mono<Void> execute(GenProjectInfoDTO dto) {
+        return generateDomainService.generateProject(dto).then();
+    }
+}
