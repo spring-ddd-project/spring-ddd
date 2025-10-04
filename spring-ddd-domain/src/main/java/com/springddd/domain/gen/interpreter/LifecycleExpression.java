f4a0b9c82c15d6a26cef1f1e4e5f74ebf07d2b0b
henrikabel25 <henrikabel@foxmail.com>
2025-10-04 11:38:59 +0800

    feat: implement interpreter pattern for domain aggregate lifecycle rules

---
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/interpreter/LifecycleExpression.java
@@ -0,0 +1,7 @@
+package com.springddd.domain.gen.interpreter;
+
+import com.springddd.domain.gen.GenAggregateDomain;
+
+public interface LifecycleExpression {
+    boolean evaluate(GenAggregateDomain domain);
+}
