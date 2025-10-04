ba4b546ff35259953765e38b71d6b7e2c60d6eef
henrikabel25 <henrikabel@foxmail.com>
2025-10-04 20:21:52 +0800

    feat: implement factory method pattern for generate command instantiation

---
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/factory/PreviewCommandFactory.java
@@ -0,0 +1,18 @@
+package com.springddd.application.service.gen.factory;
+
+import com.springddd.application.service.gen.command.GenerateCommand;
+import com.springddd.application.service.gen.command.PreviewCommand;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PreviewCommandFactory implements GenerateCommandFactory {
+
+    private final PreviewCommand previewCommand;
+
+    @Override
+    public GenerateCommand createCommand() {
+        return previewCommand;
+    }
+}
