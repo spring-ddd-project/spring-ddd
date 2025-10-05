d9adffc78e1d7a994ca3d3ade535531aaf3bb4ba
henrikabel25 <henrikabel@foxmail.com>
2025-10-05 20:28:43 +0800

    feat: add security tests for gen

--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenColumnsExtendInfo.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenColumnsExtendInfo.java
@@ -3,3 +3,4 @@ package com.springddd.domain.gen;
 public record GenColumnsExtendInfo(Long propDictId, Byte typescriptType) {
 }
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenProjectInfoDomainRepository.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenProjectInfoDomainRepository.java
@@ -5,3 +5,4 @@ import com.springddd.domain.DomainRepository;
 public interface GenProjectInfoDomainRepository extends DomainRepository<InfoId, GenProjectInfoDomain> {
 }
 
+
