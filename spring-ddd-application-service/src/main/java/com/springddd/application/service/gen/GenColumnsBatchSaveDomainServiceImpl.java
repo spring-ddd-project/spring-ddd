a10de3f29d6ddde3de7bedfb41e5337cf15edf6e
henrikabel25 <henrikabel@foxmail.com>
2025-10-08 14:12:56 +0800

    refactor: enhance role type safety

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenColumnsBatchSaveDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenColumnsBatchSaveDomainServiceImpl.java
@@ -68,3 +68,4 @@ public class GenColumnsBatchSaveDomainServiceImpl implements GenColumnsBatchSave
     }
 }
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/dept/SysDeptDomainRepository.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dept/SysDeptDomainRepository.java
@@ -6,3 +6,4 @@ public interface SysDeptDomainRepository extends DomainRepository<DeptId, SysDep
 }
 
 
+
