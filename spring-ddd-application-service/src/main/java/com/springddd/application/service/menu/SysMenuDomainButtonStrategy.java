a1143b3becd0ffd18bba3cd97073c17fd062b6a5
henrikabel25 <henrikabel@foxmail.com>
2025-10-09 17:10:58 +0800

    feat: add security tests for util

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/SysMenuDomainButtonStrategy.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/SysMenuDomainButtonStrategy.java
@@ -24,3 +24,4 @@ public class SysMenuDomainButtonStrategy implements SysMenuDomainStrategy {
         return domain;
     }
 }
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/dict/state/EnabledDictItemState.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dict/state/EnabledDictItemState.java
@@ -21,3 +21,4 @@ public class EnabledDictItemState implements DictItemState {
 }
 
 
+
