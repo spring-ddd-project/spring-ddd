33764607016502a3aca794512b121f957dbee8f6
henrikabel25 <henrikabel@foxmail.com>
2025-10-13 19:33:56 +0800

    refactor: clean up cache dependencies

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/SysDictDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/SysDictDomainFactoryImpl.java
@@ -18,3 +18,4 @@ public class SysDictDomainFactoryImpl implements SysDictDomainFactory {
         return sysDictDomain;
     }
 }
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/UpdateCacheDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/UpdateCacheDomainServiceImpl.java
@@ -93,3 +93,4 @@ public class UpdateCacheDomainServiceImpl implements UpdateCacheDomainService {
 }
 
 
+
