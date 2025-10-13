609923d3111efe32a57cc6d926a555104f8d7295
henrikabel25 <henrikabel@foxmail.com>
2025-10-13 19:10:13 +0800

    refactor: simplify cache module structure

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/WipeSysDictByIdsDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/WipeSysDictByIdsDomainServiceImpl.java
@@ -48,3 +48,4 @@ public class WipeSysDictByIdsDomainServiceImpl implements WipeSysDictByIdsDomain
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/role/LinkRoleAndMenusDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/role/LinkRoleAndMenusDomainServiceImpl.java
@@ -48,3 +48,4 @@ public class LinkRoleAndMenusDomainServiceImpl implements LinkRoleAndMenusDomain
 
 
 
+
