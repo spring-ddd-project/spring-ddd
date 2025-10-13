dc7bae806ae5d4173a75d76dedefa1e2f3e66d4f
henrikabel25 <henrikabel@foxmail.com>
2025-10-13 13:25:46 +0800

    feat: add integration tests for persistence

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/AuthorizationManagerConfig.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/AuthorizationManagerConfig.java
@@ -71,3 +71,4 @@ public class AuthorizationManagerConfig implements ReactiveAuthorizationManager<
                 }));
     }
 }
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenTemplateDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenTemplateDomainFactoryImpl.java
@@ -23,3 +23,4 @@ public class GenTemplateDomainFactoryImpl implements GenTemplateDomainFactory {
 
 
 
+
