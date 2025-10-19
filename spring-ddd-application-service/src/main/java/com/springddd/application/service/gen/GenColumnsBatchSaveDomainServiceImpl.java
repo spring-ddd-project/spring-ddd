aa66e52ad25faa4f861149584967a7483e86cbf3
henrikabel25 <henrikabel@foxmail.com>
2025-10-19 21:13:17 +0800

    refactor: clean up dict dependencies

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenColumnsBatchSaveDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenColumnsBatchSaveDomainServiceImpl.java
@@ -79,3 +79,4 @@ public class GenColumnsBatchSaveDomainServiceImpl implements GenColumnsBatchSave
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenTemplatePageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenTemplatePageQuery.java
@@ -20,3 +20,4 @@ public class GenTemplatePageQuery extends GenTemplateQuery implements Serializab
 
 
 
+
