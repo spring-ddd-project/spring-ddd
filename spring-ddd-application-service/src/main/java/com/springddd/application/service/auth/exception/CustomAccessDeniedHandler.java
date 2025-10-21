60076101f5f37fa87879b738e9e7ea83f2bf28bb
henrikabel25 <henrikabel@foxmail.com>
2025-10-21 12:31:53 +0800

    refactor: enhance leaf type safety

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/exception/CustomAccessDeniedHandler.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/exception/CustomAccessDeniedHandler.java
@@ -46,3 +46,4 @@ public class CustomAccessDeniedHandler implements ServerAccessDeniedHandler {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/role/RoleScopeConfig.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/role/RoleScopeConfig.java
@@ -8,3 +8,4 @@ public record RoleScopeConfig(List<Long> depts, List<Long> posts, List<Long> use
 
 
 
+
