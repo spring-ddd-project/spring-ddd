5cbf45090eab986f1af1d8acbe1a86dc858e3eeb
henrikabel25 <henrikabel@foxmail.com>
2025-10-28 19:19:58 +0800

    refactor: consolidate dict utilities

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/AuthorizationManagerConfig.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/AuthorizationManagerConfig.java
@@ -77,3 +77,4 @@ public class AuthorizationManagerConfig implements ReactiveAuthorizationManager<
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/user/WipeSysUserByIdsDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/user/WipeSysUserByIdsDomainServiceImpl.java
@@ -57,3 +57,4 @@ public class WipeSysUserByIdsDomainServiceImpl implements WipeSysUserByIdsDomain
 
 
 
+
