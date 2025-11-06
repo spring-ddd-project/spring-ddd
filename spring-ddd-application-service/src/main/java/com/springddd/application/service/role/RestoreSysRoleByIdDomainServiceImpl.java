2ec0ec47bbc804edbe4c45ff9853457188811473
henrikabel25 <henrikabel@foxmail.com>
2025-11-06 20:52:33 +0800

    feat: add repository tests for dict

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/role/RestoreSysRoleByIdDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/role/RestoreSysRoleByIdDomainServiceImpl.java
@@ -39,3 +39,4 @@ public class RestoreSysRoleByIdDomainServiceImpl implements RestoreSysRoleByIdDo
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/dict/exception/DictItemItemStatusNullException.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dict/exception/DictItemItemStatusNullException.java
@@ -18,3 +18,4 @@ public class DictItemItemStatusNullException extends DomainException {
 
 
 
+
