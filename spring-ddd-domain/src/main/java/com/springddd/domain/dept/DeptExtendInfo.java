f92e742b2d0369fce42fef974a9b0e84559a658c
henrikabel25 <henrikabel@foxmail.com>
2025-11-08 20:27:19 +0800

    refactor: extract common logic in gen

--- spring-ddd-domain/src/main/java/com/springddd/domain/dept/DeptExtendInfo.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dept/DeptExtendInfo.java
@@ -29,3 +29,4 @@ public record DeptExtendInfo(Integer sortOrder, Boolean deptStatus) {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/dict/SysDictItemDomainRepository.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dict/SysDictItemDomainRepository.java
@@ -14,3 +14,4 @@ public interface SysDictItemDomainRepository extends DomainRepository<DictItemId
 
 
 
+
