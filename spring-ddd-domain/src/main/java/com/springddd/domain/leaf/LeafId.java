2061960d22f3919ba2bb832f6278c232b3b44780
henrikabel25 <henrikabel@foxmail.com>
2025-11-10 14:45:25 +0800

    feat: add domain logic tests for leaf

--- spring-ddd-domain/src/main/java/com/springddd/domain/leaf/LeafId.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/leaf/LeafId.java
@@ -12,3 +12,4 @@ public record LeafId(Long value) implements AggregateRootId<Long> {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/menu/Menu.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/Menu.java
@@ -27,3 +27,4 @@ public record Menu(String menuPath, String component, Boolean affixTab, Boolean 
 
 
 
+
