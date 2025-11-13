0c7f3b6af52bbdb0fbf95064c988e4783587d3b0
henrikabel25 <henrikabel@foxmail.com>
2025-11-13 14:23:58 +0800

    refactor: improve user error handling

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/SysMenuDomainCatalogStrategy.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/SysMenuDomainCatalogStrategy.java
@@ -48,3 +48,4 @@ public class SysMenuDomainCatalogStrategy implements SysMenuDomainStrategy {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/role/RoleBasicInfo.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/role/RoleBasicInfo.java
@@ -33,3 +33,4 @@ public record RoleBasicInfo(String roleName, String roleCode, Integer roleDataSc
 
 
 
+
