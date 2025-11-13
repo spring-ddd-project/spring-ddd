597a47b3b03b6d92cd0cce2fa85e04c5ded0ce54
henrikabel25 <henrikabel@foxmail.com>
2025-11-13 19:18:56 +0800

    refactor: extract common logic in user

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dept/DeleteSysDeptByIdDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dept/DeleteSysDeptByIdDomainServiceImpl.java
@@ -45,3 +45,4 @@ public class DeleteSysDeptByIdDomainServiceImpl implements DeleteSysDeptByIdDoma
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/WipeGenTemplateDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/WipeGenTemplateDomainServiceImpl.java
@@ -47,3 +47,4 @@ public class WipeGenTemplateDomainServiceImpl implements WipeGenTemplateDomainSe
 
 
 
+
