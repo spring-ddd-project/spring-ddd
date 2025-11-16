b02a58d8984547ab175a7b0d3a17b82d8e7180dd
henrikabel25 <henrikabel@foxmail.com>
2025-11-16 17:59:32 +0800

    feat: add domain logic tests for application

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenColumnBindDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenColumnBindDomainFactoryImpl.java
@@ -42,3 +42,4 @@ public class GenColumnBindDomainFactoryImpl implements GenColumnBindDomainFactor
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/exception/TypeScriptTypeNullException.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/exception/TypeScriptTypeNullException.java
@@ -22,3 +22,4 @@ public class TypeScriptTypeNullException extends DomainException {
 
 
 
+
