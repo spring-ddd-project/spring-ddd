40db8f8b94089db2be2f5bdcfa48175a854b3115
henrikabel25 <henrikabel@foxmail.com>
2025-11-17 11:10:35 +0800

    feat: add repository tests for application

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenColumnsBatchSaveDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenColumnsBatchSaveDomainServiceImpl.java
@@ -88,3 +88,4 @@ public class GenColumnsBatchSaveDomainServiceImpl implements GenColumnsBatchSave
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenColumnBindDomainRepository.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenColumnBindDomainRepository.java
@@ -20,3 +20,4 @@ public interface GenColumnBindDomainRepository extends DomainRepository<ColumnBi
 
 
 
+
