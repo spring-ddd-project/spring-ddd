4dae0327a2d189024c7e27b22a687b281dbb7040
henrikabel25 <henrikabel@foxmail.com>
2025-11-18 21:25:49 +0800

    refactor: consolidate user utilities

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/WipeGenDataDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/WipeGenDataDomainServiceImpl.java
@@ -26,3 +26,4 @@ public class WipeGenDataDomainServiceImpl implements WipeGenDataDomainService {
         ).then();
     }
 }
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/role/exception/RoleIdNullException.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/role/exception/RoleIdNullException.java
@@ -11,3 +11,4 @@ public class RoleIdNullException extends DomainException {
 }
 
 
+
