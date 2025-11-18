4173f5178e48440411d984127f2486c97a16451b
henrikabel25 <henrikabel@foxmail.com>
2025-11-18 09:30:15 +0800

    feat: implement adapter pattern for template engine

---
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/adapter/FreemarkerTemplateAdapter.java
@@ -0,0 +1,28 @@
+package com.springddd.application.service.gen.adapter;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class FreemarkerTemplateAdapter implements TemplateEngineAdapter {
+    private final Configuration configuration;
+
+    @Override
+    public Mono<String> render(String templateName, String templateContent, Map<String, Object> dataModel) {
+        try {
+            Template template = new Template(templateName, new StringReader(templateContent), configuration);
+            StringWriter out = new StringWriter();
+            template.process(dataModel, out);
+            return Mono.just(out.toString());
+        } catch (Exception e) {
+            return Mono.error(e);
+        }
+    }
+}
---
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/adapter/TemplateEngineAdapter.java
@@ -0,0 +1,8 @@
+package com.springddd.application.service.gen.adapter;
+
+import java.util.Map;
+import reactor.core.publisher.Mono;
+
+public interface TemplateEngineAdapter {
+    Mono<String> render(String templateName, String templateContent, Map<String, Object> dataModel);
+}
