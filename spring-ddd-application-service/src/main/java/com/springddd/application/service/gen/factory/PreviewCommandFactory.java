ced3efe76edd33fad8042d252a0c2bcf423ca928
henrikabel25 <henrikabel@foxmail.com>
2025-11-18 20:33:13 +0800

    feat: add unit tests for user module

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/factory/PreviewCommandFactory.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/factory/PreviewCommandFactory.java
@@ -18,3 +18,4 @@ public class PreviewCommandFactory implements GenerateCommandFactory {
 }
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/dept/state/ActiveDeptState.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dept/state/ActiveDeptState.java
@@ -14,3 +14,4 @@ public class ActiveDeptState implements DeptState {
         // Already active
     }
 }
+
