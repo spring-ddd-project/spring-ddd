63f1fb31dd6c13ab6b5304c3fcd7e7244c0c1927
henrikabel25 <henrikabel@foxmail.com>
2025-11-19 13:30:15 +0800

    feat: implement memento pattern for project info history

---
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/memento/GenProjectInfoMemento.java
@@ -0,0 +1,16 @@
+package com.springddd.domain.gen.memento;
+
+import com.springddd.domain.gen.ProjectInfo;
+import com.springddd.domain.gen.GenProjectInfoExtendInfo;
+import lombok.Getter;
+
+@Getter
+public class GenProjectInfoMemento {
+    private final ProjectInfo projectInfo;
+    private final GenProjectInfoExtendInfo extendInfo;
+
+    public GenProjectInfoMemento(ProjectInfo projectInfo, GenProjectInfoExtendInfo extendInfo) {
+        this.projectInfo = projectInfo;
+        this.extendInfo = extendInfo;
+    }
+}
