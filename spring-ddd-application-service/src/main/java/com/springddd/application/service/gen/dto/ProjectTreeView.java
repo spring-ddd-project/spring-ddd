88e2c9f40cbb6ddf57263b70c5c2ba3c6d0a5c07
henrikabel25 <henrikabel@foxmail.com>
2025-11-19 21:30:45 +0800

    feat: implement iterator pattern for project tree traversal

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/ProjectTreeView.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/ProjectTreeView.java
@@ -1,12 +1,13 @@
 package com.springddd.application.service.gen.dto;
 
 import lombok.Data;
-
 import java.io.Serializable;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Stack;
 
 @Data
-public class ProjectTreeView implements Serializable {
+public class ProjectTreeView implements Serializable, Iterable<ProjectTreeView> {
 
     private String label;
 
@@ -18,6 +19,35 @@ public class ProjectTreeView implements Serializable {
         visitor.visit(this);
         if (children != null) {
             children.forEach(child -> child.accept(visitor));
+        }
+    }
+
+    @Override
+    public Iterator<ProjectTreeView> iterator() {
+        return new ProjectTreeIterator(this);
+    }
+
+    private static class ProjectTreeIterator implements Iterator<ProjectTreeView> {
+        private final Stack<ProjectTreeView> stack = new Stack<>();
+
+        public ProjectTreeIterator(ProjectTreeView root) {
+            stack.push(root);
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+
+        @Override
+        public ProjectTreeView next() {
+            ProjectTreeView node = stack.pop();
+            if (node.getChildren() != null) {
+                for (int i = node.getChildren().size() - 1; i >= 0; i--) {
+                    stack.push(node.getChildren().get(i));
+                }
+            }
+            return node;
         }
     }
 }
