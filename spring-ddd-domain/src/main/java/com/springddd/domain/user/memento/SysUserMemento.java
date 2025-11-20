01c079b806e23af6e01248afa1b46471b8010ed0
henrikabel25 <henrikabel@foxmail.com>
2025-11-20 21:15:45 +0800

    feat: implement memento pattern for user profile history

---
+++ spring-ddd-domain/src/main/java/com/springddd/domain/user/memento/SysUserMemento.java
@@ -0,0 +1,18 @@
+package com.springddd.domain.user.memento;
+
+import com.springddd.domain.user.Account;
+import com.springddd.domain.user.ExtendInfo;
+import lombok.Getter;
+
+@Getter
+public class SysUserMemento {
+    private final Account account;
+    private final ExtendInfo extendInfo;
+    private final Long deptId;
+
+    public SysUserMemento(Account account, ExtendInfo extendInfo, Long deptId) {
+        this.account = account;
+        this.extendInfo = extendInfo;
+        this.deptId = deptId;
+    }
+}
