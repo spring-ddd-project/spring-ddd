2f1603febcee258c522e0fc64f00e179123762d6
henrikabel25 <henrikabel@foxmail.com>
2025-11-21 18:21:42 +0800

    feat: add unit tests for cache module

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenTableInfoPageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenTableInfoPageQuery.java
@@ -28,3 +28,4 @@ public class GenTableInfoPageQuery extends GenTableInfoQuery implements Serializ
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/dict/exception/DictItemSortOrderNullException.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dict/exception/DictItemSortOrderNullException.java
@@ -23,3 +23,4 @@ public class DictItemSortOrderNullException extends DomainException {
 
 
 
+
