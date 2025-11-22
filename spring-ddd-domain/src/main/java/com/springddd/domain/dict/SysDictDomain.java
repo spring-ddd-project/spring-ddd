8c712ab1e7983ef0b7cad4a931b212b4d3e1d6b3
henrikabel25 <henrikabel@foxmail.com>
2025-11-22 14:30:45 +0800

    feat: implement prototype pattern for sys dict domain aggregate

--- spring-ddd-domain/src/main/java/com/springddd/domain/dict/SysDictDomain.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dict/SysDictDomain.java
@@ -6,13 +6,36 @@ import lombok.EqualsAndHashCode;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
-public class SysDictDomain extends AbstractDomainMask {
+public class SysDictDomain extends AbstractDomainMask implements Cloneable {
 
     private DictId dictId;
 
     private DictBasicInfo dictBasicInfo;
 
     private DictExtendInfo dictExtendInfo;
+
+    @Override
+    public SysDictDomain clone() {
+        try {
+            SysDictDomain clone = (SysDictDomain) super.clone();
+            if (this.dictId != null) clone.setDictId(new DictId(this.dictId.value()));
+            if (this.dictBasicInfo != null) {
+                DictBasicInfo basic = new DictBasicInfo();
+                basic.setDictName(this.dictBasicInfo.getDictName());
+                basic.setDictType(this.dictBasicInfo.getDictType());
+                basic.setDictStatus(this.dictBasicInfo.getDictStatus());
+                clone.setDictBasicInfo(basic);
+            }
+            if (this.dictExtendInfo != null) {
+                DictExtendInfo ext = new DictExtendInfo();
+                ext.setDictRemark(this.dictExtendInfo.getDictRemark());
+                clone.setDictExtendInfo(ext);
+            }
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
 
     private final java.util.List<com.springddd.domain.dict.observer.DictObserver> observers = new java.util.ArrayList<>();
 
