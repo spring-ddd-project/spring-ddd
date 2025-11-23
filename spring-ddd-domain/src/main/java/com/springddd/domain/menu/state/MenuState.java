a2b3d08b17ceeebca901687fc405750a2cee903b
henrikabel25 <henrikabel@foxmail.com>
2025-11-23 14:30:45 +0800

    feat: implement state pattern for menu visibility

---
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/state/MenuState.java
@@ -0,0 +1,8 @@
+package com.springddd.domain.menu.state;
+
+import com.springddd.domain.menu.SysMenuDomain;
+
+public interface MenuState {
+    void show(SysMenuDomain domain);
+    void hide(SysMenuDomain domain);
+}
