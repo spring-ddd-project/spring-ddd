a727d22c38f0da5a819a4556a4eedc233f2c5fee
henrikabel25 <henrikabel@foxmail.com>
2025-11-24 11:45:10 +0800

    feat: implement state pattern for dictionary item status

---
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dict/state/DisabledDictItemState.java
@@ -0,0 +1,16 @@
+package com.springddd.domain.dict.state;
+
+import com.springddd.domain.dict.SysDictItemDomain;
+
+public class DisabledDictItemState implements DictItemState {
+    @Override
+    public void enable(SysDictItemDomain domain) {
+        domain.getItemBasicInfo().setItemStatus(true);
+        domain.setState(new EnabledDictItemState());
+    }
+
+    @Override
+    public void disable(SysDictItemDomain domain) {
+        // Already disabled
+    }
+}
