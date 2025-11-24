b728559a2c0493981750ea446a17286a111c03ca
henrikabel25 <henrikabel@foxmail.com>
2025-11-24 15:21:16 +0800

    refactor: clean up cache dependencies

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenColumnsBatchSaveDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenColumnsBatchSaveDomainServiceImpl.java
@@ -74,3 +74,4 @@ public class GenColumnsBatchSaveDomainServiceImpl implements GenColumnsBatchSave
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/interpreter/PathExpression.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/interpreter/PathExpression.java
@@ -7,3 +7,4 @@ public interface PathExpression {
     List<ProjectTreeView> interpret(ProjectTreeView node);
 }
 
+
