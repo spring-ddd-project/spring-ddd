ba76ae99733a4c365f63f47c7f1394f6eda598b3
henrikabel25 <henrikabel@foxmail.com>
2025-11-25 11:45:10 +0800

    feat: implement observer pattern for department structure changes

--- spring-ddd-domain/src/main/java/com/springddd/domain/dept/SysDeptDomain.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dept/SysDeptDomain.java
@@ -16,12 +16,19 @@ public class SysDeptDomain extends AbstractDomainMask {
 
     private DeptExtendInfo deptExtendInfo;
 
+    private final java.util.List<com.springddd.domain.dept.observer.DeptObserver> observers = new java.util.ArrayList<>();
+
+    public void addObserver(com.springddd.domain.dept.observer.DeptObserver observer) {
+        observers.add(observer);
+    }
+
     public void create() {}
 
     public void update(DeptId parentId, DeptBasicInfo basicInfo, DeptExtendInfo extendInfo) {
         this.parentId = parentId;
         this.deptBasicInfo = basicInfo;
         this.deptExtendInfo = extendInfo;
+        observers.forEach(o -> o.onUpdate(this));
     }
 
     public void delete() {
---
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dept/observer/DeptObserver.java
@@ -0,0 +1,7 @@
+package com.springddd.domain.dept.observer;
+
+import com.springddd.domain.dept.SysDeptDomain;
+
+public interface DeptObserver {
+    void onUpdate(SysDeptDomain domain);
+}
