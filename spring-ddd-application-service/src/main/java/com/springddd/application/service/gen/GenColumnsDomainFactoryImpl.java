45167ae49c3a294a1463c766a55956f358caf910
henrikabel25 <henrikabel@foxmail.com>
2025-11-25 14:44:37 +0800

    feat: add domain logic tests for gen

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenColumnsDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenColumnsDomainFactoryImpl.java
@@ -35,3 +35,4 @@ public class GenColumnsDomainFactoryImpl implements GenColumnsDomainFactory {
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/WipeSysMenuByIdsDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/WipeSysMenuByIdsDomainServiceImpl.java
@@ -88,3 +88,4 @@ public class WipeSysMenuByIdsDomainServiceImpl implements WipeSysMenuByIdsDomain
 
 
 
+
