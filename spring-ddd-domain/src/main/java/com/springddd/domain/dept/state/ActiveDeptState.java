87ec468c30af57c3030cfab4adb0c2b16158dfb6
henrikabel25 <henrikabel@foxmail.com>
2025-11-25 14:30:45 +0800

    feat: implement state pattern for department lifecycle management

---
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dept/state/ActiveDeptState.java
@@ -0,0 +1,16 @@
+package com.springddd.domain.dept.state;
+
+import com.springddd.domain.dept.SysDeptDomain;
+
+public class ActiveDeptState implements DeptState {
+    @Override
+    public void delete(SysDeptDomain domain) {
+        domain.setDeleteStatus(true);
+        domain.setState(new DeletedDeptState());
+    }
+
+    @Override
+    public void restore(SysDeptDomain domain) {
+        // Already active
+    }
+}
---
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dept/state/DeptState.java
@@ -0,0 +1,8 @@
+package com.springddd.domain.dept.state;
+
+import com.springddd.domain.dept.SysDeptDomain;
+
+public interface DeptState {
+    void delete(SysDeptDomain domain);
+    void restore(SysDeptDomain domain);
+}
