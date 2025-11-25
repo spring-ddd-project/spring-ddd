cb2b4f0be84fe574426dc6d955b9bf2b5f58fde2
henrikabel25 <henrikabel@foxmail.com>
2025-11-25 21:15:45 +0800

    feat: implement memento pattern for department history

---
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dept/memento/SysDeptMemento.java
@@ -0,0 +1,19 @@
+package com.springddd.domain.dept.memento;
+
+import com.springddd.domain.dept.DeptId;
+import com.springddd.domain.dept.DeptBasicInfo;
+import com.springddd.domain.dept.DeptExtendInfo;
+import lombok.Getter;
+
+@Getter
+public class SysDeptMemento {
+    private final DeptId parentId;
+    private final DeptBasicInfo deptBasicInfo;
+    private final DeptExtendInfo deptExtendInfo;
+
+    public SysDeptMemento(DeptId parentId, DeptBasicInfo deptBasicInfo, DeptExtendInfo deptExtendInfo) {
+        this.parentId = parentId;
+        this.deptBasicInfo = deptBasicInfo;
+        this.deptExtendInfo = deptExtendInfo;
+    }
+}
