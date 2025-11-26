3c88d652984b24f226e01292574c0af58d275548
henrikabel25 <henrikabel@foxmail.com>
2025-11-26 19:33:48 +0800

    feat: improve coverage for dept tests

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/exception/CustomAccessDeniedHandler.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/exception/CustomAccessDeniedHandler.java
@@ -43,3 +43,4 @@ public class CustomAccessDeniedHandler implements ServerAccessDeniedHandler {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenColumnsDomainRepository.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenColumnsDomainRepository.java
@@ -9,3 +9,4 @@ public interface GenColumnsDomainRepository extends DomainRepository<ColumnsId, 
 
 
 
+
