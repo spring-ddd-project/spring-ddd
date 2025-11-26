5a878b7f290f0c98331ba965cfa79b5e16035448
henrikabel25 <henrikabel@foxmail.com>
2025-11-26 14:30:45 +0800

    feat: implement abstract factory pattern for template engine adapters

---
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/factory/FreemarkerEngineFactory.java
@@ -0,0 +1,19 @@
+package com.springddd.application.service.gen.factory;
+
+import com.springddd.application.service.gen.adapter.FreemarkerTemplateAdapter;
+import com.springddd.application.service.gen.adapter.TemplateEngineAdapter;
+import freemarker.template.Configuration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class FreemarkerEngineFactory implements TemplateEngineFactory {
+
+    private final Configuration configuration;
+
+    @Override
+    public TemplateEngineAdapter createEngineAdapter() {
+        return new FreemarkerTemplateAdapter(configuration);
+    }
+}
