a8c79e86be5f421a3c08bdb6756cf67d8495f28c
henrikabel25 <henrikabel@foxmail.com>
2025-11-26 11:45:10 +0800

    feat: implement command pattern for asynchronous code generation tasks

---
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/command/DownloadCommand.java
@@ -0,0 +1,19 @@
+package com.springddd.application.service.gen.command;
+
+import com.springddd.application.service.gen.GenDownloadDomainServiceImpl;
+import com.springddd.application.service.gen.dto.GenProjectInfoDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class DownloadCommand implements GenerateCommand {
+
+    private final GenDownloadDomainServiceImpl genDownloadDomainService;
+
+    @Override
+    public Mono<Void> execute(GenProjectInfoDTO dto) {
+        return genDownloadDomainService.downloadCode(dto).then();
+    }
+}
---
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/command/GenerateCommand.java
@@ -0,0 +1,8 @@
+package com.springddd.application.service.gen.command;
+
+import com.springddd.application.service.gen.dto.GenProjectInfoDTO;
+import reactor.core.publisher.Mono;
+
+public interface GenerateCommand {
+    Mono<Void> execute(GenProjectInfoDTO dto);
+}
