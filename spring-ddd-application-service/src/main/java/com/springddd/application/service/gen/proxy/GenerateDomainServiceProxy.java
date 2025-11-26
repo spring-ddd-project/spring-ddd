e833b3385de32032f2856f3a73f7e11495ed9792
henrikabel25 <henrikabel@foxmail.com>
2025-11-26 18:30:12 +0800

    feat: implement proxy pattern for generator service performance monitoring

---
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/proxy/GenerateDomainServiceProxy.java
@@ -0,0 +1,27 @@
+package com.springddd.application.service.gen.proxy;
+
+import com.springddd.domain.gen.GenerateDomainService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Primary
+@Service
+public class GenerateDomainServiceProxy implements GenerateDomainService {
+
+    private final GenerateDomainService targetService;
+
+    public GenerateDomainServiceProxy(@org.springframework.beans.factory.annotation.Qualifier("generateDomainServiceImpl") GenerateDomainService targetService) {
+        this.targetService = targetService;
+    }
+
+    @Override
+    public Mono<Void> generate(String tableName) {
+        long start = System.currentTimeMillis();
+        return targetService.generate(tableName)
+                .doFinally(signalType -> log.info("Code generation for {} took {} ms", tableName, System.currentTimeMillis() - start));
+    }
+}
