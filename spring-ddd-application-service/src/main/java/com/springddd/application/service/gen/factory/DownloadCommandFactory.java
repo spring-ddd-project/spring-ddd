3f355776f92a54269f14ec6daee7d9724b55ef05
henrikabel25 <henrikabel@foxmail.com>
2025-11-27 09:30:15 +0800

    feat: implement factory method pattern for generate command instantiation

---
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/factory/DownloadCommandFactory.java
@@ -0,0 +1,18 @@
+package com.springddd.application.service.gen.factory;
+
+import com.springddd.application.service.gen.command.DownloadCommand;
+import com.springddd.application.service.gen.command.GenerateCommand;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DownloadCommandFactory implements GenerateCommandFactory {
+
+    private final DownloadCommand downloadCommand;
+
+    @Override
+    public GenerateCommand createCommand() {
+        return downloadCommand;
+    }
+}
---
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/factory/GenerateCommandFactory.java
@@ -0,0 +1,7 @@
+package com.springddd.application.service.gen.factory;
+
+import com.springddd.application.service.gen.command.GenerateCommand;
+
+public interface GenerateCommandFactory {
+    GenerateCommand createCommand();
+}
