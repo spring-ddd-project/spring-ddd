056b6f98da61dd8d101052387b9086fe80370af8
henrikabel25 <henrikabel@foxmail.com>
2025-11-27 16:25:20 +0800

    refactor: enhance dept type safety

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/UpdateLeafAllocMaxIdByTagDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/UpdateLeafAllocMaxIdByTagDomainServiceImpl.java
@@ -32,3 +32,4 @@ public class UpdateLeafAllocMaxIdByTagDomainServiceImpl implements UpdateLeafAll
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/exception/FilterComponentNullException.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/exception/FilterComponentNullException.java
@@ -13,3 +13,4 @@ public class FilterComponentNullException extends DomainException {
 
 
 
+
