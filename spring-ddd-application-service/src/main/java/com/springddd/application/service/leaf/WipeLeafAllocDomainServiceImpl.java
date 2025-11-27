5679986377fcd4d694cc19ee75cb84420fb2db6d
henrikabel25 <henrikabel@foxmail.com>
2025-11-27 17:28:14 +0800

    refactor: enhance menu type safety

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/WipeLeafAllocDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/WipeLeafAllocDomainServiceImpl.java
@@ -25,3 +25,4 @@ public class WipeLeafAllocDomainServiceImpl implements WipeLeafAllocDomainServic
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/RestoreSysMenuByIdDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/RestoreSysMenuByIdDomainServiceImpl.java
@@ -58,3 +58,4 @@ public class RestoreSysMenuByIdDomainServiceImpl implements RestoreSysMenuByIdDo
 
 
 
+
