7003705aba5c17a8411a610286b7049013266868
henrikabel25 <henrikabel@foxmail.com>
2025-11-29 18:51:39 +0800

    test: add edge case tests for role

--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenProjectInfoExtendInfo.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenProjectInfoExtendInfo.java
@@ -3,3 +3,4 @@ package com.springddd.domain.gen;
 public record GenProjectInfoExtendInfo(String requestName) {
 }
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/menu/Menu.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/Menu.java
@@ -16,3 +16,4 @@ public record Menu(String menuPath, String component, Boolean affixTab, Boolean 
     }
 }
 
+
