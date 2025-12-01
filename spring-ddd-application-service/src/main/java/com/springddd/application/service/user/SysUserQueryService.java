9ed8143806cde699e4f5e3f1a448cc400d8fc830
henrikabel25 <henrikabel@foxmail.com>
2025-12-01 17:48:32 +0800

    test: add repository tests for domain

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/user/SysUserQueryService.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/user/SysUserQueryService.java
@@ -61,3 +61,4 @@ public class SysUserQueryService {
                 .map(sysUserViewMapStruct::toView);
     }
 }
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/util/PageResponse.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/util/PageResponse.java
@@ -33,3 +33,4 @@ public class PageResponse<T> implements java.io.Serializable, Iterable<T> {
 
 
 
+
