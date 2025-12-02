9a5a2ebb0867ef97c0c0df15a8e5eb7cc9172c94
henrikabel25 <henrikabel@foxmail.com>
2025-12-02 11:10:54 +0800

    refactor: optimize application data flow

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/WipeSysDictByIdsDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/WipeSysDictByIdsDomainServiceImpl.java
@@ -65,3 +65,4 @@ public class WipeSysDictByIdsDomainServiceImpl implements WipeSysDictByIdsDomain
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenColumnBindPageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenColumnBindPageQuery.java
@@ -39,3 +39,4 @@ public class GenColumnBindPageQuery extends GenColumnBindQuery implements Serial
 
 
 
+
