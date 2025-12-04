3c8a4db2fdddbddf83c49255266f50bc6c50b97c
henrikabel25 <henrikabel@foxmail.com>
2025-12-04 17:43:18 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/SysDictCommandService.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/SysDictCommandService.java
@@ -53,3 +53,4 @@ public class SysDictCommandService {
         return restoreSysDictByIdDomainService.restoreByIds(ids);
     }
 }
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/DeleteSysMenuByIdDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/DeleteSysMenuByIdDomainServiceImpl.java
@@ -55,3 +55,4 @@ public class DeleteSysMenuByIdDomainServiceImpl implements DeleteSysMenuByIdDoma
 
 
 
+
