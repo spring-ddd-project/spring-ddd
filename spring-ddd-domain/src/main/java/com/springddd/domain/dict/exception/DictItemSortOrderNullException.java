61ddff1c2e697546469db73eb69449d0fd6a1922
henrikabel25 <henrikabel@foxmail.com>
2025-12-04 19:37:23 +0800

    refactor: consolidate cache utilities

--- spring-ddd-domain/src/main/java/com/springddd/domain/dict/exception/DictItemSortOrderNullException.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dict/exception/DictItemSortOrderNullException.java
@@ -15,3 +15,4 @@ public class DictItemSortOrderNullException extends DomainException {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/menu/AdvancedOptions.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/AdvancedOptions.java
@@ -18,3 +18,4 @@ public record AdvancedOptions(Integer order, String icon, Integer menuType, Bool
 
 
 
+
