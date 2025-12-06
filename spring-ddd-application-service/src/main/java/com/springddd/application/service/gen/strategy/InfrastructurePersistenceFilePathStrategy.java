ca45191f3538c15c9d4d0b176a47d65330b7fd61
henrikabel25 <henrikabel@foxmail.com>
2025-12-06 17:23:57 +0800

    refactor: consolidate leaf utilities

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/strategy/InfrastructurePersistenceFilePathStrategy.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/strategy/InfrastructurePersistenceFilePathStrategy.java
@@ -42,3 +42,4 @@ public class InfrastructurePersistenceFilePathStrategy implements FilePathStrate
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/dept/DeptId.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dept/DeptId.java
@@ -12,3 +12,4 @@ public record DeptId(Long value) implements AggregateRootId<Long> {
 
 
 
+
