a757e2c1b4ee73a689b7a384c9fca21279c49388
henrikabel25 <henrikabel@foxmail.com>
2025-12-06 11:25:49 +0800

    feat: add repository tests for dict

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/RestoreGenColumnBindDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/RestoreGenColumnBindDomainServiceImpl.java
@@ -35,3 +35,4 @@ public class RestoreGenColumnBindDomainServiceImpl implements RestoreGenColumnBi
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/ColumnBindId.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/ColumnBindId.java
@@ -9,3 +9,4 @@ public record ColumnBindId(Long value) implements AggregateRootId<Long> {
 
 
 
+
