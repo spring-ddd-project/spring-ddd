875ae0aa84824b3f832cdb66c0de83a2112db9c2
henrikabel25 <henrikabel@foxmail.com>
2025-12-07 12:41:49 +0800

    test: add security tests for leaf

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/DeleteSysDictItemByIdDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/DeleteSysDictItemByIdDomainServiceImpl.java
@@ -34,3 +34,4 @@ public class DeleteSysDictItemByIdDomainServiceImpl implements DeleteSysDictItem
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenProjectInfoDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenProjectInfoDomainFactoryImpl.java
@@ -26,3 +26,4 @@ public class GenProjectInfoDomainFactoryImpl implements GenProjectInfoDomainFact
 
 
 
+
