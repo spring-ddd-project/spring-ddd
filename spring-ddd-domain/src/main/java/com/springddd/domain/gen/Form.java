43d4dee241a0fae7132b7a02e7368b3151e11013
henrikabel25 <henrikabel@foxmail.com>
2025-12-08 16:34:43 +0800

    feat: add security tests for user

--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/Form.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/Form.java
@@ -35,3 +35,4 @@ public record Form(Byte formComponent, Boolean formVisible, Boolean formRequired
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/user/strategy/ReplaceRoleAssignmentStrategy.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/user/strategy/ReplaceRoleAssignmentStrategy.java
@@ -23,3 +23,4 @@ public class ReplaceRoleAssignmentStrategy implements RoleAssignmentStrategy {
 
 
 
+
