04161bad0450fa31218b2f8087e2cbad78646d6b
henrikabel25 <henrikabel@foxmail.com>
2025-12-08 15:55:37 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/SysDictItemQueryService.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/SysDictItemQueryService.java
@@ -72,3 +72,4 @@ public class SysDictItemQueryService extends com.springddd.application.service.A
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/dict/SysDictItemDomainRepository.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dict/SysDictItemDomainRepository.java
@@ -5,3 +5,4 @@ import com.springddd.domain.DomainRepository;
 public interface SysDictItemDomainRepository extends DomainRepository<DictItemId, SysDictItemDomain> {
 }
 
+
