92bc2876a4834c62694567edb393987eabe1820c
henrikabel25 <henrikabel@foxmail.com>
2025-12-08 10:31:36 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/exception/CustomAccessDeniedHandler.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/exception/CustomAccessDeniedHandler.java
@@ -39,3 +39,4 @@ public class CustomAccessDeniedHandler implements ServerAccessDeniedHandler {
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dept/RestoreSysDeptByIdDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dept/RestoreSysDeptByIdDomainServiceImpl.java
@@ -32,3 +32,4 @@ public class RestoreSysDeptByIdDomainServiceImpl implements RestoreSysDeptByIdDo
 
 
 
+
