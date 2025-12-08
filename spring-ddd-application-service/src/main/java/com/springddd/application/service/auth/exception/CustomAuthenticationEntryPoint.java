4d9803f5f65e785a4ebe047d0aeff4edbb6269bc
henrikabel25 <henrikabel@foxmail.com>
2025-12-08 16:10:10 +0800

    feat: add validation tests for role

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/exception/CustomAuthenticationEntryPoint.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/exception/CustomAuthenticationEntryPoint.java
@@ -39,3 +39,4 @@ public class CustomAuthenticationEntryPoint implements ServerAuthenticationEntry
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/dict/DictId.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dict/DictId.java
@@ -13,3 +13,4 @@ public record DictId(Long value) implements AggregateRootId<Long> {
 
 
 
+
