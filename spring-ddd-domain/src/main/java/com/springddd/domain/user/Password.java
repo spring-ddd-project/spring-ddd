e2fde6e117355507ca3f86d5fb96f3b5aeb6ec2a
henrikabel25 <henrikabel@foxmail.com>
2025-12-10 12:52:58 +0800

    feat: create system user domain

--- spring-ddd-domain/src/main/java/com/springddd/domain/user/Password.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/user/Password.java
@@ -1,7 +1,9 @@
 package com.springddd.domain.user;
 
 import com.springddd.domain.AggregateRootId;
+import com.springddd.domain.exception.PasswordNullException;
 import lombok.Data;
+import org.springframework.util.ObjectUtils;
 
 @Data
 public class Password {
@@ -9,6 +11,9 @@ public class Password {
     private final String value;
 
     public Password(String value) {
+        if (ObjectUtils.isEmpty(value)) {
+            throw new PasswordNullException();
+        }
         this.value = value;
     }
 }
