0fc11fb124268631c28c27b290a9fdf2aa342f10
henrikabel25 <henrikabel@foxmail.com>
2025-12-11 14:34:11 +0800

    feat: add validation tests for leaf

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/dto/SysDictPageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/dto/SysDictPageQuery.java
@@ -28,3 +28,4 @@ public class SysDictPageQuery extends SysDictQuery implements Serializable {
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/LeafAllocDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/LeafAllocDomainFactoryImpl.java
@@ -24,3 +24,4 @@ public class LeafAllocDomainFactoryImpl implements LeafAllocDomainFactory {
 
 
 
+
