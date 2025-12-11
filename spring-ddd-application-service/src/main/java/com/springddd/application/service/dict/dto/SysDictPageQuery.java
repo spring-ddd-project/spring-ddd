2b4038056e50dbb8a520b6f3f9499e8e090073df
henrikabel25 <henrikabel@foxmail.com>
2025-12-11 20:27:45 +0800

    feat: add domain logic tests for menu

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/dto/SysDictPageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/dto/SysDictPageQuery.java
@@ -30,3 +30,4 @@ public class SysDictPageQuery extends SysDictQuery implements Serializable {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/dict/SysDictItemDomainRepository.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dict/SysDictItemDomainRepository.java
@@ -16,3 +16,4 @@ public interface SysDictItemDomainRepository extends DomainRepository<DictItemId
 
 
 
+
