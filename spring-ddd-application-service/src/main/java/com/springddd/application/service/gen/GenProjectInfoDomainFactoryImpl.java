6c8304cb288b274949a111c3e4a036faf49bf6ee
henrikabel25 <henrikabel@foxmail.com>
2025-12-12 21:32:43 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenProjectInfoDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenProjectInfoDomainFactoryImpl.java
@@ -23,3 +23,4 @@ public class GenProjectInfoDomainFactoryImpl implements GenProjectInfoDomainFact
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/exception/TemplateContentNullException.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/exception/TemplateContentNullException.java
@@ -15,3 +15,4 @@ public class TemplateContentNullException extends DomainException {
 
 
 
+
