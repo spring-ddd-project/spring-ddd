d2be100b18aabaa69a2d452e77a047a93d6f3af9
henrikabel25 <henrikabel@foxmail.com>
2025-12-12 10:32:28 +0800

    feat: Add global error handling

--- spring-ddd-domain/src/main/java/com/springddd/domain/exception/PasswordNullException.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/exception/PasswordNullException.java
@@ -3,12 +3,9 @@ package com.springddd.domain.exception;
 import lombok.Getter;
 
 @Getter
-public class PasswordNullException extends RuntimeException {
-
-    private final int errorCode;
+public class PasswordNullException extends DomainException {
 
     public PasswordNullException() {
-        super("密码不能为空");
-        this.errorCode = 1001;
+        super(1000, "密码不能为空");
     }
 }
