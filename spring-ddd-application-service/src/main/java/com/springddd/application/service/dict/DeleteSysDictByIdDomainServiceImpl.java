36dffa90e037b7c115fbfc0043d804c2e6429c12
henrikabel25 <henrikabel@foxmail.com>
2025-12-13 18:10:56 +0800

    refactor: improve user error handling

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/DeleteSysDictByIdDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/DeleteSysDictByIdDomainServiceImpl.java
@@ -57,3 +57,4 @@ public class DeleteSysDictByIdDomainServiceImpl implements DeleteSysDictByIdDoma
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/UpdateLeafAllocMaxIdByCustomStepDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/UpdateLeafAllocMaxIdByCustomStepDomainServiceImpl.java
@@ -51,3 +51,4 @@ public class UpdateLeafAllocMaxIdByCustomStepDomainServiceImpl implements Update
 
 
 
+
