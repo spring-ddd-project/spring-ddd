8b9af71fe7e4825eb16dcbe7ef6def5fcffeb177
henrikabel25 <henrikabel@foxmail.com>
2025-12-13 10:35:35 +0800

    test: add unit tests for dict module

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenAggregateDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenAggregateDomainFactoryImpl.java
@@ -23,3 +23,4 @@ public class GenAggregateDomainFactoryImpl implements GenAggregateDomainFactory 
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/dict/DictItemBasicInfo.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dict/DictItemBasicInfo.java
@@ -21,3 +21,4 @@ public record DictItemBasicInfo(String itemLabel, Integer itemValue) {
 
 
 
+
