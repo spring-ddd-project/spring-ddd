1d5ddde4a9177ca95c00ee262a85bb1d629579be
henrikabel25 <henrikabel@foxmail.com>
2025-12-13 19:12:48 +0800

    test: add edge case tests for gen

--- spring-ddd-domain/src/main/java/com/springddd/domain/dept/SysDeptDomain.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dept/SysDeptDomain.java
@@ -96,3 +96,4 @@ public class SysDeptDomain extends AbstractDomainMask implements Cloneable {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/TemplateInfo.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/TemplateInfo.java
@@ -25,3 +25,4 @@ public record TemplateInfo(String templateName, String templateContent) {
 
 
 
+
