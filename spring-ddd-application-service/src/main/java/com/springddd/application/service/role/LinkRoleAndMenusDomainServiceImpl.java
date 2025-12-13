f1448418d2b7905b1d524c9f960c80f06f8ec8bd
henrikabel25 <henrikabel@foxmail.com>
2025-12-13 18:10:11 +0800

    refactor: simplify user module structure

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/role/LinkRoleAndMenusDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/role/LinkRoleAndMenusDomainServiceImpl.java
@@ -56,3 +56,4 @@ public class LinkRoleAndMenusDomainServiceImpl implements LinkRoleAndMenusDomain
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/util/PageResponse.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/util/PageResponse.java
@@ -50,3 +50,4 @@ public class PageResponse<T> implements java.io.Serializable, Iterable<T> {
 
 
 
+
