21908ab439da12580d431d489a58dc0efac5230d
henrikabel25 <henrikabel@foxmail.com>
2025-12-14 16:20:50 +0800

    test: add security tests for user

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/exception/CustomAccessDeniedHandler.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/exception/CustomAccessDeniedHandler.java
@@ -50,3 +50,4 @@ public class CustomAccessDeniedHandler implements ServerAccessDeniedHandler {
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/DeleteGenColumnBindDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/DeleteGenColumnBindDomainServiceImpl.java
@@ -38,3 +38,4 @@ public class DeleteGenColumnBindDomainServiceImpl implements DeleteGenColumnBind
 
 
 
+
