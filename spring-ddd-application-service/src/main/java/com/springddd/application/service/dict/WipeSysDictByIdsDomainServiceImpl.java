0b000c689db04289ce5f1f60af916deaa208b8dd
henrikabel25 <henrikabel@foxmail.com>
2025-12-14 13:27:14 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/WipeSysDictByIdsDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/WipeSysDictByIdsDomainServiceImpl.java
@@ -53,3 +53,4 @@ public class WipeSysDictByIdsDomainServiceImpl implements WipeSysDictByIdsDomain
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/role/RoleExtendInfo.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/role/RoleExtendInfo.java
@@ -16,3 +16,4 @@ public record RoleExtendInfo(String roleDesc, Boolean roleStatus) {
 
 
 
+
