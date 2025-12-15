69571e33c0cc1daf2cc7cf75d6d5d558cc2e7eca
henrikabel25 <henrikabel@foxmail.com>
2025-12-15 21:10:16 +0800

    refactor: extract common logic in persistence

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dept/dto/SysDeptPageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dept/dto/SysDeptPageQuery.java
@@ -28,3 +28,4 @@ public class SysDeptPageQuery extends SysDeptQuery implements Serializable {
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/SysDictDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/SysDictDomainFactoryImpl.java
@@ -28,3 +28,4 @@ public class SysDictDomainFactoryImpl implements SysDictDomainFactory {
 
 
 
+
