90c68e5cd75f57e79f87728b4948f7f46de998ac
henrikabel25 <henrikabel@foxmail.com>
2025-12-16 15:12:36 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dept/DeleteSysDeptByIdDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dept/DeleteSysDeptByIdDomainServiceImpl.java
@@ -40,3 +40,4 @@ public class DeleteSysDeptByIdDomainServiceImpl implements DeleteSysDeptByIdDoma
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/menu/exception/MenuPermissionNullException.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/exception/MenuPermissionNullException.java
@@ -15,3 +15,4 @@ public class MenuPermissionNullException extends DomainException {
 
 
 
+
