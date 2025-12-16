3d8b4c393f381a7771d08c60916c39bb1f287068
henrikabel25 <henrikabel@foxmail.com>
2025-12-16 21:10:21 +0800

    refactor: improve auth error handling

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/strategy/InfrastructurePersistenceFilePathStrategy.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/strategy/InfrastructurePersistenceFilePathStrategy.java
@@ -54,3 +54,4 @@ public class InfrastructurePersistenceFilePathStrategy implements FilePathStrate
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/role/WipeSysRoleByIdsDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/role/WipeSysRoleByIdsDomainServiceImpl.java
@@ -71,3 +71,4 @@ public class WipeSysRoleByIdsDomainServiceImpl implements WipeSysRoleByIdsDomain
 
 
 
+
