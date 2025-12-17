03a350e8092a4b6e2626dac1a8e36e30cca30810
henrikabel25 <henrikabel@foxmail.com>
2025-12-17 13:44:56 +0800

    refactor: enhance leaf type safety

--- spring-ddd-domain/src/main/java/com/springddd/domain/leaf/LeafAllocDomainRepository.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/leaf/LeafAllocDomainRepository.java
@@ -27,3 +27,4 @@ public interface LeafAllocDomainRepository extends DomainRepository<LeafId, Leaf
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/user/SysUserDomain.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/user/SysUserDomain.java
@@ -105,3 +105,4 @@ public class SysUserDomain extends AbstractDomainMask implements Cloneable {
 
 
 
+
