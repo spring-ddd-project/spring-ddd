eee9745bb2906ea8a19caaada71f5d224be8612e
henrikabel25 <henrikabel@foxmail.com>
2025-12-18 16:47:23 +0800

    feat: improve coverage for persistence tests

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/SysMenuDomainMenuStrategy.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/SysMenuDomainMenuStrategy.java
@@ -48,3 +48,4 @@ public class SysMenuDomainMenuStrategy implements SysMenuDomainStrategy {
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/user/dto/SysUserPageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/user/dto/SysUserPageQuery.java
@@ -38,3 +38,4 @@ public class SysUserPageQuery extends SysUserQuery implements Serializable {
 
 
 
+
