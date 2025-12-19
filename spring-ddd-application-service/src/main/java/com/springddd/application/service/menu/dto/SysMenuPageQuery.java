47effb14f88a8a81d848bdbaa54d7ecd70f9d7c2
henrikabel25 <henrikabel@foxmail.com>
2025-12-19 21:18:12 +0800

    test: add repository tests for user

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/dto/SysMenuPageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/dto/SysMenuPageQuery.java
@@ -29,3 +29,4 @@ public class SysMenuPageQuery extends SysMenuQuery implements Serializable {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenProjectInfoDomainRepository.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenProjectInfoDomainRepository.java
@@ -17,3 +17,4 @@ public interface GenProjectInfoDomainRepository extends DomainRepository<InfoId,
 
 
 
+
