6326c5be6a29fd244e5603112655817b6d460aec
henrikabel25 <henrikabel@foxmail.com>
2025-12-21 15:18:39 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenTemplateDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenTemplateDomainFactoryImpl.java
@@ -27,3 +27,4 @@ public class GenTemplateDomainFactoryImpl implements GenTemplateDomainFactory {
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/user/DeleteSysUserByIdDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/user/DeleteSysUserByIdDomainServiceImpl.java
@@ -39,3 +39,4 @@ public class DeleteSysUserByIdDomainServiceImpl implements DeleteSysUserByIdDoma
 
 
 
+
