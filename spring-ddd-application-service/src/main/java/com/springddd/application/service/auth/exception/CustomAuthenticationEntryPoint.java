577f4b05d09a1c36ac6c4f595c50bf9d1dde14e4
henrikabel25 <henrikabel@foxmail.com>
2025-12-22 15:17:43 +0800

    refactor: extract common logic in auth

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/exception/CustomAuthenticationEntryPoint.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/exception/CustomAuthenticationEntryPoint.java
@@ -45,3 +45,4 @@ public class CustomAuthenticationEntryPoint implements ServerAuthenticationEntry
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/WipeGenColumnBindByIdsDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/WipeGenColumnBindByIdsDomainServiceImpl.java
@@ -37,3 +37,4 @@ public class WipeGenColumnBindByIdsDomainServiceImpl implements WipeGenColumnBin
 
 
 
+
