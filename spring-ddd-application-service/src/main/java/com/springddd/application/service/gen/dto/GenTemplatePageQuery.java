6fdfd528ed6f0c54341ea1852eaa186a6eb29026
henrikabel25 <henrikabel@foxmail.com>
2025-12-22 20:54:15 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenTemplatePageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenTemplatePageQuery.java
@@ -22,3 +22,4 @@ public class GenTemplatePageQuery extends GenTemplateQuery implements Serializab
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/UpdateLeafAllocMaxIdByTagDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/UpdateLeafAllocMaxIdByTagDomainServiceImpl.java
@@ -38,3 +38,4 @@ public class UpdateLeafAllocMaxIdByTagDomainServiceImpl implements UpdateLeafAll
 
 
 
+
