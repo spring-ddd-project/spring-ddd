ad4e0bd497dc4190efdbbeb04118e63e02f96c17
henrikabel25 <henrikabel@foxmail.com>
2025-12-23 13:34:15 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/strategy/ApplicationServiceFilePathStrategy.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/strategy/ApplicationServiceFilePathStrategy.java
@@ -84,3 +84,4 @@ public class ApplicationServiceFilePathStrategy implements FilePathStrategy {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/Form.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/Form.java
@@ -30,3 +30,4 @@ public record Form(Byte formComponent, Boolean formVisible, Boolean formRequired
 
 
 
+
