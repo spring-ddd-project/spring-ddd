f73fb9fdab40cc55e3ca42afffa143d91de863e9
henrikabel25 <henrikabel@foxmail.com>
2025-12-23 18:19:05 +0800

    test: increase test coverage and refactor

--- spring-ddd-domain/src/main/java/com/springddd/domain/dict/DictItemExtendInfo.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dict/DictItemExtendInfo.java
@@ -22,3 +22,4 @@ public record DictItemExtendInfo(Integer sortOrder, Boolean itemStatus) {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenAggregateDomainRepository.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenAggregateDomainRepository.java
@@ -15,3 +15,4 @@ public interface GenAggregateDomainRepository extends DomainRepository<Aggregate
 
 
 
+
