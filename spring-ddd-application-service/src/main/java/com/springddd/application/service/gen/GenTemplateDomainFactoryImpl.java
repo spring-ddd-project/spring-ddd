3c2f0ebf467434441fdc0d4f6dc55031b31d6766
henrikabel25 <henrikabel@foxmail.com>
2025-12-24 11:13:17 +0800

    test: add mock tests for gen service

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenTemplateDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenTemplateDomainFactoryImpl.java
@@ -33,3 +33,4 @@ public class GenTemplateDomainFactoryImpl implements GenTemplateDomainFactory {
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/SysMenuDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/SysMenuDomainFactoryImpl.java
@@ -41,3 +41,4 @@ public class SysMenuDomainFactoryImpl implements SysMenuDomainFactory {
 
 
 
+
