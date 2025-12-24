10bf5bcc2525043957026cf2446055807604e57a
henrikabel25 <henrikabel@foxmail.com>
2025-12-24 21:55:56 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/WipeSysDictItemByIdsDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/WipeSysDictItemByIdsDomainServiceImpl.java
@@ -27,3 +27,4 @@ public class WipeSysDictItemByIdsDomainServiceImpl implements WipeSysDictItemByI
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/dto/LeafAllocPageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/dto/LeafAllocPageQuery.java
@@ -29,3 +29,4 @@ public class LeafAllocPageQuery extends LeafAllocQuery implements Serializable {
 
 
 
+
