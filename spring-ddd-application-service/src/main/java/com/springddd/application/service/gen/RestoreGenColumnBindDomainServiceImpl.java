ceea7e46025d3b1b77e6bcd5d58b6dbf6e1ea954
henrikabel25 <henrikabel@foxmail.com>
2025-12-24 15:28:47 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/RestoreGenColumnBindDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/RestoreGenColumnBindDomainServiceImpl.java
@@ -33,3 +33,4 @@ public class RestoreGenColumnBindDomainServiceImpl implements RestoreGenColumnBi
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenTableInfoPageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenTableInfoPageQuery.java
@@ -21,3 +21,4 @@ public class GenTableInfoPageQuery extends GenTableInfoQuery implements Serializ
 
 
 
+
