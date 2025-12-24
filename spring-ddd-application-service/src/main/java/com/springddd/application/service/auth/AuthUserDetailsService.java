537d3f6e7f47f792b890c4c20ada27ab268b1f98
henrikabel25 <henrikabel@foxmail.com>
2025-12-24 15:13:17 +0800

    fix: Simplify the contents of the token and provide a global method to access user information.

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/AuthUserDetailsService.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/AuthUserDetailsService.java
@@ -3,10 +3,13 @@ package com.springddd.application.service.auth;
 import com.springddd.application.service.menu.SysMenuQueryService;
 import com.springddd.application.service.menu.dto.SysMenuView;
 import com.springddd.application.service.role.SysRoleMenuQueryService;
+import com.springddd.application.service.role.SysRoleQueryService;
 import com.springddd.application.service.user.SysUserQueryService;
 import com.springddd.application.service.user.SysUserRoleQueryService;
+import com.springddd.application.service.user.dto.SysUserRoleView;
 import com.springddd.domain.auth.AuthUser;
 import com.springddd.domain.menu.MenuPermission;
+import com.springddd.domain.role.RoleCode;
 import com.springddd.domain.user.UserId;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
@@ -14,6 +17,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -27,6 +32,8 @@ public class AuthUserDetailsService implements ReactiveUserDetailsService {
 
     private final SysMenuQueryService sysMenuQueryService;
 
+    private final SysRoleQueryService sysRoleQueryService;
+
     @Override
     public Mono<UserDetails> findByUsername(String username) {
         return sysUserQueryService.queryUserByUsername(username)
@@ -38,22 +45,38 @@ public class AuthUserDetailsService implements ReactiveUserDetailsService {
 
                     return sysUserRoleQueryService.queryLinkUserAndRole(user.getUserId().value())
                             .flatMapMany(Flux::fromIterable)
-                            .flatMap(roleView ->
-                                    sysRoleMenuQueryService.queryLinkRoleAndMenus(roleView.getRoleId())
-                                            .flatMapMany(Flux::fromIterable)
-                            )
-                            .flatMap(roleMenuView ->
-                                    sysMenuQueryService.queryByMenuId(roleMenuView.getMenuId())
-                            )
-                            .map(SysMenuView::getPermission)
+                            .map(SysUserRoleView::getRoleId)
                             .distinct()
-                            .map(MenuPermission::new)
                             .collectList()
-                            .map(permissions -> {
-                                user.setPermissions(permissions);
-                                return user;
-                            });
+                            .flatMap(roleIds -> Flux.fromIterable(roleIds)
+                                    .flatMap(sysRoleQueryService::getById)
+                                    .collectList()
+                                    .flatMap(roleViews -> {
+                                        List<RoleCode> roles = roleViews.stream()
+                                                .map(role -> new RoleCode(role.getRoleCode()))
+                                                .toList();
+                                        user.setRoles(roles);
+
+                                        return Flux.fromIterable(roleIds)
+                                                .flatMap(roleId ->
+                                                        sysRoleMenuQueryService.queryLinkRoleAndMenus(roleId)
+                                                                .flatMapMany(Flux::fromIterable)
+                                                )
+                                                .flatMap(roleMenuView ->
+                                                        sysMenuQueryService.queryByMenuId(roleMenuView.getMenuId())
+                                                )
+                                                .map(SysMenuView::getPermission)
+                                                .distinct()
+                                                .map(MenuPermission::new)
+                                                .collectList()
+                                                .map(permissions -> {
+                                                    user.setPermissions(permissions);
+                                                    return user;
+                                                });
+                                    }));
+
                 });
     }
+
 
 }
