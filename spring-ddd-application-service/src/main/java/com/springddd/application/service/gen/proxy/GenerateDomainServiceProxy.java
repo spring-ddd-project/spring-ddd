1cae1bd942b2f313085038216758860f91fa2a47
henrikabel25 <henrikabel@foxmail.com>
2025-12-25 21:18:12 +0800

    test: add security tests for menu

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/proxy/GenerateDomainServiceProxy.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/proxy/GenerateDomainServiceProxy.java
@@ -41,3 +41,4 @@ public class GenerateDomainServiceProxy implements GenerateDomainService {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/role/SysRoleDomain.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/role/SysRoleDomain.java
@@ -103,3 +103,4 @@ public class SysRoleDomain extends AbstractDomainMask implements Cloneable {
 
 
 
+
