42062c6c2697c9a7c3c308e1845f3f202f5a6613
henrikabel25 <henrikabel@foxmail.com>
2025-12-26 15:17:43 +0800

    refactor: restructure util packages

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/AbstractQueryService.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/AbstractQueryService.java
@@ -66,3 +66,4 @@ public abstract class AbstractQueryService<T, V, Q extends com.springddd.domain.
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/strategy/ApplicationServiceFilePathStrategy.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/strategy/ApplicationServiceFilePathStrategy.java
@@ -93,3 +93,4 @@ public class ApplicationServiceFilePathStrategy implements FilePathStrategy {
 
 
 
+
