a29b232d91871aaab60f014295d74a1dafebca9d
henrikabel25 <henrikabel@foxmail.com>
2025-12-26 17:23:57 +0800

    refactor: extract common logic in util

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/AuthorizationManagerConfig.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/AuthorizationManagerConfig.java
@@ -89,3 +89,4 @@ public class AuthorizationManagerConfig implements ReactiveAuthorizationManager<
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/menu/AdvancedOptions.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/AdvancedOptions.java
@@ -32,3 +32,4 @@ public record AdvancedOptions(Integer order, String icon, Integer menuType, Bool
 
 
 
+
