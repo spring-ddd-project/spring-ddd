5f85e040d470b8a7f64df5b4cf668945fbd21c19
henrikabel25 <henrikabel@foxmail.com>
2025-12-27 22:10:14 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/exception/CustomAuthenticationEntryPoint.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/exception/CustomAuthenticationEntryPoint.java
@@ -38,3 +38,4 @@ public class CustomAuthenticationEntryPoint implements ServerAuthenticationEntry
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dept/DeleteSysDeptByIdDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dept/DeleteSysDeptByIdDomainServiceImpl.java
@@ -42,3 +42,4 @@ public class DeleteSysDeptByIdDomainServiceImpl implements DeleteSysDeptByIdDoma
 
 
 
+
