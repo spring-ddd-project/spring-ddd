7cbcd8be8950eae2e964eaa714bb7eb0944ea1be
henrikabel25 <henrikabel@foxmail.com>
2025-12-28 21:10:23 +0800

    feat: add controller tests for role

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dept/dto/SysDeptPageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dept/dto/SysDeptPageQuery.java
@@ -39,3 +39,4 @@ public class SysDeptPageQuery extends SysDeptQuery implements Serializable {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenColumnsExtendInfo.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenColumnsExtendInfo.java
@@ -26,3 +26,4 @@ public record GenColumnsExtendInfo(Long propDictId, Byte typescriptType) {
 
 
 
+
