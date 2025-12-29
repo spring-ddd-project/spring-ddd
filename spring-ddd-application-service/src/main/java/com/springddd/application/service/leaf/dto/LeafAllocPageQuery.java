1410926887c16cc1f8d28efba0df9e3f9af854ee
henrikabel25 <henrikabel@foxmail.com>
2025-12-29 16:53:27 +0800

    test: add domain logic tests for cache

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/dto/LeafAllocPageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/dto/LeafAllocPageQuery.java
@@ -39,3 +39,4 @@ public class LeafAllocPageQuery extends LeafAllocQuery implements Serializable {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/menu/AdvancedOptions.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/AdvancedOptions.java
@@ -34,3 +34,4 @@ public record AdvancedOptions(Integer order, String icon, Integer menuType, Bool
 
 
 
+
