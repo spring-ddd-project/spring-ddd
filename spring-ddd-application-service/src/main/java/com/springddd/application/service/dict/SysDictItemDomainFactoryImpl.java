1eb19a97a39898ae28b786cf1ec39651be9bd2f9
henrikabel25 <henrikabel@foxmail.com>
2025-12-29 12:35:46 +0800

    refactor: extract common logic in util

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/SysDictItemDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/SysDictItemDomainFactoryImpl.java
@@ -49,3 +49,4 @@ public class SysDictItemDomainFactoryImpl implements SysDictItemDomainFactory {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/user/SysUserRoleDomainRepository.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/user/SysUserRoleDomainRepository.java
@@ -37,3 +37,4 @@ public interface SysUserRoleDomainRepository extends DomainRepository<UserRoleId
 
 
 
+
