c75759a5389f754a7bde34d499807109e857b043
henrikabel25 <henrikabel@foxmail.com>
2025-12-29 20:15:55 +0800

    refactor: improve cache error handling

--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenColumnsDomainRepository.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenColumnsDomainRepository.java
@@ -33,3 +33,4 @@ public interface GenColumnsDomainRepository extends DomainRepository<ColumnsId, 
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/menu/exception/MenuPermissionNullException.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/exception/MenuPermissionNullException.java
@@ -32,3 +32,4 @@ public class MenuPermissionNullException extends DomainException {
 
 
 
+
