e1a737a67e50b954095669e3d3860f98cab007cd
henrikabel25 <henrikabel@foxmail.com>
2025-12-30 18:26:14 +0800

    test: add domain logic tests for dept

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/DeleteGenTemplateDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/DeleteGenTemplateDomainServiceImpl.java
@@ -45,3 +45,4 @@ public class DeleteGenTemplateDomainServiceImpl implements DeleteGenTemplateDoma
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/UpdateCacheDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/UpdateCacheDomainServiceImpl.java
@@ -114,3 +114,4 @@ public class UpdateCacheDomainServiceImpl implements UpdateCacheDomainService {
 
 
 
+
