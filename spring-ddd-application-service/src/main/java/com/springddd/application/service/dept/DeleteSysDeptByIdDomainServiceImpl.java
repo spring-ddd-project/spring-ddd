a2226aa94d08a05c28c9d8fdb5f26669f3d9b9de
henrikabel25 <henrikabel@foxmail.com>
2025-12-30 16:10:54 +0800

    feat: add repository tests for domain

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dept/DeleteSysDeptByIdDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dept/DeleteSysDeptByIdDomainServiceImpl.java
@@ -56,3 +56,4 @@ public class DeleteSysDeptByIdDomainServiceImpl implements DeleteSysDeptByIdDoma
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/user/dto/SysUserPageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/user/dto/SysUserPageQuery.java
@@ -42,3 +42,4 @@ public class SysUserPageQuery extends SysUserQuery implements Serializable {
 
 
 
+
