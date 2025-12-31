ba4480e9c7015660c7c348905fb666f061f8069a
henrikabel25 <henrikabel@foxmail.com>
2025-12-31 14:31:50 +0800

    test: increase test coverage and refactor

--- spring-ddd-domain/src/main/java/com/springddd/domain/leaf/LeafAllocDomainRepository.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/leaf/LeafAllocDomainRepository.java
@@ -25,3 +25,4 @@ public interface LeafAllocDomainRepository extends DomainRepository<LeafId, Leaf
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/util/PageResponse.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/util/PageResponse.java
@@ -46,3 +46,4 @@ public class PageResponse<T> implements java.io.Serializable, Iterable<T> {
 
 
 
+
