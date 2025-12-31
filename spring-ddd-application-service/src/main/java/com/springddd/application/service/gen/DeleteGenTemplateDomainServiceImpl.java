882dbe5cd599e57b418d3f917c0e5ee76e3d4dbe
henrikabel25 <henrikabel@foxmail.com>
2025-12-31 22:21:19 +0800

    test: add controller tests for gen

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/DeleteGenTemplateDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/DeleteGenTemplateDomainServiceImpl.java
@@ -46,3 +46,4 @@ public class DeleteGenTemplateDomainServiceImpl implements DeleteGenTemplateDoma
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/menu/AdvancedOptions.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/AdvancedOptions.java
@@ -37,3 +37,4 @@ public record AdvancedOptions(Integer order, String icon, Integer menuType, Bool
 
 
 
+
