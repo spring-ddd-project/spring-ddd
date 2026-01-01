d5000e5daba65e0d9aeb71f368549b4afdbfdd66
henrikabel25 <henrikabel@foxmail.com>
2026-01-01 11:45:25 +0800

    pref: Build Menus More Efficiently with record

--- spring-ddd-domain/src/main/java/com/springddd/domain/menu/MenuBasicInfo.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/MenuBasicInfo.java
@@ -1,28 +1,4 @@
 package com.springddd.domain.menu;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-public class MenuBasicInfo {
-
-    private MenuName menuName;
-
-    private MenuPath menuPath;
-
-    private MenuComponent menuComponent;
-
-    private MenuRedirect menuRedirect;
-
-    private MenuPermission menuPermission;
-
-    public MenuBasicInfo(String menuName, String menuPath, String menuComponent, String menuRedirect, String menuPermission) {
-        this.menuName = new MenuName(menuName);
-        this.menuPath = new MenuPath(menuPath);
-        this.menuComponent = new MenuComponent(menuComponent);
-        this.menuRedirect = new MenuRedirect(menuRedirect);
-        this.menuPermission = new MenuPermission(menuPermission);
-    }
-
+public record MenuBasicInfo(MenuName menuName, MenuPath menuPath, MenuComponent menuComponent, MenuRedirect menuRedirect, MenuPermission menuPermission) {
 }
