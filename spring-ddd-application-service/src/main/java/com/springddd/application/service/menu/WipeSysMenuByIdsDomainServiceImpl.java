ce0a526e96257ca1f528829e6e331605176d5379
henrikabel25 <henrikabel@foxmail.com>
2026-01-01 19:37:03 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/WipeSysMenuByIdsDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/WipeSysMenuByIdsDomainServiceImpl.java
@@ -78,3 +78,4 @@ public class WipeSysMenuByIdsDomainServiceImpl implements WipeSysMenuByIdsDomain
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/dept/DeptExtendInfo.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dept/DeptExtendInfo.java
@@ -27,3 +27,4 @@ public record DeptExtendInfo(Integer sortOrder, Boolean deptStatus) {
 
 
 
+
