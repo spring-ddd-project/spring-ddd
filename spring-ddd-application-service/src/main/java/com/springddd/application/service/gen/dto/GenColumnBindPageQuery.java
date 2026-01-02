6f36655d3f82457936dae7eba2bc7f2b616812b4
henrikabel25 <henrikabel@foxmail.com>
2026-01-02 13:01:55 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenColumnBindPageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenColumnBindPageQuery.java
@@ -26,3 +26,4 @@ public class GenColumnBindPageQuery extends GenColumnBindQuery implements Serial
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/leaf/LeafId.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/leaf/LeafId.java
@@ -11,3 +11,4 @@ public record LeafId(Long value) implements AggregateRootId<Long> {
 
 
 
+
