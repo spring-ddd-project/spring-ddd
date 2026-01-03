a0b67307101069b7c1d867a87bc7c38012b67e83
henrikabel25 <henrikabel@foxmail.com>
2026-01-03 22:38:16 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/proxy/GenerateDomainServiceProxy.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/proxy/GenerateDomainServiceProxy.java
@@ -35,3 +35,4 @@ public class GenerateDomainServiceProxy implements GenerateDomainService {
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/WipeSysMenuByIdsDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/WipeSysMenuByIdsDomainServiceImpl.java
@@ -79,3 +79,4 @@ public class WipeSysMenuByIdsDomainServiceImpl implements WipeSysMenuByIdsDomain
 
 
 
+
