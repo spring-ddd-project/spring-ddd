a254a32c7e2b4ab4dadfa157ad03940fe5b976c6
henrikabel25 <henrikabel@foxmail.com>
2026-01-05 22:46:44 +0800

    test: add repository tests for cache

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/WipeGenProjectInfoByIdsDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/WipeGenProjectInfoByIdsDomainServiceImpl.java
@@ -51,3 +51,4 @@ public class WipeGenProjectInfoByIdsDomainServiceImpl implements WipeGenProjectI
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/menu/Menu.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/Menu.java
@@ -38,3 +38,4 @@ public record Menu(String menuPath, String component, Boolean affixTab, Boolean 
 
 
 
+
