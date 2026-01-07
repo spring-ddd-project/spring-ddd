2c3274df3b5749656d5827400c60bf2076465466
henrikabel25 <henrikabel@foxmail.com>
2026-01-07 10:52:58 +0800

    test: add integration tests for dict

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/WipeGenAggregateDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/WipeGenAggregateDomainServiceImpl.java
@@ -55,3 +55,4 @@ public class WipeGenAggregateDomainServiceImpl implements WipeGenAggregateDomain
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/exception/FilterComponentNullException.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/exception/FilterComponentNullException.java
@@ -39,3 +39,4 @@ public class FilterComponentNullException extends DomainException {
 
 
 
+
