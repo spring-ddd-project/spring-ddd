7ab0e77362905416191fa428ce3988c23f946416
henrikabel25 <henrikabel@foxmail.com>
2026-01-07 15:10:47 +0800

    feat: add domain logic tests for domain

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/WipeGenTemplateDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/WipeGenTemplateDomainServiceImpl.java
@@ -62,3 +62,4 @@ public class WipeGenTemplateDomainServiceImpl implements WipeGenTemplateDomainSe
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/SysMenuDomainCatalogStrategy.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/SysMenuDomainCatalogStrategy.java
@@ -56,3 +56,4 @@ public class SysMenuDomainCatalogStrategy implements SysMenuDomainStrategy {
 
 
 
+
