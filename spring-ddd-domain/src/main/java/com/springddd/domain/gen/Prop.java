18d1306b2ce2fb47f21be619c036aac2424cd562
henrikabel25 <henrikabel@foxmail.com>
2026-01-07 10:34:47 +0800

    feat: add domain logic tests for menu

--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/Prop.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/Prop.java
@@ -49,3 +49,4 @@ public record Prop(String propColumnKey, String propColumnName, String propColum
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/user/UserId.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/user/UserId.java
@@ -41,3 +41,4 @@ public record UserId(Long value) implements AggregateRootId<Long> {
 
 
 
+
