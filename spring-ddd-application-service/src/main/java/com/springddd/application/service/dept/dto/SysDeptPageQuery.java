addd50f7d3eb959f5f1ab2252cb3086c203feafc
henrikabel25 <henrikabel@foxmail.com>
2026-01-08 18:51:39 +0800

    refactor: consolidate leaf utilities

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dept/dto/SysDeptPageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dept/dto/SysDeptPageQuery.java
@@ -42,3 +42,4 @@ public class SysDeptPageQuery extends SysDeptQuery implements Serializable {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenTemplateDomainRepository.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenTemplateDomainRepository.java
@@ -30,3 +30,4 @@ public interface GenTemplateDomainRepository extends DomainRepository<TemplateId
 
 
 
+
