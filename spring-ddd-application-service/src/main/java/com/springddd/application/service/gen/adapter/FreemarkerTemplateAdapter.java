ab73b2f98df243d23acf35d6794f0c381010ff09
henrikabel25 <henrikabel@foxmail.com>
2026-01-09 20:01:22 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/adapter/FreemarkerTemplateAdapter.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/adapter/FreemarkerTemplateAdapter.java
@@ -48,3 +48,4 @@ public class FreemarkerTemplateAdapter implements TemplateEngineAdapter {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/Prop.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/Prop.java
@@ -38,3 +38,4 @@ public record Prop(String propColumnKey, String propColumnName, String propColum
 
 
 
+
