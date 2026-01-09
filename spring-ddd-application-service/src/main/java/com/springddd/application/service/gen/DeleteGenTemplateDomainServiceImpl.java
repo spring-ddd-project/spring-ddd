659e952f11f1d842bb14d473042c68cc87c02e55
henrikabel25 <henrikabel@foxmail.com>
2026-01-09 18:10:40 +0800

    feat: add controller tests for auth

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/DeleteGenTemplateDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/DeleteGenTemplateDomainServiceImpl.java
@@ -53,3 +53,4 @@ public class DeleteGenTemplateDomainServiceImpl implements DeleteGenTemplateDoma
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/WipeGenColumnsByIdsDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/WipeGenColumnsByIdsDomainServiceImpl.java
@@ -64,3 +64,4 @@ public class WipeGenColumnsByIdsDomainServiceImpl implements WipeGenColumnsByIds
 
 
 
+
