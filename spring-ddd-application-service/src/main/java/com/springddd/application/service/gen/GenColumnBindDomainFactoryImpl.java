581f22fbc6e04d6b1decd5586dc508abc34834b9
henrikabel25 <henrikabel@foxmail.com>
2026-01-09 13:11:29 +0800

    test: add edge case tests for role

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenColumnBindDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenColumnBindDomainFactoryImpl.java
@@ -62,3 +62,4 @@ public class GenColumnBindDomainFactoryImpl implements GenColumnBindDomainFactor
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/RestoreSysMenuByIdDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/RestoreSysMenuByIdDomainServiceImpl.java
@@ -68,3 +68,4 @@ public class RestoreSysMenuByIdDomainServiceImpl implements RestoreSysMenuByIdDo
 
 
 
+
