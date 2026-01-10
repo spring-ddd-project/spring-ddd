e2b61b8fd64aea01d8b518219fdbb9c5fa988444
henrikabel25 <henrikabel@foxmail.com>
2026-01-10 20:08:34 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/RestoreGenTemplateDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/RestoreGenTemplateDomainServiceImpl.java
@@ -35,3 +35,4 @@ public class RestoreGenTemplateDomainServiceImpl implements RestoreGenTemplateDo
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/LeafAllocDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/LeafAllocDomainFactoryImpl.java
@@ -27,3 +27,4 @@ public class LeafAllocDomainFactoryImpl implements LeafAllocDomainFactory {
 
 
 
+
