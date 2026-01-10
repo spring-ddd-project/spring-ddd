04b084e06298b46d945c98e39be44d2c8beed781
henrikabel25 <henrikabel@foxmail.com>
2026-01-10 19:31:20 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/WipeGenTemplateDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/WipeGenTemplateDomainServiceImpl.java
@@ -49,3 +49,4 @@ public class WipeGenTemplateDomainServiceImpl implements WipeGenTemplateDomainSe
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/dept/SysDeptDomain.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dept/SysDeptDomain.java
@@ -103,3 +103,4 @@ public class SysDeptDomain extends AbstractDomainMask implements Cloneable {
 
 
 
+
