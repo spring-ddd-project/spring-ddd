022a07591e4863f3b52f65f05c46fb3940c97728
henrikabel25 <henrikabel@foxmail.com>
2026-01-11 17:10:42 +0800

    feat: add domain logic tests for menu

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/RestoreGenTemplateDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/RestoreGenTemplateDomainServiceImpl.java
@@ -52,3 +52,4 @@ public class RestoreGenTemplateDomainServiceImpl implements RestoreGenTemplateDo
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/dict/exception/DictDictStatusNullException.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dict/exception/DictDictStatusNullException.java
@@ -44,3 +44,4 @@ public class DictDictStatusNullException extends DomainException {
 
 
 
+
