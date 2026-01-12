04c7f7307569d31b431d4f6112eb914725fff294
henrikabel25 <henrikabel@foxmail.com>
2026-01-12 11:29:20 +0800

    refactor: optimize role data flow

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/SysDictItemQueryService.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/SysDictItemQueryService.java
@@ -106,3 +106,4 @@ public class SysDictItemQueryService extends com.springddd.application.service.A
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/dict/SysDictItemDomainRepository.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dict/SysDictItemDomainRepository.java
@@ -31,3 +31,4 @@ public interface SysDictItemDomainRepository extends DomainRepository<DictItemId
 
 
 
+
