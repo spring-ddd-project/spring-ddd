910c8c539180da1b0c20467b40d6831534af6cf3
henrikabel25 <henrikabel@foxmail.com>
2026-01-13 21:01:00 +0800

    test: increase test coverage and refactor

--- spring-ddd-domain/src/main/java/com/springddd/domain/dict/SysDictItemDomain.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dict/SysDictItemDomain.java
@@ -103,3 +103,4 @@ public class SysDictItemDomain extends AbstractDomainMask implements Cloneable {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/exception/TypeScriptTypeNullException.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/exception/TypeScriptTypeNullException.java
@@ -26,3 +26,4 @@ public class TypeScriptTypeNullException extends DomainException {
 
 
 
+
