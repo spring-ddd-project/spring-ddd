6f86ce652facbc2d161a63f824eb4f118582b08b
henrikabel25 <henrikabel@foxmail.com>
2026-01-13 13:10:18 +0800

    refactor: clean up persistence dependencies

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/user/WipeSysUserByIdsDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/user/WipeSysUserByIdsDomainServiceImpl.java
@@ -84,3 +84,4 @@ public class WipeSysUserByIdsDomainServiceImpl implements WipeSysUserByIdsDomain
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/ProjectInfo.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/ProjectInfo.java
@@ -70,3 +70,4 @@ public record ProjectInfo(String tableName, String packageName, String className
 
 
 
+
