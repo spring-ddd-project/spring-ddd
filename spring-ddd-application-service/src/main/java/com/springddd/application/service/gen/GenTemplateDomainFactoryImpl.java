664786f76ef9cc270193a453bf272895b60f6de2
henrikabel25 <henrikabel@foxmail.com>
2026-01-15 16:20:50 +0800

    test: add edge case tests for role

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenTemplateDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenTemplateDomainFactoryImpl.java
@@ -48,3 +48,4 @@ public class GenTemplateDomainFactoryImpl implements GenTemplateDomainFactory {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/util/IdGenerator.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/util/IdGenerator.java
@@ -119,3 +119,4 @@ public class IdGenerator implements BeforeConvertCallback<Object> {
 
 
 
+
