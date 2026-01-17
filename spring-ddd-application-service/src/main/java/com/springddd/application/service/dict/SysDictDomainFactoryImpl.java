64a101cc57a339052235cc4e2372f58fcecfc98d
henrikabel25 <henrikabel@foxmail.com>
2026-01-17 12:33:59 +0800

    feat: add repository tests for dept

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/SysDictDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/SysDictDomainFactoryImpl.java
@@ -58,3 +58,4 @@ public class SysDictDomainFactoryImpl implements SysDictDomainFactory {
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/SysDictItemDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/SysDictItemDomainFactoryImpl.java
@@ -58,3 +58,4 @@ public class SysDictItemDomainFactoryImpl implements SysDictItemDomainFactory {
 
 
 
+
