32af2017569ec73d17727832fa5f790ca776b074
henrikabel25 <henrikabel@foxmail.com>
2026-01-19 22:54:30 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/DeleteGenColumnBindDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/DeleteGenColumnBindDomainServiceImpl.java
@@ -43,3 +43,4 @@ public class DeleteGenColumnBindDomainServiceImpl implements DeleteGenColumnBind
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/user/SysUserDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/user/SysUserDomainFactoryImpl.java
@@ -44,3 +44,4 @@ public class SysUserDomainFactoryImpl implements SysUserDomainFactory {
 
 
 
+
