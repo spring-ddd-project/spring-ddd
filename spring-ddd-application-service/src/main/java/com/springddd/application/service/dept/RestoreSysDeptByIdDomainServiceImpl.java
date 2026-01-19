77cdb58abcdfb9fef1fede30ea82bf5c10986d7a
henrikabel25 <henrikabel@foxmail.com>
2026-01-19 11:55:15 +0800

    test: add repository tests for auth

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dept/RestoreSysDeptByIdDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dept/RestoreSysDeptByIdDomainServiceImpl.java
@@ -79,3 +79,4 @@ public class RestoreSysDeptByIdDomainServiceImpl implements RestoreSysDeptByIdDo
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenColumnsBatchSaveDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenColumnsBatchSaveDomainServiceImpl.java
@@ -112,3 +112,4 @@ public class GenColumnsBatchSaveDomainServiceImpl implements GenColumnsBatchSave
 
 
 
+
