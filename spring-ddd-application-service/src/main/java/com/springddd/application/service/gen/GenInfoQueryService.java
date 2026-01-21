9e476a16f23c3e3de63fc4aad03894eb5d45a072
henrikabel25 <henrikabel@foxmail.com>
2026-01-21 13:11:29 +0800

    feat: add a Query Service for Field Information in Code Generation Management

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenInfoQueryService.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenInfoQueryService.java
@@ -1,15 +1,19 @@
 package com.springddd.application.service.gen;
 
 import com.springddd.application.service.gen.dto.GenInfoPageQuery;
+import com.springddd.application.service.gen.dto.GenInfoQuery;
 import com.springddd.application.service.gen.dto.GenInfoView;
 import com.springddd.application.service.gen.dto.GenInfoViewMapStruct;
+import com.springddd.domain.gen.exception.TableNameNullException;
 import com.springddd.domain.util.PageResponse;
 import com.springddd.infrastructure.persistence.entity.GenInfoEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
 import org.springframework.data.relational.core.query.Criteria;
 import org.springframework.data.relational.core.query.Query;
+import org.springframework.r2dbc.core.DatabaseClient;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
 import reactor.core.publisher.Mono;
 
@@ -23,6 +27,8 @@ public class GenInfoQueryService {
 
     private final GenInfoViewMapStruct genInfoViewMapStruct;
 
+    private final DatabaseClient databaseClient;
+
     public Mono<PageResponse<GenInfoView>> index(GenInfoPageQuery query) {
         Criteria criteria = Criteria.where(GenInfoPageQuery.Fields.deleteStatus).is(false);
         if (!ObjectUtils.isEmpty(query.getTableName())) {
@@ -32,5 +38,56 @@ public class GenInfoQueryService {
         Mono<List<GenInfoView>> list = r2dbcEntityTemplate.select(GenInfoEntity.class).matching(qry).all().collectList().map(genInfoViewMapStruct::toViews);
         Mono<Long> count = r2dbcEntityTemplate.count(Query.query(criteria), GenInfoEntity.class);
         return Mono.zip(list, count).map(tuple -> new PageResponse<>(tuple.getT1(), tuple.getT2(), query.getPageNum(), query.getPageSize()));
+    }
+
+    public Mono<GenInfoView> queryGenInfoByTableName(String tableName) {
+
+        if (ObjectUtils.isEmpty(tableName)) {
+            throw new TableNameNullException();
+        }
+
+        String sql = """
+                SELECT
+                  column_key AS "propColumnKey",
+                  column_name AS "propColumnName",
+                  data_type AS "propColumnType",
+                  column_comment AS "propColumnComment"
+                FROM
+                  information_schema.COLUMNS
+                WHERE
+                  table_name = :tn
+                """;
+
+        DatabaseClient.GenericExecuteSpec dataSpec = databaseClient.sql(sql)
+                .bind("tn", "spring_ddd");
+
+        Mono<List<GenInfoView>> columnData = dataSpec
+                .map((row, meta) -> new GenInfoView(
+                        row.get("propColumnKey", String.class),
+                        row.get("propColumnName", String.class),
+                        row.get("propColumnType", String.class),
+                        row.get("propColumnComment", String.class)
+                ))
+                .all()
+                .collectList();
+
+        Criteria criteria = Criteria
+                .where(GenInfoQuery.Fields.deleteStatus).is(false)
+                .and(GenInfoQuery.Fields.tableName).is(tableName);
+        Query qry = Query.query(criteria);
+        Mono<GenInfoView> genInfo = r2dbcEntityTemplate.select(GenInfoEntity.class).matching(qry).one().map(genInfoViewMapStruct::toView);
+        return Mono.zip(genInfo, columnData)
+                .flatMap(tuple -> {
+                    GenInfoView genInfoView = tuple.getT1();
+                    List<GenInfoView> t2 = tuple.getT2();
+                    if (CollectionUtils.isEmpty(t2)) {
+                        return Mono.empty();
+                    }
+                    genInfoView.setPropColumnName(t2.getFirst().getPropColumnName());
+                    genInfoView.setPropColumnType(t2.getFirst().getPropColumnType());
+                    genInfoView.setPropColumnComment(t2.getFirst().getPropColumnComment());
+                    genInfoView.setPropColumnType(t2.getFirst().getPropColumnType());
+                    return Mono.just(genInfoView);
+                });
     }
 }
