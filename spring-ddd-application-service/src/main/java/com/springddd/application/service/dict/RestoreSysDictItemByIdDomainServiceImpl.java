8788f1cd0e9db8369157dfa8a7cd54a292d9917a
henrikabel25 <henrikabel@foxmail.com>
2026-01-21 18:34:16 +0800

    test: add validation tests for gen

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/RestoreSysDictItemByIdDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/RestoreSysDictItemByIdDomainServiceImpl.java
@@ -56,3 +56,4 @@ public class RestoreSysDictItemByIdDomainServiceImpl implements RestoreSysDictIt
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/dict/DictExtendInfo.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dict/DictExtendInfo.java
@@ -52,3 +52,4 @@ public record DictExtendInfo(Integer sortOrder, Boolean dictStatus) {
 
 
 
+
