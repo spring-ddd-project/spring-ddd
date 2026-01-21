03637c4507018d1a3e3e80cc05a4d1a0ebe08bc3
henrikabel25 <henrikabel@foxmail.com>
2026-01-21 09:38:30 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenColumnBindDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenColumnBindDomainFactoryImpl.java
@@ -50,3 +50,4 @@ public class GenColumnBindDomainFactoryImpl implements GenColumnBindDomainFactor
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/dict/exception/DictDictStatusNullException.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dict/exception/DictDictStatusNullException.java
@@ -24,3 +24,4 @@ public class DictDictStatusNullException extends DomainException {
 
 
 
+
