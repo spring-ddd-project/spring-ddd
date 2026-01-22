0f0189bc499bab6d4c696f3681d24ae614c32cc1
henrikabel25 <henrikabel@foxmail.com>
2026-01-22 12:26:14 +0800

    feat: add code generation for field aggregate root

---
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenColumnsExtendInfo.java
@@ -0,0 +1,41 @@
+package com.springddd.domain.gen;
+
+import com.springddd.domain.gen.exception.*;
+import org.springframework.util.ObjectUtils;
+
+public record GenColumnsExtendInfo(Long propDictId,
+                                   Boolean tableVisible,
+                                   Boolean tableOrder,
+                                   Boolean tableFilter,
+                                   Integer tableFilterComponent,
+                                   Integer tableFilterType,
+                                   Integer formComponent,
+                                   Boolean formVisible,
+                                   Boolean formRequired) {
+    public GenColumnsExtendInfo {
+        if (ObjectUtils.isEmpty(tableVisible)) {
+            throw new VisibleNullException();
+        }
+        if (ObjectUtils.isEmpty(tableOrder)) {
+            throw new OrderNullException();
+        }
+        if (ObjectUtils.isEmpty(tableFilter)) {
+            throw new FilterNullException();
+        }
+        if (ObjectUtils.isEmpty(tableFilterComponent)) {
+            throw new FilterComponentNullException();
+        }
+        if (ObjectUtils.isEmpty(tableFilterType)) {
+            throw new FilterTypeNullException();
+        }
+        if (ObjectUtils.isEmpty(formComponent)) {
+            throw new FormComponentNullException();
+        }
+        if (ObjectUtils.isEmpty(formVisible)) {
+            throw new FormVisibleNullException();
+        }
+        if (ObjectUtils.isEmpty(formRequired)) {
+            throw new FormRequiredNullException();
+        }
+    }
+}
