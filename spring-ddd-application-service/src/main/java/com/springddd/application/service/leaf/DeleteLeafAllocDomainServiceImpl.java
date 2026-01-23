c6770b1bd395c705fb7bd397b7184a11ebcfe5e3
henrikabel25 <henrikabel@foxmail.com>
2026-01-23 17:31:59 +0800

    test: improve coverage for util tests

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/DeleteLeafAllocDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/DeleteLeafAllocDomainServiceImpl.java
@@ -67,3 +67,4 @@ public class DeleteLeafAllocDomainServiceImpl implements DeleteLeafAllocDomainSe
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/role/DeleteSysRoleByIdDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/role/DeleteSysRoleByIdDomainServiceImpl.java
@@ -77,3 +77,4 @@ public class DeleteSysRoleByIdDomainServiceImpl implements DeleteSysRoleByIdDoma
 
 
 
+
