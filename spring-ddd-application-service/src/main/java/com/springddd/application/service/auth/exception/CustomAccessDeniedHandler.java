3d27d9cd0cbcbb3d22c047bf94ee798fc6acbdd4
henrikabel25 <henrikabel@foxmail.com>
2026-01-24 17:10:51 +0800

    feat: add mock tests for dict service

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/exception/CustomAccessDeniedHandler.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/exception/CustomAccessDeniedHandler.java
@@ -70,3 +70,4 @@ public class CustomAccessDeniedHandler implements ServerAccessDeniedHandler {
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/user/SysUserRoleDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/user/SysUserRoleDomainFactoryImpl.java
@@ -61,3 +61,4 @@ public class SysUserRoleDomainFactoryImpl implements SysUserRoleDomainFactory {
 
 
 
+
