c5758632a10ba69394c18727d16fdcd3852b7862
henrikabel25 <henrikabel@foxmail.com>
2026-01-25 18:28:58 +0800

    feat: add edge case tests for menu

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/dto/LeafAllocPageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/dto/LeafAllocPageQuery.java
@@ -66,3 +66,4 @@ public class LeafAllocPageQuery extends LeafAllocQuery implements Serializable {
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/user/dto/SysUserPageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/user/dto/SysUserPageQuery.java
@@ -60,3 +60,4 @@ public class SysUserPageQuery extends SysUserQuery implements Serializable {
 
 
 
+
