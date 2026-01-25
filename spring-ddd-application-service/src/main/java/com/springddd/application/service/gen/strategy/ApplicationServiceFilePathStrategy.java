08e1ca91716c4a10a170434ccedaf063a40ed67b
henrikabel25 <henrikabel@foxmail.com>
2026-01-25 13:26:17 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/strategy/ApplicationServiceFilePathStrategy.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/strategy/ApplicationServiceFilePathStrategy.java
@@ -94,3 +94,4 @@ public class ApplicationServiceFilePathStrategy implements FilePathStrategy {
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/role/LinkRoleAndMenusDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/role/LinkRoleAndMenusDomainServiceImpl.java
@@ -61,3 +61,4 @@ public class LinkRoleAndMenusDomainServiceImpl implements LinkRoleAndMenusDomain
 
 
 
+
