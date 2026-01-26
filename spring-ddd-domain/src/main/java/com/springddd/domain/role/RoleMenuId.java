4b75211f3673684c410a6e7de66ba29f171eff69
henrikabel25 <henrikabel@foxmail.com>
2026-01-26 16:10:50 +0800

    refactor: extract common logic in role

--- spring-ddd-domain/src/main/java/com/springddd/domain/role/RoleMenuId.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/role/RoleMenuId.java
@@ -55,3 +55,4 @@ public record RoleMenuId(Long value) implements AggregateRootId<Long> {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/util/WebfluxGlobalErrorHandler.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/util/WebfluxGlobalErrorHandler.java
@@ -103,3 +103,4 @@ public class WebfluxGlobalErrorHandler implements ErrorWebExceptionHandler {
 
 
 
+
