24fe37d5b2ce19b906aeb8d851489ddeac8eb9a3
henrikabel25 <henrikabel@foxmail.com>
2026-01-26 20:26:35 +0800

    feat: add unit tests for user module

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/exception/CustomAccessDeniedHandler.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/exception/CustomAccessDeniedHandler.java
@@ -74,3 +74,4 @@ public class CustomAccessDeniedHandler implements ServerAccessDeniedHandler {
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenAggregatePageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenAggregatePageQuery.java
@@ -58,3 +58,4 @@ public class GenAggregatePageQuery extends GenAggregateQuery implements Serializ
 
 
 
+
