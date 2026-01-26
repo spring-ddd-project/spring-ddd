1b7531849934330ff84f2cbc2c1bc90e03069fb2
henrikabel25 <henrikabel@foxmail.com>
2026-01-26 17:48:32 +0800

    test: add controller tests for dict

--- spring-ddd-domain/src/main/java/com/springddd/domain/role/RoleBasicInfo.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/role/RoleBasicInfo.java
@@ -62,3 +62,4 @@ public record RoleBasicInfo(String roleName, String roleCode, Integer roleDataSc
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/util/WebfluxGlobalErrorHandler.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/util/WebfluxGlobalErrorHandler.java
@@ -102,3 +102,4 @@ public class WebfluxGlobalErrorHandler implements ErrorWebExceptionHandler {
 
 
 
+
