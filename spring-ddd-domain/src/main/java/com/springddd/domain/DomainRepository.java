f9ed7abfb776da34d09f46c6ad6a838291b5ad92
henrikabel25 <henrikabel@foxmail.com>
2026-01-27 17:08:10 +0800

    test: increase test coverage and refactor

--- spring-ddd-domain/src/main/java/com/springddd/domain/DomainRepository.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/DomainRepository.java
@@ -30,3 +30,4 @@ public interface DomainRepository<T extends AggregateRootId<?>, E extends Abstra
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/role/RoleMenuId.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/role/RoleMenuId.java
@@ -25,3 +25,4 @@ public record RoleMenuId(Long value) implements AggregateRootId<Long> {
 
 
 
+
