9aee73a6b3ac6007367fdf9004a0756a0a5532d0
henrikabel25 <henrikabel@foxmail.com>
2026-01-27 13:33:22 +0800

    refactor: clean up user dependencies

--- spring-ddd-domain/src/main/java/com/springddd/domain/dict/SysDictDomain.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dict/SysDictDomain.java
@@ -120,3 +120,4 @@ public class SysDictDomain extends AbstractDomainMask implements Cloneable {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/menu/SysMenuDomainRepository.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/SysMenuDomainRepository.java
@@ -49,3 +49,4 @@ public interface SysMenuDomainRepository extends DomainRepository<MenuId, SysMen
 
 
 
+
