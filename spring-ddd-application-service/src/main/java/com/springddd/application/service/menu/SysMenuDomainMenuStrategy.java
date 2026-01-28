24fca1a7545cc54891fafe4e9d54a584b443158c
henrikabel25 <henrikabel@foxmail.com>
2026-01-28 18:51:39 +0800

    test: add integration tests for role

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/SysMenuDomainMenuStrategy.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/SysMenuDomainMenuStrategy.java
@@ -62,3 +62,4 @@ public class SysMenuDomainMenuStrategy implements SysMenuDomainStrategy {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/Form.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/Form.java
@@ -70,3 +70,4 @@ public record Form(Byte formComponent, Boolean formVisible, Boolean formRequired
 
 
 
+
