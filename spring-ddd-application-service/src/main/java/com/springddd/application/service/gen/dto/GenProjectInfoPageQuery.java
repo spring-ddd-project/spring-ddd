2a6bff418b1981b1102706976cefe6096984ca13
henrikabel25 <henrikabel@foxmail.com>
2026-01-28 21:18:12 +0800

    test: add mock tests for role service

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenProjectInfoPageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenProjectInfoPageQuery.java
@@ -58,3 +58,4 @@ public class GenProjectInfoPageQuery extends GenProjectInfoQuery implements Seri
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/role/WipeSysRoleByIdsDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/role/WipeSysRoleByIdsDomainServiceImpl.java
@@ -97,3 +97,4 @@ public class WipeSysRoleByIdsDomainServiceImpl implements WipeSysRoleByIdsDomain
 
 
 
+
