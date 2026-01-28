d89d357b739a8d9b51d7af277dad8852615c7f70
henrikabel25 <henrikabel@foxmail.com>
2026-01-28 13:34:18 +0800

    refactor: optimize domain data flow

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/DeleteSysMenuByIdDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/DeleteSysMenuByIdDomainServiceImpl.java
@@ -91,3 +91,4 @@ public class DeleteSysMenuByIdDomainServiceImpl implements DeleteSysMenuByIdDoma
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/menu/exception/MenuPermissionDeniedException.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/exception/MenuPermissionDeniedException.java
@@ -52,3 +52,4 @@ public class MenuPermissionDeniedException extends DomainException {
 
 
 
+
