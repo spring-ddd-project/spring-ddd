024f18ea1c11a286ca0082c94dc1ad969412f71a
henrikabel25 <henrikabel@foxmail.com>
2026-01-28 21:38:10 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenProjectInfoPageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenProjectInfoPageQuery.java
@@ -37,3 +37,4 @@ public class GenProjectInfoPageQuery extends GenProjectInfoQuery implements Seri
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/Table.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/Table.java
@@ -44,3 +44,4 @@ public record Table(Boolean tableVisible, Boolean tableOrder, Boolean tableFilte
 
 
 
+
