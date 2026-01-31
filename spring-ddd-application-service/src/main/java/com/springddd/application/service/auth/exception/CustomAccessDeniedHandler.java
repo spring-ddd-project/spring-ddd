541914c0e4ed1a2adf63fcf28d4c73942728cd11
henrikabel25 <henrikabel@foxmail.com>
2026-01-31 16:28:52 +0800

    refactor: clean up application dependencies

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/exception/CustomAccessDeniedHandler.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/exception/CustomAccessDeniedHandler.java
@@ -75,3 +75,4 @@ public class CustomAccessDeniedHandler implements ServerAccessDeniedHandler {
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dept/RestoreSysDeptByIdDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dept/RestoreSysDeptByIdDomainServiceImpl.java
@@ -88,3 +88,4 @@ public class RestoreSysDeptByIdDomainServiceImpl implements RestoreSysDeptByIdDo
 
 
 
+
