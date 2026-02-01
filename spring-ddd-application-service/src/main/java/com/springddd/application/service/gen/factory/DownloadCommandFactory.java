a69976656eb88262f15542890c424418aba1afd3
henrikabel25 <henrikabel@foxmail.com>
2026-02-01 16:45:25 +0800

    test: add mock tests for cache service

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/factory/DownloadCommandFactory.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/factory/DownloadCommandFactory.java
@@ -57,3 +57,4 @@ public class DownloadCommandFactory implements GenerateCommandFactory {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/dict/SysDictDomain.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dict/SysDictDomain.java
@@ -123,3 +123,4 @@ public class SysDictDomain extends AbstractDomainMask implements Cloneable {
 
 
 
+
