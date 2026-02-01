f759e2a9e41a9f02f0d2c9a79eb105bd35fcc26d
henrikabel25 <henrikabel@foxmail.com>
2026-02-01 20:31:26 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/SysMenuDomainButtonStrategy.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/SysMenuDomainButtonStrategy.java
@@ -45,3 +45,4 @@ public class SysMenuDomainButtonStrategy implements SysMenuDomainStrategy {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/role/RoleScopeConfig.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/role/RoleScopeConfig.java
@@ -25,3 +25,4 @@ public record RoleScopeConfig(List<Long> depts, List<Long> posts, List<Long> use
 
 
 
+
