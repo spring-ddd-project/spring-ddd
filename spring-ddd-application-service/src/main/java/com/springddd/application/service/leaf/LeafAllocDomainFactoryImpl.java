12864630d705ec3acacc793e90fd01018b9fe9f7
henrikabel25 <henrikabel@foxmail.com>
2026-02-02 12:32:10 +0800

    feat: add edge case tests for gen

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/LeafAllocDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/LeafAllocDomainFactoryImpl.java
@@ -63,3 +63,4 @@ public class LeafAllocDomainFactoryImpl implements LeafAllocDomainFactory {
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/user/dto/SysUserRolePageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/user/dto/SysUserRolePageQuery.java
@@ -65,3 +65,4 @@ public class SysUserRolePageQuery extends SysUserRoleQuery implements Serializab
 
 
 
+
