cce35ed38742b6b0757ae867946ef21f7a4d583c
henrikabel25 <henrikabel@foxmail.com>
2026-02-02 19:27:19 +0800

    feat: add unit tests for leaf module

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/strategy/ApplicationServiceFilePathStrategy.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/strategy/ApplicationServiceFilePathStrategy.java
@@ -119,3 +119,4 @@ public class ApplicationServiceFilePathStrategy implements FilePathStrategy {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/role/SysRoleDomain.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/role/SysRoleDomain.java
@@ -129,3 +129,4 @@ public class SysRoleDomain extends AbstractDomainMask implements Cloneable {
 
 
 
+
