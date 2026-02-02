a045e9a51ea161cba0ee0496929424652ae7f488
henrikabel25 <henrikabel@foxmail.com>
2026-02-02 11:00:54 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/SysDictItemQueryService.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/SysDictItemQueryService.java
@@ -99,3 +99,4 @@ public class SysDictItemQueryService extends com.springddd.application.service.A
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/strategy/InterfaceWebFilePathStrategy.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/strategy/InterfaceWebFilePathStrategy.java
@@ -44,3 +44,4 @@ public class InterfaceWebFilePathStrategy implements FilePathStrategy {
 
 
 
+
