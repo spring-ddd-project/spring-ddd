95f8167859fdcc4ed5c5b13407220614a1a9d6c4
henrikabel25 <henrikabel@foxmail.com>
2026-02-02 14:14:36 +0800

    refactor: consolidate dept utilities

--- spring-ddd-domain/src/main/java/com/springddd/domain/DomainRepository.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/DomainRepository.java
@@ -64,3 +64,4 @@ public interface DomainRepository<T extends AggregateRootId<?>, E extends Abstra
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/dict/DictExtendInfo.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dict/DictExtendInfo.java
@@ -61,3 +61,4 @@ public record DictExtendInfo(Integer sortOrder, Boolean dictStatus) {
 
 
 
+
