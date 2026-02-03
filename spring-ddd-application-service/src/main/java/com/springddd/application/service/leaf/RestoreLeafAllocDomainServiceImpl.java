9bea35272c410feb0fd24526d596ebedc4160a04
henrikabel25 <henrikabel@foxmail.com>
2026-02-03 11:30:40 +0800

    test: add edge case tests for gen

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/RestoreLeafAllocDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/RestoreLeafAllocDomainServiceImpl.java
@@ -66,3 +66,4 @@ public class RestoreLeafAllocDomainServiceImpl implements RestoreLeafAllocDomain
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/dept/DeptExtendInfo.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dept/DeptExtendInfo.java
@@ -64,3 +64,4 @@ public record DeptExtendInfo(Integer sortOrder, Boolean deptStatus) {
 
 
 
+
