dca60dbe058aa4fb6e5a4aba1caa08b7e732e666
henrikabel25 <henrikabel@foxmail.com>
2026-02-03 13:32:14 +0800

    feat: add domain logic tests for menu

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenColumnsBatchSaveDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenColumnsBatchSaveDomainServiceImpl.java
@@ -126,3 +126,4 @@ public class GenColumnsBatchSaveDomainServiceImpl implements GenColumnsBatchSave
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/role/dto/SysRoleMenuPageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/role/dto/SysRoleMenuPageQuery.java
@@ -62,3 +62,4 @@ public class SysRoleMenuPageQuery extends SysRoleMenuQuery implements Serializab
 
 
 
+
