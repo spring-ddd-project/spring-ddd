3ace553459d02f347675e9c5346a70aaf1a6968e
henrikabel25 <henrikabel@foxmail.com>
2026-02-04 10:54:46 +0800

    feat: add Command service for aggregate root in codegen

---
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenAggregateDomainFactoryImpl.java
@@ -0,0 +1,17 @@
+package com.springddd.application.service.gen;
+
+import com.springddd.domain.gen.*;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GenAggregateDomainFactoryImpl implements GenAggregateDomainFactory {
+
+    @Override
+    public GenAggregateDomain newInstance(GenProjectInfoId infoId, GenAggregateBasicInfo basicInfo, GenAggregateExtendInfo extendInfo) {
+        GenAggregateDomain domain = new GenAggregateDomain();
+        domain.setInfoId(infoId);
+        domain.setBasicInfo(basicInfo);
+        domain.setExtendInfo(extendInfo);
+        return domain;
+    }
+}
