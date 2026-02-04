ac148407b7f21c79db2f9cc93a501e89a9a090b9
henrikabel25 <henrikabel@foxmail.com>
2026-02-04 13:11:29 +0800

    refactor: improve leaf error handling

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenColumnsDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenColumnsDomainFactoryImpl.java
@@ -67,3 +67,4 @@ public class GenColumnsDomainFactoryImpl implements GenColumnsDomainFactory {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/AggregateId.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/AggregateId.java
@@ -52,3 +52,4 @@ public record AggregateId(Long value) implements AggregateRootId<Long> {
 
 
 
+
