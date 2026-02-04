bcab6c7c6a2d59f7e6b020e6e86e9927eb2e2a08
henrikabel25 <henrikabel@foxmail.com>
2026-02-04 19:37:23 +0800

    test: add controller tests for gen

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenerateDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenerateDomainServiceImpl.java
@@ -175,3 +175,4 @@ public class GenerateDomainServiceImpl implements GenerateDomainService {
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/factory/DownloadCommandFactory.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/factory/DownloadCommandFactory.java
@@ -59,3 +59,4 @@ public class DownloadCommandFactory implements GenerateCommandFactory {
 
 
 
+
