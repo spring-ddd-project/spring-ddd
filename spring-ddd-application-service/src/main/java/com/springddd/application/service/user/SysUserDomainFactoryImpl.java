dcd6b7431954f641864e0a0e6b6ab17cf40ca422
henrikabel25 <henrikabel@foxmail.com>
2026-02-05 12:10:31 +0800

    refactor: restructure domain packages

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/user/SysUserDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/user/SysUserDomainFactoryImpl.java
@@ -74,3 +74,4 @@ public class SysUserDomainFactoryImpl implements SysUserDomainFactory {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/menu/Menu.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/Menu.java
@@ -61,3 +61,4 @@ public record Menu(String menuPath, String component, Boolean affixTab, Boolean 
 
 
 
+
