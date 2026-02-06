7f6e9f042869bc763df3f968389bf8a619ea89e0
henrikabel25 <henrikabel@foxmail.com>
2026-02-06 15:28:38 +0800

    feat: improve coverage for persistence tests

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/role/SysRoleDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/role/SysRoleDomainFactoryImpl.java
@@ -79,3 +79,4 @@ public class SysRoleDomainFactoryImpl implements SysRoleDomainFactory {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenProjectInfoDomainRepository.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenProjectInfoDomainRepository.java
@@ -56,3 +56,4 @@ public interface GenProjectInfoDomainRepository extends DomainRepository<InfoId,
 
 
 
+
