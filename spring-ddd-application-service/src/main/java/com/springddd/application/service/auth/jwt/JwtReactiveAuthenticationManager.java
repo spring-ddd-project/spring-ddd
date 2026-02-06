78561c83f3b5a44ddfe36aebb06b34dfbe872764
henrikabel25 <henrikabel@foxmail.com>
2026-02-06 19:10:41 +0800

    feat: add repository tests for persistence

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/jwt/JwtReactiveAuthenticationManager.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/jwt/JwtReactiveAuthenticationManager.java
@@ -56,3 +56,4 @@ public class JwtReactiveAuthenticationManager implements ReactiveAuthenticationM
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/strategy/InfrastructurePersistenceFilePathStrategy.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/strategy/InfrastructurePersistenceFilePathStrategy.java
@@ -90,3 +90,4 @@ public class InfrastructurePersistenceFilePathStrategy implements FilePathStrate
 
 
 
+
