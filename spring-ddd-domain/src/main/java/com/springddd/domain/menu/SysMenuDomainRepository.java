201cd635ac814cf1839aca24a34210cfb4e14a04
henrikabel25 <henrikabel@foxmail.com>
2026-02-07 19:54:46 +0800

    refactor: consolidate infrastructure utilities

--- spring-ddd-domain/src/main/java/com/springddd/domain/menu/SysMenuDomainRepository.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/SysMenuDomainRepository.java
@@ -53,3 +53,4 @@ public interface SysMenuDomainRepository extends DomainRepository<MenuId, SysMen
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/user/SysUserRoleDomainRepository.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/user/SysUserRoleDomainRepository.java
@@ -62,3 +62,4 @@ public interface SysUserRoleDomainRepository extends DomainRepository<UserRoleId
 
 
 
+
