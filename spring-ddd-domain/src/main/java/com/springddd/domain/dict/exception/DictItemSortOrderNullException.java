ddb9982848550d1b0148bb1f32426e6a9970f357
henrikabel25 <henrikabel@foxmail.com>
2026-02-07 13:28:37 +0800

    refactor: extract common logic in cache

--- spring-ddd-domain/src/main/java/com/springddd/domain/dict/exception/DictItemSortOrderNullException.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dict/exception/DictItemSortOrderNullException.java
@@ -61,3 +61,4 @@ public class DictItemSortOrderNullException extends DomainException {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/util/WebfluxGlobalErrorHandler.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/util/WebfluxGlobalErrorHandler.java
@@ -108,3 +108,4 @@ public class WebfluxGlobalErrorHandler implements ErrorWebExceptionHandler {
 
 
 
+
