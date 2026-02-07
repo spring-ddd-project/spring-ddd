5a1f7c9913870596cf18fd787b23eaf153a4fa3e
henrikabel25 <henrikabel@foxmail.com>
2026-02-07 11:30:40 +0800

    test: add unit tests for infrastructure module

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/RestoreSysDictItemByIdDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/RestoreSysDictItemByIdDomainServiceImpl.java
@@ -77,3 +77,4 @@ public class RestoreSysDictItemByIdDomainServiceImpl implements RestoreSysDictIt
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/WipeGenDataDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/WipeGenDataDomainServiceImpl.java
@@ -69,3 +69,4 @@ public class WipeGenDataDomainServiceImpl implements WipeGenDataDomainService {
 
 
 
+
