2cee031aed0210bab197b9103c9ae088f9358ac2
henrikabel25 <henrikabel@foxmail.com>
2026-02-08 16:20:50 +0800

    test: add domain logic tests for cache

--- spring-ddd-domain/src/main/java/com/springddd/domain/dict/exception/DictItemSortOrderNullException.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dict/exception/DictItemSortOrderNullException.java
@@ -62,3 +62,4 @@ public class DictItemSortOrderNullException extends DomainException {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/user/SysUserDomain.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/user/SysUserDomain.java
@@ -148,3 +148,4 @@ public class SysUserDomain extends AbstractDomainMask implements Cloneable {
 
 
 
+
