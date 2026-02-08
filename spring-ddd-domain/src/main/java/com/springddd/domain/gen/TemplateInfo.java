4b06e6f4c4b5746c091d8d23b694b9f16c3a2889
henrikabel25 <henrikabel@foxmail.com>
2026-02-08 11:29:21 +0800

    feat: add exception for codegen template domain model

--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/TemplateInfo.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/TemplateInfo.java
@@ -1,4 +1,17 @@
 package com.springddd.domain.gen;
 
+import com.springddd.domain.gen.exception.TemplateContentNullException;
+import com.springddd.domain.gen.exception.TemplateNameNullException;
+import org.springframework.util.ObjectUtils;
+
 public record TemplateInfo(String templateName, String templateContent) {
+
+    public TemplateInfo {
+        if (ObjectUtils.isEmpty(templateName)) {
+            throw new TemplateNameNullException();
+        }
+        if (ObjectUtils.isEmpty(templateContent)) {
+            throw new TemplateContentNullException();
+        }
+    }
 }
