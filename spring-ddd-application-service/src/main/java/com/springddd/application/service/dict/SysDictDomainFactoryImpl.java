d41e9386331e5f61ce0aa544136fffafd571af1b
henrikabel25 <henrikabel@foxmail.com>
2026-02-08 10:58:36 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/SysDictDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/SysDictDomainFactoryImpl.java
@@ -43,3 +43,4 @@ public class SysDictDomainFactoryImpl implements SysDictDomainFactory {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/menu/AdvancedOptions.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/AdvancedOptions.java
@@ -40,3 +40,4 @@ public record AdvancedOptions(Integer order, String icon, Integer menuType, Bool
 
 
 
+
