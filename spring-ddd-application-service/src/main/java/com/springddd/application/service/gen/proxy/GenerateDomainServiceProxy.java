c14ed5d3d1dc3e760222afb855ae346afd5b2cdd
henrikabel25 <henrikabel@foxmail.com>
2026-02-09 16:10:46 +0800

    feat: add edge case tests for dict

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/proxy/GenerateDomainServiceProxy.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/proxy/GenerateDomainServiceProxy.java
@@ -67,3 +67,4 @@ public class GenerateDomainServiceProxy implements GenerateDomainService {
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/SysMenuDomainCatalogStrategy.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/SysMenuDomainCatalogStrategy.java
@@ -81,3 +81,4 @@ public class SysMenuDomainCatalogStrategy implements SysMenuDomainStrategy {
 
 
 
+
