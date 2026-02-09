e7a47d2c3d6f895be056acb43b4cce486f13f4f9
henrikabel25 <henrikabel@foxmail.com>
2026-02-09 20:15:55 +0800

    refactor: clean up auth dependencies

--- spring-ddd-domain/src/main/java/com/springddd/domain/dict/SysDictItemDomainRepository.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dict/SysDictItemDomainRepository.java
@@ -44,3 +44,4 @@ public interface SysDictItemDomainRepository extends DomainRepository<DictItemId
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/menu/exception/MenuPermissionNullException.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/exception/MenuPermissionNullException.java
@@ -62,3 +62,4 @@ public class MenuPermissionNullException extends DomainException {
 
 
 
+
