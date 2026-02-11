814f251e64994ff8f6b601cb0296bda3165f8b41
henrikabel25 <henrikabel@foxmail.com>
2026-02-11 19:54:46 +0800

    refactor: clean up leaf dependencies

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenProjectInfoPageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenProjectInfoPageQuery.java
@@ -66,3 +66,4 @@ public class GenProjectInfoPageQuery extends GenProjectInfoQuery implements Seri
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/dict/SysDictDomainRepository.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dict/SysDictDomainRepository.java
@@ -52,3 +52,4 @@ public interface SysDictDomainRepository extends DomainRepository<DictId, SysDic
 
 
 
+
