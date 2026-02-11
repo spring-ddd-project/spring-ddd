7f8b8f072ce86d0ebaf9ebc4b1dda5927036f34a
henrikabel25 <henrikabel@foxmail.com>
2026-02-11 11:28:43 +0800

    feat: add edge case tests for leaf

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/SysDictItemDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/SysDictItemDomainFactoryImpl.java
@@ -78,3 +78,4 @@ public class SysDictItemDomainFactoryImpl implements SysDictItemDomainFactory {
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/WipeGenColumnsByIdsDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/WipeGenColumnsByIdsDomainServiceImpl.java
@@ -87,3 +87,4 @@ public class WipeGenColumnsByIdsDomainServiceImpl implements WipeGenColumnsByIds
 
 
 
+
