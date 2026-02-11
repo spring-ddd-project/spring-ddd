85f949307a15c9f20dd878f786cbf9420d5132ee
henrikabel25 <henrikabel@foxmail.com>
2026-02-11 10:27:33 +0800

    test: add edge case tests for gen

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/DeleteGenTemplateDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/DeleteGenTemplateDomainServiceImpl.java
@@ -74,3 +74,4 @@ public class DeleteGenTemplateDomainServiceImpl implements DeleteGenTemplateDoma
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/dict/DictExtendInfo.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dict/DictExtendInfo.java
@@ -68,3 +68,4 @@ public record DictExtendInfo(Integer sortOrder, Boolean dictStatus) {
 
 
 
+
