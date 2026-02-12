c352ddd7dfeb649e69bc88a021b08d7414646db3
henrikabel25 <henrikabel@foxmail.com>
2026-02-12 21:10:55 +0800

    feat: add validation tests for util

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/UpdateCacheDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/UpdateCacheDomainServiceImpl.java
@@ -152,3 +152,4 @@ public class UpdateCacheDomainServiceImpl implements UpdateCacheDomainService {
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/SysMenuDomainMenuStrategy.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/menu/SysMenuDomainMenuStrategy.java
@@ -75,3 +75,4 @@ public class SysMenuDomainMenuStrategy implements SysMenuDomainStrategy {
 
 
 
+
