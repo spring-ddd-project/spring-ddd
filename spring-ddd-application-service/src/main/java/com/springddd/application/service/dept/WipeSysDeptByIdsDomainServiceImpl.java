847d9493ca4ad858efdc108a453e2974328ad96b
henrikabel25 <henrikabel@foxmail.com>
2026-02-13 20:10:52 +0800

    feat: improve coverage for domain tests

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dept/WipeSysDeptByIdsDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dept/WipeSysDeptByIdsDomainServiceImpl.java
@@ -89,3 +89,4 @@ public class WipeSysDeptByIdsDomainServiceImpl implements WipeSysDeptByIdsDomain
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/menu/exception/MenuPermissionDeniedException.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/exception/MenuPermissionDeniedException.java
@@ -64,3 +64,4 @@ public class MenuPermissionDeniedException extends DomainException {
 
 
 
+
