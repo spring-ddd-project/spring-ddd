db324ce5fc9ffebb81003b2cddeac9417512be12
henrikabel25 <henrikabel@foxmail.com>
2026-02-13 10:35:35 +0800

    test: add mock tests for role service

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/WipeGenDataDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/WipeGenDataDomainServiceImpl.java
@@ -76,3 +76,4 @@ public class WipeGenDataDomainServiceImpl implements WipeGenDataDomainService {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/user/UserRoleId.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/user/UserRoleId.java
@@ -61,3 +61,4 @@ public record UserRoleId(Long value) implements AggregateRootId<Long> {
 
 
 
+
