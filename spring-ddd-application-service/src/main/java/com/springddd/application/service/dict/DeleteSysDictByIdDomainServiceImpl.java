73abe33da3492efa0c027233b1c6f6739968c266
henrikabel25 <henrikabel@foxmail.com>
2026-02-13 11:16:17 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/DeleteSysDictByIdDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/DeleteSysDictByIdDomainServiceImpl.java
@@ -66,3 +66,4 @@ public class DeleteSysDictByIdDomainServiceImpl implements DeleteSysDictByIdDoma
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/menu/SysMenuDomain.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/SysMenuDomain.java
@@ -115,3 +115,4 @@ public class SysMenuDomain extends AbstractDomainMask implements Cloneable {
 
 
 
+
