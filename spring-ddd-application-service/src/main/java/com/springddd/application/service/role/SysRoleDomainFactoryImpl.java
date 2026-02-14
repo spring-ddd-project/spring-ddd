b5f6e3e75f028d01092ba7521ad0ea46e7169d53
henrikabel25 <henrikabel@foxmail.com>
2026-02-14 15:17:43 +0800

    test: improve coverage for domain tests

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/role/SysRoleDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/role/SysRoleDomainFactoryImpl.java
@@ -85,3 +85,4 @@ public class SysRoleDomainFactoryImpl implements SysRoleDomainFactory {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenColumnBindBasicInfo.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenColumnBindBasicInfo.java
@@ -73,3 +73,4 @@ public record GenColumnBindBasicInfo(String columnType, String entityType, Byte 
 
 
 
+
