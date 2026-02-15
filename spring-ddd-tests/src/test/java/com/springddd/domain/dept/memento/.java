2f58717893b96676183c593c2ed33e31311dc014
henrikabel25 <henrikabel@foxmail.com>
2026-02-15 19:30:37 +0800

    feat: add controller tests for leaf

---
+++ spring-ddd-tests/src/test/java/com/springddd/domain/dept/memento/.java
@@ -0,0 +1,9 @@
+package com.springddd.domain.dept.memento;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class  {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
