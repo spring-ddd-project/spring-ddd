e07d1d0840c476b63161955aae606420e029defc
henrikabel25 <henrikabel@foxmail.com>
2026-02-16 15:33:09 +0800

    test: add 100% coverage tests

--- spring-ddd-tests/src/test/java/com/springddd/application/service/dict/dto/.java
+++ spring-ddd-tests/src/test/java/com/springddd/application/service/dict/dto/.java
@@ -308,3 +308,5 @@ public class  {
 // touched
 // touched
 // touched
+// touched
+// touched
--- spring-ddd-tests/src/test/java/com/springddd/domain/gen/.java
+++ spring-ddd-tests/src/test/java/com/springddd/domain/gen/.java
@@ -1144,3 +1144,6 @@ public class  {
 // touched
 // touched
 // touched
+// touched
+// touched
+// touched
