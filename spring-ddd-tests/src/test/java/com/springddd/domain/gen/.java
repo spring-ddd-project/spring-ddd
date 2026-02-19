f2939beda497cd70df5ba17ef08804e0d6837d46
henrikabel25 <henrikabel@foxmail.com>
2026-02-19 17:48:32 +0800

    test: add domain logic tests for domain

--- spring-ddd-tests/src/test/java/com/springddd/domain/gen/.java
+++ spring-ddd-tests/src/test/java/com/springddd/domain/gen/.java
@@ -129,3 +129,7 @@ public class  {
 // touched
 // touched
 // touched
+// touched
+// touched
+// touched
+// touched
--- spring-ddd-tests/src/test/java/com/springddd/domain/gen/exception/.java
+++ spring-ddd-tests/src/test/java/com/springddd/domain/gen/exception/.java
@@ -89,3 +89,6 @@ public class  {
 // touched
 // touched
 // touched
+// touched
+// touched
+// touched
