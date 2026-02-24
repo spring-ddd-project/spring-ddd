972909e426e30c4568f9c6cdc58b6569d79c57bb
henrikabel25 <henrikabel@foxmail.com>
2026-02-24 19:07:14 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/strategy/InterfaceWebFilePathStrategy.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/strategy/InterfaceWebFilePathStrategy.java
@@ -51,3 +51,4 @@ public class InterfaceWebFilePathStrategy implements FilePathStrategy {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/role/RoleScopeConfig.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/role/RoleScopeConfig.java
@@ -35,3 +35,4 @@ public record RoleScopeConfig(List<Long> depts, List<Long> posts, List<Long> use
 
 
 
+
