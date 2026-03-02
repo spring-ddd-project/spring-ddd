744ccd699bf42ab9aed86ef18db9afbb6b22c47c
henrikabel25 <henrikabel@foxmail.com>
2026-03-02 19:35:37 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/RestoreGenTemplateDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/RestoreGenTemplateDomainServiceImpl.java
@@ -53,3 +53,4 @@ public class RestoreGenTemplateDomainServiceImpl implements RestoreGenTemplateDo
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/role/dto/SysRolePageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/role/dto/SysRolePageQuery.java
@@ -58,3 +58,4 @@ public class SysRolePageQuery extends SysRoleQuery implements Serializable {
 
 
 
+
