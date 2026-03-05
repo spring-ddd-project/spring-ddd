068915f54d41546652fbb66f75e2f7cc3abd6ef0
henrikabel25 <henrikabel@foxmail.com>
2026-03-05 16:10:13 +0800

    feat: add unit tests for user module

--- spring-ddd-tests/src/test/java/com/springddd/domain/dict/exception/.java
+++ spring-ddd-tests/src/test/java/com/springddd/domain/dict/exception/.java
@@ -124,3 +124,5 @@ public class  {
 // touched
 // touched
 // touched
+// touched
+// touched
--- spring-ddd-tests/src/test/java/com/springddd/domain/gen/.java
+++ spring-ddd-tests/src/test/java/com/springddd/domain/gen/.java
@@ -470,3 +470,6 @@ public class  {
 // touched
 // touched
 // touched
+// touched
+// touched
+// touched
