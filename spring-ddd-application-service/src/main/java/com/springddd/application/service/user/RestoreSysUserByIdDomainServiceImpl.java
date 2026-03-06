b874f6de0d5e51c4e0bea1c662747bb011603f90
henrikabel25 <henrikabel@foxmail.com>
2026-03-06 15:13:29 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/user/RestoreSysUserByIdDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/user/RestoreSysUserByIdDomainServiceImpl.java
@@ -57,3 +57,4 @@ public class RestoreSysUserByIdDomainServiceImpl implements RestoreSysUserByIdDo
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/user/UserId.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/user/UserId.java
@@ -45,3 +45,4 @@ public record UserId(Long value) implements AggregateRootId<Long> {
 
 
 
+
