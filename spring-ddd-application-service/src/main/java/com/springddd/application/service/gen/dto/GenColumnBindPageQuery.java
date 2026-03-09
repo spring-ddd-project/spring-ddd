3504eed7df9a1bc6f881baa013b4afac42d33c94
henrikabel25 <henrikabel@foxmail.com>
2026-03-09 09:36:05 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenColumnBindPageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenColumnBindPageQuery.java
@@ -57,3 +57,4 @@ public class GenColumnBindPageQuery extends GenColumnBindQuery implements Serial
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/user/strategy/ReplaceRoleAssignmentStrategy.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/user/strategy/ReplaceRoleAssignmentStrategy.java
@@ -57,3 +57,4 @@ public class ReplaceRoleAssignmentStrategy implements RoleAssignmentStrategy {
 
 
 
+
