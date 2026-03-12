90490dcfecf5f7e62e804430f6677641f2a6e427
henrikabel25 <henrikabel@foxmail.com>
2026-03-12 19:34:24 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/jwt/JwtAuthenticationConverter.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/auth/jwt/JwtAuthenticationConverter.java
@@ -129,3 +129,4 @@ public class JwtAuthenticationConverter implements ServerAuthenticationConverter
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/WipeGenDataDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/WipeGenDataDomainServiceImpl.java
@@ -60,3 +60,4 @@ public class WipeGenDataDomainServiceImpl implements WipeGenDataDomainService {
 
 
 
+
