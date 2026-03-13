a869f57e18a427cf6c628f89f6952573e34db757
henrikabel25 <henrikabel@foxmail.com>
2026-03-13 11:23:03 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenColumnsDomainFactoryImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenColumnsDomainFactoryImpl.java
@@ -60,3 +60,4 @@ public class GenColumnsDomainFactoryImpl implements GenColumnsDomainFactory {
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/role/LinkRoleAndMenusDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/role/LinkRoleAndMenusDomainServiceImpl.java
@@ -75,3 +75,4 @@ public class LinkRoleAndMenusDomainServiceImpl implements LinkRoleAndMenusDomain
 
 
 
+
