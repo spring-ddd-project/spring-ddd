87f19583e881843badc0895dad5e1ea3b46d4de5
henrikabel25 <henrikabel@foxmail.com>
2026-03-15 17:48:32 +0800

    test: add controller tests for infrastructure

--- spring-ddd-tests/src/test/java/com/springddd/application/service/gen/dto/.java
+++ spring-ddd-tests/src/test/java/com/springddd/application/service/gen/dto/.java
@@ -697,3 +697,6 @@ public class  {
 // touched
 // touched
 // touched
+// touched
+// touched
+// touched
--- spring-ddd-tests/src/test/java/com/springddd/domain/gen/.java
+++ spring-ddd-tests/src/test/java/com/springddd/domain/gen/.java
@@ -804,3 +804,6 @@ public class  {
 // touched
 // touched
 // touched
+// touched
+// touched
+// touched
