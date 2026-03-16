cb16a33b7cbf67e6c5c778ca72b6fcafbb71637b
henrikabel25 <henrikabel@foxmail.com>
2026-03-16 09:48:10 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/strategy/InterfaceWebFilePathStrategy.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/strategy/InterfaceWebFilePathStrategy.java
@@ -61,3 +61,4 @@ public class InterfaceWebFilePathStrategy implements FilePathStrategy {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/leaf/LeafAllocDomainRepository.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/leaf/LeafAllocDomainRepository.java
@@ -51,3 +51,4 @@ public interface LeafAllocDomainRepository extends DomainRepository<LeafId, Leaf
 
 
 
+
