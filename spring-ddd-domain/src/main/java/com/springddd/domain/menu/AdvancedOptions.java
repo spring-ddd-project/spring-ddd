b43f4184bf3764db9fed7a4189eb46b8430451b4
henrikabel25 <henrikabel@foxmail.com>
2026-03-18 13:48:22 +0800

    test: increase test coverage and refactor

--- spring-ddd-domain/src/main/java/com/springddd/domain/menu/AdvancedOptions.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/AdvancedOptions.java
@@ -53,3 +53,4 @@ public record AdvancedOptions(Integer order, String icon, Integer menuType, Bool
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/menu/exception/MenuPermissionDeniedException.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/exception/MenuPermissionDeniedException.java
@@ -44,3 +44,4 @@ public class MenuPermissionDeniedException extends DomainException {
 
 
 
+
