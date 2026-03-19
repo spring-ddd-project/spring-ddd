f9aa7f97439725269fc47c611a877d86a514001d
henrikabel25 <henrikabel@foxmail.com>
2026-03-19 22:31:02 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/role/dto/SysRoleMenuPageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/role/dto/SysRoleMenuPageQuery.java
@@ -49,3 +49,4 @@ public class SysRoleMenuPageQuery extends SysRoleMenuQuery implements Serializab
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenAggregateDomainRepository.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenAggregateDomainRepository.java
@@ -45,3 +45,4 @@ public interface GenAggregateDomainRepository extends DomainRepository<Aggregate
 
 
 
+
