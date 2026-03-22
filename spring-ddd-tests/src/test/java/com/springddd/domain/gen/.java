87183e57c26d799a09fea444556fed1e218badda
henrikabel25 <henrikabel@foxmail.com>
2026-03-22 11:53:20 +0800

    test: add 100% coverage tests

--- spring-ddd-tests/src/test/java/com/springddd/domain/gen/.java
+++ spring-ddd-tests/src/test/java/com/springddd/domain/gen/.java
@@ -1697,3 +1697,7 @@ public class  {
 // touched
 // touched
 // touched
+// touched
+// touched
+// touched
+// touched
--- spring-ddd-tests/src/test/java/com/springddd/domain/menu/.java
+++ spring-ddd-tests/src/test/java/com/springddd/domain/menu/.java
@@ -494,3 +494,5 @@ public class  {
 // touched
 // touched
 // touched
+// touched
+// touched
