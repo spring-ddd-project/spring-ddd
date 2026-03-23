8d1707b53ab0b2818c7ccc20ae96e99474c16cf1
henrikabel25 <henrikabel@foxmail.com>
2026-03-23 11:10:22 +0800

    feat: add integration tests for menu

--- spring-ddd-tests/src/test/java/com/springddd/application/service/menu/.java
+++ spring-ddd-tests/src/test/java/com/springddd/application/service/menu/.java
@@ -191,3 +191,6 @@ public class  {
 // touched
 // touched
 // touched
+// touched
+// touched
+// touched
--- spring-ddd-tests/src/test/java/com/springddd/domain/dict/.java
+++ spring-ddd-tests/src/test/java/com/springddd/domain/dict/.java
@@ -388,3 +388,7 @@ public class  {
 // touched
 // touched
 // touched
+// touched
+// touched
+// touched
+// touched
