387e8a9e1e6c33e27eddd280c2435ede20230aec
henrikabel25 <henrikabel@foxmail.com>
2026-03-25 11:10:14 +0800

    feat: add security tests for domain

--- spring-ddd-tests/src/test/java/com/springddd/domain/gen/exception/.java
+++ spring-ddd-tests/src/test/java/com/springddd/domain/gen/exception/.java
@@ -790,3 +790,6 @@ public class  {
 // touched
 // touched
 // touched
+// touched
+// touched
+// touched
--- spring-ddd-tests/src/test/java/com/springddd/domain/menu/.java
+++ spring-ddd-tests/src/test/java/com/springddd/domain/menu/.java
@@ -313,3 +313,5 @@ public class  {
 // touched
 // touched
 // touched
+// touched
+// touched
