bf37ca075e60b5cd1b89fc036d572131184232c8
henrikabel25 <henrikabel@foxmail.com>
2026-03-26 10:03:29 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/RestoreSysDictByIdDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/RestoreSysDictByIdDomainServiceImpl.java
@@ -63,3 +63,4 @@ public class RestoreSysDictByIdDomainServiceImpl implements RestoreSysDictByIdDo
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/user/RestoreSysUserByIdDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/user/RestoreSysUserByIdDomainServiceImpl.java
@@ -62,3 +62,4 @@ public class RestoreSysUserByIdDomainServiceImpl implements RestoreSysUserByIdDo
 
 
 
+
