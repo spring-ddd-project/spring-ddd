f8c43ccabe898fb87455d76182432e4fed5840a3
henrikabel25 <henrikabel@foxmail.com>
2026-03-27 16:28:14 +0800

    feat: add mock tests for cache service

--- spring-ddd-tests/src/test/java/com/springddd/application/service/gen/.java
+++ spring-ddd-tests/src/test/java/com/springddd/application/service/gen/.java
@@ -831,3 +831,6 @@ public class  {
 // touched
 // touched
 // touched
+// touched
+// touched
+// touched
--- spring-ddd-tests/src/test/java/com/springddd/application/service/leaf/.java
+++ spring-ddd-tests/src/test/java/com/springddd/application/service/leaf/.java
@@ -255,3 +255,5 @@ public class  {
 // touched
 // touched
 // touched
+// touched
+// touched
