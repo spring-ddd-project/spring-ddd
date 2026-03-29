050dd7536d41738b40617bb40b33704141bc0739
henrikabel25 <henrikabel@foxmail.com>
2026-03-29 19:10:51 +0800

    feat: add integration tests for leaf

--- spring-ddd-tests/src/test/java/com/springddd/application/service/gen/.java
+++ spring-ddd-tests/src/test/java/com/springddd/application/service/gen/.java
@@ -883,3 +883,7 @@ public class  {
 // touched
 // touched
 // touched
+// touched
+// touched
+// touched
+// touched
--- spring-ddd-tests/src/test/java/com/springddd/application/service/gen/dto/.java
+++ spring-ddd-tests/src/test/java/com/springddd/application/service/gen/dto/.java
@@ -982,3 +982,6 @@ public class  {
 // touched
 // touched
 // touched
+// touched
+// touched
+// touched
