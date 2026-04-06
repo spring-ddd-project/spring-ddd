851ec8a027835d69f728c4235725c4297c2a0f4b
henrikabel25 <henrikabel@foxmail.com>
2026-04-06 11:28:31 +0800

    feat: add unit tests for role module

--- spring-ddd-tests/src/test/java/com/springddd/application/service/gen/.java
+++ spring-ddd-tests/src/test/java/com/springddd/application/service/gen/.java
@@ -1043,3 +1043,5 @@ public class  {
 // touched
 // touched
 // touched
+// touched
+// touched
--- spring-ddd-tests/src/test/java/com/springddd/application/service/gen/dto/.java
+++ spring-ddd-tests/src/test/java/com/springddd/application/service/gen/dto/.java
@@ -1175,3 +1175,7 @@ public class  {
 // touched
 // touched
 // touched
+// touched
+// touched
+// touched
+// touched
