f594efb38a972c9fddd73016aa989a67c01d6ec1
henrikabel25 <henrikabel@foxmail.com>
2026-04-11 22:51:39 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/adapter/FreemarkerTemplateAdapter.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/adapter/FreemarkerTemplateAdapter.java
@@ -87,3 +87,4 @@ public class FreemarkerTemplateAdapter implements TemplateEngineAdapter {
 
 
 
+
--- spring-ddd-application-service/src/main/java/com/springddd/application/service/role/WipeSysRoleMenuByIdsDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/role/WipeSysRoleMenuByIdsDomainServiceImpl.java
@@ -61,3 +61,4 @@ public class WipeSysRoleMenuByIdsDomainServiceImpl implements WipeSysRoleMenuByI
 
 
 
+
