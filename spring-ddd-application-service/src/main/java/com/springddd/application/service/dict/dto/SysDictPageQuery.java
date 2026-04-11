77115db118d74b1e5a27ef887469a7563a0cd103
henrikabel25 <henrikabel@foxmail.com>
2026-04-11 12:53:35 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/dto/SysDictPageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/dict/dto/SysDictPageQuery.java
@@ -73,3 +73,4 @@ public class SysDictPageQuery extends SysDictQuery implements Serializable {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/role/RoleMenuId.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/role/RoleMenuId.java
@@ -60,3 +60,4 @@ public record RoleMenuId(Long value) implements AggregateRootId<Long> {
 
 
 
+
