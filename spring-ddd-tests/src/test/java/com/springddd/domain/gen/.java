a1274d9b21b49ef11895221fc97d6ac19c5dd18b
henrikabel25 <henrikabel@foxmail.com>
2026-04-12 21:10:43 +0800

    feat: add repository tests for leaf

--- spring-ddd-tests/src/test/java/com/springddd/domain/gen/.java
+++ spring-ddd-tests/src/test/java/com/springddd/domain/gen/.java
@@ -1538,3 +1538,5 @@ public class  {
 // touched
 // touched
 // touched
+// touched
+// touched
--- spring-ddd-tests/src/test/java/com/springddd/domain/role/.java
+++ spring-ddd-tests/src/test/java/com/springddd/domain/role/.java
@@ -718,3 +718,6 @@ public class  {
 // touched
 // touched
 // touched
+// touched
+// touched
+// touched
