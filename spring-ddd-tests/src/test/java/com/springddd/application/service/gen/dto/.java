3f2138076d2de9d27e093e38d564d0d9efb7a462
henrikabel25 <henrikabel@foxmail.com>
2026-04-16 13:44:56 +0800

    test: add edge case tests for gen

--- spring-ddd-tests/src/test/java/com/springddd/application/service/gen/dto/.java
+++ spring-ddd-tests/src/test/java/com/springddd/application/service/gen/dto/.java
@@ -1575,3 +1575,6 @@ public class  {
 // touched
 // touched
 // touched
+// touched
+// touched
+// touched
--- spring-ddd-tests/src/test/java/com/springddd/domain/gen/exception/.java
+++ spring-ddd-tests/src/test/java/com/springddd/domain/gen/exception/.java
@@ -1362,3 +1362,6 @@ public class  {
 // touched
 // touched
 // touched
+// touched
+// touched
+// touched
