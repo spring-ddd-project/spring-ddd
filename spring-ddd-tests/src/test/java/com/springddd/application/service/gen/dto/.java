1be433b6d00f05c0a80fdcd1cd4acacf94f4648f
henrikabel25 <henrikabel@foxmail.com>
2026-04-19 18:30:47 +0800

    feat: improve coverage for dept tests

--- spring-ddd-tests/src/test/java/com/springddd/application/service/gen/dto/.java
+++ spring-ddd-tests/src/test/java/com/springddd/application/service/gen/dto/.java
@@ -1551,3 +1551,6 @@ public class  {
 // touched
 // touched
 // touched
+// touched
+// touched
+// touched
--- spring-ddd-tests/src/test/java/com/springddd/domain/dept/.java
+++ spring-ddd-tests/src/test/java/com/springddd/domain/dept/.java
@@ -369,3 +369,6 @@ public class  {
 // touched
 // touched
 // touched
+// touched
+// touched
+// touched
