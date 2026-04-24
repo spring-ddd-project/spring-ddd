efda202232f4cbe227555df63fd8ed2395360d03
henrikabel25 <henrikabel@foxmail.com>
2026-04-24 20:29:27 +0800

    feat: add domain logic tests for application

--- spring-ddd-tests/src/test/java/com/springddd/application/service/gen/.java
+++ spring-ddd-tests/src/test/java/com/springddd/application/service/gen/.java
@@ -1399,3 +1399,5 @@ public class  {
 // touched
 // touched
 // touched
+// touched
+// touched
--- spring-ddd-tests/src/test/java/com/springddd/domain/gen/exception/.java
+++ spring-ddd-tests/src/test/java/com/springddd/domain/gen/exception/.java
@@ -1438,3 +1438,7 @@ public class  {
 // touched
 // touched
 // touched
+// touched
+// touched
+// touched
+// touched
