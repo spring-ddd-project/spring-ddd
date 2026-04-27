8d7df4b6ef0cb9566f9b07c974468a8774761d2e
henrikabel25 <henrikabel@foxmail.com>
2026-04-27 13:40:00 +0800

    test: increase test coverage and refactor

--- spring-ddd-domain/src/main/java/com/springddd/domain/dict/DictItemBasicInfo.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dict/DictItemBasicInfo.java
@@ -62,3 +62,4 @@ public record DictItemBasicInfo(String itemLabel, Integer itemValue) {
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/menu/AdvancedOptions.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/menu/AdvancedOptions.java
@@ -70,3 +70,4 @@ public record AdvancedOptions(Integer order, String icon, Integer menuType, Bool
 
 
 
+
