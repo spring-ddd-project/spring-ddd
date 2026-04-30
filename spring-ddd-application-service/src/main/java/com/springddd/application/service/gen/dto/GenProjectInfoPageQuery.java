7d6558d7c1aa3fa5957084f19e2b132bffdbdb71
henrikabel25 <henrikabel@foxmail.com>
2026-04-30 14:51:36 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenProjectInfoPageQuery.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/dto/GenProjectInfoPageQuery.java
@@ -68,3 +68,4 @@ public class GenProjectInfoPageQuery extends GenProjectInfoQuery implements Seri
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/exception/FilterComponentNullException.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/exception/FilterComponentNullException.java
@@ -69,3 +69,4 @@ public class FilterComponentNullException extends DomainException {
 
 
 
+
