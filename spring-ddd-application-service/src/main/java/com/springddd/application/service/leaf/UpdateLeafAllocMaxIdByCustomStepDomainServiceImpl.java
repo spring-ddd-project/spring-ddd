25a07fb7e7e0651283e62d9def7283040b5571fd
henrikabel25 <henrikabel@foxmail.com>
2026-05-01 17:08:19 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/UpdateLeafAllocMaxIdByCustomStepDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/leaf/UpdateLeafAllocMaxIdByCustomStepDomainServiceImpl.java
@@ -82,3 +82,4 @@ public class UpdateLeafAllocMaxIdByCustomStepDomainServiceImpl implements Update
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenColumnBindBasicInfo.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/gen/GenColumnBindBasicInfo.java
@@ -72,3 +72,4 @@ public record GenColumnBindBasicInfo(String columnType, String entityType, Byte 
 
 
 
+
