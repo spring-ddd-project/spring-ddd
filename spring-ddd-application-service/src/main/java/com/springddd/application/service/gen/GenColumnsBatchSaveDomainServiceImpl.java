e95aca45284aac9a50f3c090d0c64404c1d862b4
henrikabel25 <henrikabel@foxmail.com>
2026-05-01 11:26:37 +0800

    test: increase test coverage and refactor

--- spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenColumnsBatchSaveDomainServiceImpl.java
+++ spring-ddd-application-service/src/main/java/com/springddd/application/service/gen/GenColumnsBatchSaveDomainServiceImpl.java
@@ -132,3 +132,4 @@ public class GenColumnsBatchSaveDomainServiceImpl implements GenColumnsBatchSave
 
 
 
+
--- spring-ddd-domain/src/main/java/com/springddd/domain/dept/DeptExtendInfo.java
+++ spring-ddd-domain/src/main/java/com/springddd/domain/dept/DeptExtendInfo.java
@@ -77,3 +77,4 @@ public record DeptExtendInfo(Integer sortOrder, Boolean deptStatus) {
 
 
 
+
