f680a33888cee0a6f7a91a23f262b3286870258d
henrikabel25 <henrikabel@foxmail.com>
2026-06-22 21:46:14 +0800

    Revert "Merge pull request #192 from henrikabel25/feature/fix-command-service-template"
    This reverts commit 86bb5718b36637ba52ffb6bec13becf6a7af6937, reversing
    changes made to 539deba28056e95740a129ca2607b82c96154d43.

--- spring-ddd-domain/src/main/java/com/springddd/domain/leaf/LeafWorkerDomainRepository.java
+++
@@ -1,6 +0,0 @@
-package com.springddd.domain.leaf;
-
-import com.springddd.domain.DomainRepository;
-
-public interface LeafWorkerDomainRepository extends DomainRepository<LeafId, LeafWorkerDomain> {
-}
